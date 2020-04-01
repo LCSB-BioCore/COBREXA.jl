d42c9fcfdb3c42b659c684f701e16577eebbaf19
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2020-04-01 21:53:41 +0200

    better use types

--- src/modeling.jl
+++ src/modeling.jl
@@ -1,5 +1,9 @@
 using SparseArrays
 
+const VT = Union{Array{Float64,1}, SparseVector{Float64,Int64}}
+const MT = Union{AbstractMatrix, SparseMatrixCSC{Float64,Int64}}
+const ST = AbstractVector
+
 """
 A linear optimization problem of the form:
 ```
@@ -8,23 +12,23 @@ s.t. S x = b
      lb ≤ x ≤ ub
 ```
 """
-mutable struct LinearModel
-    S       ::Union{AbstractMatrix, SparseMatrixCSC{Float64,Int64}}
-    b       ::Union{Array{Float64,1}, SparseVector{Float64,Int64}}
-    c       ::Union{Array{Float64,1}, SparseVector{Float64,Int64}}
-    lb      ::Union{Array{Float64,1}, SparseVector{Float64,Int64}}
-    ub      ::Union{Array{Float64,1}, SparseVector{Float64,Int64}}
-    rxns    ::Array{String,1}
-    mets    ::Array{String,1}
+mutable struct LinearModel{M<:MT, V<:VT, C<:ST}
+    S       ::M
+    b       ::V
+    c       ::V
+    lb      ::V
+    ub      ::V
+    rxns    ::C
+    mets    ::C
 
     function LinearModel(
-        S       ::Union{AbstractMatrix, SparseMatrixCSC{Float64,Int64}},
-        b       ::Union{Array{Float64,1}, SparseVector{Float64,Int64}},
-        c       ::Union{Array{Float64,1}, SparseVector{Float64,Int64}},
-        lb      ::Union{Array{Float64,1}, SparseVector{Float64,Int64}},
-        ub      ::Union{Array{Float64,1}, SparseVector{Float64,Int64}},
-        rxns    ::Array{String,1},
-        mets    ::Array{String,1})
+        S       ::M,
+        b       ::V,
+        c       ::V,
+        lb      ::V,
+        ub      ::V,
+        rxns    ::C,
+        mets    ::C) where {V<:VT,M<:MT,C<:ST}
 
         n_c = length(c)
         n_b = length(b)
@@ -33,7 +37,7 @@ mutable struct LinearModel
         n_c == length(lb) || throw(DimensionMismatch("`c` doesn't have the same size as `lb`"))
 
         size(S) == (n_b, n_c) || throw(DimensionMismatch("`S` shape doesn't match with `c` and `b`"))
-        new(S, b, c, lb, ub, rxns, mets)
+        new{M,V,C}(S, b, c, lb, ub, rxns, mets)
     end
 end
 
