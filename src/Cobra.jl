961b13676176376602bd9cce646ca2ac9ea3dc95
st elmo <stelmozors@gmail.com>
2021-02-05 22:38:32 +0100

    update

--- src/Cobra.jl
+++ src/Cobra.jl
@@ -5,10 +5,19 @@ Model fields define the basic information necessary to run analysis tools
 struct Model
     S :: SparseMatrixCSC{Float64,Int64} # stoichiometric matrix
     b :: SparseVector{Float64,Int64} # mass balance rhs
-    lb :: Array{Float64, 1} # reaction lower bounds
-    ub :: Array{Float64, 1} # rxn upper bounds
+    lbs :: Array{Float64, 1} # reaction lower bounds
+    ubs :: Array{Float64, 1} # rxn upper bounds
     rxns :: Array{String, 1} # reactions
     mets :: Array{String, 1} # metabolites
-    grrs # gene-reaction-rules
+    grrs :: Dict{String, Array{Array{String, 1}, 1}} # reaction -> [[genes]]
+    Model() = begin
+        new(sparse(rand(0,0)), 
+            sparse(rand(0)), 
+            Float64[], 
+            Float64[], 
+            String[], 
+            String[], 
+            Dict{String, 
+            Array{Array{String, 1}, 1}})
+    end
 end
-
