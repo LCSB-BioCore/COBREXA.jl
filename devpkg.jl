b7c821945cba607b0becf34ecf7bac801f2494b3
st elmo <stelmozors@gmail.com>
2021-02-06 20:40:30 +0100

    fixed matlab model read

--- devpkg.jl
+++ devpkg.jl
@@ -2,15 +2,15 @@ using CobraTools
 using MATLAB
 using JSON
 
-jsonpath = joinpath("test/iMM904.json")
-m = JSON.parsefile(jsonpath)
-jsonmodel = CobraTools.readmodel(jsonpath)
+# jsonpath = joinpath("test/iMM904.json")
+# m = JSON.parsefile(jsonpath)
+# jsonmodel = CobraTools.readmodel(jsonpath)
 
-# matpath = joinpath("test/iMM904.mat")
+matpath = joinpath("test/iMM904.mat")
 # matpath = joinpath("test", "yeastGEM.mat") # broken
 
 
-# matmodel = CobraTools.readmodel(matpath)
+matmodel = CobraTools.readmodel(matpath)
 
 # mf = MatFile(matpath)
 # model_name = variable_names(mf)[1] # assume model name is the only variable
