d2829df3003b1a1d601b5abacab6ae0f2e151c2e
st elmo <stelmozors@gmail.com>
2021-02-06 23:53:54 +0100

    fixed read and write json and mat

--- devpkg.jl
+++ devpkg.jl
@@ -9,17 +9,17 @@ using JSON
 # jsonmodel2 = CobraTools.readmodel("test/iMM904_2.json")
 # m2 = JSON.parsefile("test/iMM904_2.json")
 
-# matpath = joinpath("test/iMM904.mat")
-# matmodel = CobraTools.readmodel(matpath)
-# CobraTools.savemodel(matmodel, "test/iMM904_2.mat")
-# matpath2 = joinpath("test/iMM904_2.mat")
-# matmodel2 = CobraTools.readmodel(matpath)
+matpath = joinpath("test/iMM904.mat")
+matmodel = CobraTools.readmodel(matpath)
+CobraTools.savemodel(matmodel, "test/iMM904_2.mat")
+matpath2 = joinpath("test/iMM904_2.mat")
+matmodel2 = CobraTools.readmodel(matpath2)
 
 
 # matpath = joinpath("test", "yeastGEM.mat") # broken
 
-# mf = MatFile(matpath)
-# model_name = variable_names(mf)[1] # assume model name is the only variable
-# modeldict = get_variable(mf, model_name)
-# close(mf)
+mf = MatFile(matpath2)
+model_name = variable_names(mf)[1] # assume model name is the only variable
+modeldict = get_variable(mf, model_name)
+close(mf)
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -18,13 +18,16 @@ yeast_mat = joinpath("yeastGEM.mat")
     sbmlmodel_ecoli = CobraTools.readmodel(iJO1366_xml)
 
     # Yeast GEM models
-    matlabmodel_yeast = CobraTools.readmodel(yeast_mat)
+    matlabmodel_yeast = CobraTools.readmodel(yeast_mat) # matlab issue
     sbmlmodel_yeast = CobraTools.readmodel(yeast_xml)
     
     @test model_comparison(jsonmodel_ecoli, matlabmodel_ecoli)
-    @test_broken model_comparison(sbmlmodel_ecoli, matlabmodel_ecoli)
+    @test_broken model_comparison(sbmlmodel_ecoli, matlabmodel_ecoli) # SBML not implemented yet
     @test_broken model_comparison(matlabmodel_yeast, sbmlmodel_yeast) # yeast GEM matlab format issue
 
+    @test read_write_read(jsonmodel_ecoli, "json")
+    @test read_write_read(matlabmodel_ecoli, "mat")
+    @test_broken read_write_read(sbmlmodel_ecoli, "xml") # SBML not implemented yet
 end    
 
 # @testset "Test something else" begin
