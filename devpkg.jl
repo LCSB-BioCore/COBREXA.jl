fa87f89da4ed2640f184717cd854a4353137f0db
st elmo <stelmozors@gmail.com>
2021-02-08 19:25:24 +0100

    added overall atom balance

--- devpkg.jl
+++ devpkg.jl
@@ -1,14 +1,13 @@
 using CobraTools
 
 modelpath = joinpath("models", "iJO1366.json") 
-jsonmodel = CobraTools.readmodel(modelpath)
+model = CobraTools.readmodel(modelpath)
 
-biomass_rxn = findfirst(jsonmodel.rxns, "BIOMASS_Ec_iJO1366_WT_53p95M")
-solobj = CobraTools.pfba(jsonmodel, biomass_rxn)
+biomass_rxn = findfirst(model.rxns, "BIOMASS_Ec_iJO1366_WT_53p95M")
+solobj = CobraTools.fba(model, biomass_rxn)
 
+ad = CobraTools.atom_exchange(model, biomass_rxn)
 
-# using JuMP
-# using Gurobi
 # coremodel = CobraTools.CoreModel(jsonmodel)
 # cbmodel, v, massbalance, fluxlbs, fluxubs = CobraTools.initCBM(coremodel)
 
