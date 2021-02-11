5c181286f78c85ea20819e77a5623ca5e39a9102
st elmo <stelmozors@gmail.com>
2021-02-11 18:25:00 +0100

    fixed basis analysis

--- src/basic_analysis.jl
+++ src/basic_analysis.jl
@@ -1,92 +1,77 @@
 """
-ReactionFluxes holds references to reactions, fluxes from analysis (e.g. FBA)
+ReactionFlux associates a rxn with a flux.
+"""
+mutable struct ReactionFlux
+    rxn :: Reaction
+    flux :: Float64
+end
+
+"""
+ReactionFluxes holds references to the objective, reactions and fluxes from analysis (e.g. FBA)
 """
 mutable struct ReactionFluxes
-    rxns :: Array{Reaction, 1} # each metabolite has an associated concentration
     objective_id :: String 
     objective :: Float64
-    fluxes :: Array{Float64, 1} # order will match because CoreModel is constructed from rxns in order
+    rxnfluxes :: Array{ReactionFlux, 1}
 end
 
 """
-updatefluxes!(reactionData, newfluxes)
+getindex(reactionfluxes, rxn)
 
-Sets the fluxes field of reactionData to the new fluxes
+Return the index of rxn in reactionfluxes and -1 if it is not found.
 """
-function update!(rd::ReactionFluxes, objective_id::String, objective::Float64,  fluxes::Array{Float64, 1})
-    rd.objective_id = objective_id
-    rd.objective = objective
-    for i in eachindex(rd)
-        rd.fluxes[i] = fluxes[i]
+function Base.getindex(rfs::ReactionFluxes, rxn)
+    for i in eachindex(rfs.rxnfluxes)
+        if rxn.id == rfs.rxnfluxes[i].rxn.id
+            return i
+        end
     end
+    return -1
 end
 
-function Base.show(io::IO, rd::ReactionFluxes)
-    afluxes = abs.(rd.fluxes)
-    if maximum(afluxes) ≈ 0.0
-        println(io, "No solution found.")
-    else
-        inds = sortperm(afluxes, rev=true) # display maximum absolute fluxes 
-        println(io, "Optimum for $(rd.objective_id) = ", round(rd.objective, digits=4))
-        counter = 0
-        for i in inds
-            if startswith(rd.rxns[i].id, "EX_")
-                println(io, rd.rxns[i].name, " = ", round(rd.fluxes[i], digits=4), " mmol/gDW/h")
-                counter += 1
-            end
-            if counter > 10
-                break
-            end
+"""
+Pretty printing of ReactionFluxes objects.
+"""
+function Base.show(io::IO, rfs::ReactionFluxes)
+    inds = sortperm(abs.([rf.flux for rf in rfs.rxnfluxes]), rev=true) # max abs fluxes 
+    println(io, "Optimum for $(rfs.objective_id) = ", round(rfs.objective, digits=4))
+    counter = 0
+    for i in inds
+        if startswith(rfs.rxnfluxes[i].rxn.id, "EX_")
+            println(io, rfs.rxnfluxes[i].rxn.name, " = ", round(rfs.rxnfluxes[i].flux, digits=4), " mmol/gDW/h")
+            counter += 1
+        end
+        if counter > 10 # only display top 10
+            break
         end
     end
 end
 
 """
-cbmodel = initCBM(coremodel :: CoreModel; optimizer="Gurobi")
+cbmodel = CBM(model::Model)
 
 Initialize a constraint based model. Creates a model that satisfies the mass balance
-and flux constraints but no objective is set. Return references to these objects for 
-simple modification if necessary.
+and flux constraints but no objective or optimizer is set. Returns the JuMP model.
 """
-function initCBM(coremodel::CoreModel; optimizer="gurobi")
+function CBM(model::Model)
+    coremodel = CoreModel(model) # Construct S, b, lbs, ubs from model
     cbmodel = JuMP.Model()
-
-    # set optimizer
-    if optimizer == "glpk"
-        set_optimizer(cbmodel, GLPK.Optimizer)
-        set_optimizer_attribute(model, "msg_lev", GLPK.GLP_MSG_OFF) # quiet
-    elseif optimizer == "gurobi"
-        set_optimizer(cbmodel, Gurobi.Optimizer)
-        set_optimizer_attribute(cbmodel, "OutputFlag", 0) # quiet
-    elseif optimizer == "tulip"
-        set_optimizer(cbmodel, Tulip.Optimizer) # quiet by default
-    elseif optimizer == "ipopt"
-        set_optimizer(cbmodel, Ipopt.Optimizer)
-        set_optimizer_attribute(model, "print_level", 0) # quiet    
-    else
-        cto.verbose && @warn "Optimizer not yet directly supported, however, you can set it yourself with `set_optimizer(cbmodel, OPTIMIZER)`.\nSee JuMP's documentation."
-    end
-    
     nvars = size(coremodel.S, 2) # number of variables in model
-    
     v = @variable(cbmodel, v[1:nvars]) # flux variables
     @constraint(cbmodel, massbalance, coremodel.S*v .== coremodel.b) # mass balance constraints
     @constraint(cbmodel, fluxlbs, coremodel.lbs .<= v)
     @constraint(cbmodel, fluxubs, v .<= coremodel.ubs)
-    
     return cbmodel
 end
 
 """
-solvefba(model::Model, objective_rxns; weights = Float64[], optimizer="gubori")
+reactionfluxes = fba(model::Model, objective_rxns; weights = Float64[], optimizer="gubori")
 
 Run flux balance analysis on the model using objective_rxn(s) and optionally specifying their weights.
-Optimiser can also be set here.
+Optimiser can also be set here. Uses the constraints implied by the model object. Objective is set separately.
 """
-function solvefirstfba(model::Model, objective_rxns::Union{Reaction, Array{Reaction, 1}}; weights=Float64[], optimizer="gurobi")
-
-    coremodel = CoreModel(model) # Construct S, b, lbs, ubs from model
-    cbm = initCBM(coremodel, optimizer=optimizer) # create the optimization problem
+function fba(model::Model, objective_rxns::Union{Reaction, Array{Reaction, 1}}; weights=Float64[], optimizer="gurobi")
+    cbm = CBM(model) # get the base constraint based model
 
     # ensure that an array of objective indices are fed in
     if typeof(objective_rxns) == Reaction
@@ -94,10 +79,12 @@ function solvefirstfba(model::Model, objective_rxns::Union{Reaction, Array{React
     else
         objective_indices = [model[rxn] for rxn in objective_rxns]
     end
+    
     # ensure corrects weights are given to objective
     if isempty(weights)
         weights = ones(length(objective_indices))
     end
+    
     # update the objective function tracker
     wcounter = 1
     for i in eachindex(model.rxns)
@@ -108,47 +95,52 @@ function solvefirstfba(model::Model, objective_rxns::Union{Reaction, Array{React
             model.rxns[i].objective_coefficient = 0.0
         end
     end
+
+    # set optimizer
+    if optimizer == "glpk"
+        set_optimizer(cbm, GLPK.Optimizer)
+        set_optimizer_attribute(model, "msg_lev", GLPK.GLP_MSG_OFF) # quiet
+    elseif optimizer == "gurobi"
+        set_optimizer(cbm, Gurobi.Optimizer)
+        set_optimizer_attribute(cbm, "OutputFlag", 0) # quiet
+    elseif optimizer == "tulip"
+        set_optimizer(cbm, Tulip.Optimizer) # quiet by default
+    elseif optimizer == "ipopt"
+        set_optimizer(cbm, Ipopt.Optimizer)
+        set_optimizer_attribute(model, "print_level", 0) # quiet    
+    else
+        @warn "Optimizer not yet directly supported, however, you can set it yourself with `set_optimizer(cbmodel, OPTIMIZER)`.\nSee JuMP's documentation."
+    end
+    
     v = all_variables(cbm)
     @objective(cbm, Max, sum(v[i] for i in objective_indices))
     optimize!(cbm)    
 
-    return cbm
-end
-
-function fba(model::Model, objective_rxns::Union{Reaction, Array{Reaction, 1}}; weights=Float64[], optimizer="gurobi")
-    
-    cbm = solvefirstfba(model, objective_rxns; weights=weights, optimizer=optimizer)
-    
-    v = all_variables(cbm)
-
-    if typeof(objective_rxns) == Reaction
-        objective_indices = [model[objective_rxns]]
-    else
-        objective_indices = [model[rxn] for rxn in objective_rxns]
-    end
-
     status = termination_status(cbm) == MOI.OPTIMAL
     
     if status
-        objective_id = length(objective_indices) == 1 ? model.rxns[objective_indices[1]].id : "multiple reactions"    
-        return ReactionFluxes(model.rxns, objective_id, objective_value(cbm), [value(v[i]) for i in eachindex(v)])        
+        objective_id = length(objective_indices) == 1 ? model.rxns[objective_indices[1]].id : "multiple reactions"
+        arr= Array{ReactionFlux, 1}()
+        for (i, rxn) in enumerate(model.rxns)
+            push!(arr, ReactionFlux(rxn, value(v[i])))
+        end
+        return ReactionFluxes(objective_id, objective_value(cbm), arr)        
     else
-        return ReactionFluxes(model.rxns, "", 0.0, zeros(length(v)))
+        return ReactionFluxes("Optimization issues", 0.0, Array{ReactionFlux, 1}())
     end
 end
 
 """
-solobj = pfba()
+reactionfluxes = pfba(model::Model, objective_rxns::Union{Reaction, Array{Reaction, 1}}; weights=Float64[], optimizer="gurobi")
 
-Not efficient.
+Run parsimonious flux balance analysis on the model using objective_rxn(s) as the objective for the initial FBA problem. 
+Optionally, this/these objectives can be weighted by weights.
+Optimiser can also be set here. Uses the constraints implied by the model object.
 """
 function pfba(model::Model, objective_rxns::Union{Reaction, Array{Reaction, 1}}; weights=Float64[], optimizer="gurobi")
-    
-    cbm = solvefirstfba(model, objective_rxns; weights=weights, optimizer=optimizer)
-    v = all_variables(cbm)
+    ## FBA ################################################
+    cbm = CBM(model) # get the base constraint based model
 
-    statusfba = termination_status(cbm) == MOI.OPTIMAL
-    
     # ensure that an array of objective indices are fed in
     if typeof(objective_rxns) == Reaction
         objective_indices = [model[objective_rxns]]
@@ -156,39 +148,94 @@ function pfba(model::Model, objective_rxns::Union{Reaction, Array{Reaction, 1}};
         objective_indices = [model[rxn] for rxn in objective_rxns]
     end
     
+    # ensure corrects weights are given to objective
+    if isempty(weights)
+        weights = ones(length(objective_indices))
+    end
+    
+    # update the objective function tracker
+    wcounter = 1
+    for i in eachindex(model.rxns)
+        if i in objective_indices
+            model.rxns[i].objective_coefficient = weights[wcounter]
+            wcounter += 1
+        else
+            model.rxns[i].objective_coefficient = 0.0
+        end
+    end
+
+    # set optimizer
+    if optimizer == "glpk"
+        set_optimizer(cbm, GLPK.Optimizer)
+        set_optimizer_attribute(model, "msg_lev", GLPK.GLP_MSG_OFF) # quiet
+    elseif optimizer == "gurobi"
+        set_optimizer(cbm, Gurobi.Optimizer)
+        set_optimizer_attribute(cbm, "OutputFlag", 0) # quiet
+    elseif optimizer == "tulip"
+        set_optimizer(cbm, Tulip.Optimizer) # quiet by default
+    elseif optimizer == "ipopt"
+        set_optimizer(cbm, Ipopt.Optimizer)
+        set_optimizer_attribute(model, "print_level", 0) # quiet    
+    else
+        @warn "Optimizer not yet directly supported, however, you can set it yourself with `set_optimizer(cbmodel, OPTIMIZER)`.\nSee JuMP's documentation."
+    end
+    
+    v = all_variables(cbm)
+    @objective(cbm, Max, sum(v[i] for i in objective_indices))
+    optimize!(cbm)    
+
+    fba_status = termination_status(cbm) == MOI.OPTIMAL
+
+    ## pFBA ###############################################
     λ = objective_value(cbm)
 
-    @constraint(cbm, pfbacon, 0.9999*λ <= sum(v[i] for i in objective_indices) <= λ) # constrain model - 0.9999 should be close enough?
+    @constraint(cbm, pfbacon, 0.999999*λ <= sum(v[i] for i in objective_indices) <= λ) # constrain model - 0.9999 should be close enough?
     @objective(cbm, Min, sum(dot(v, v)))
     optimize!(cbm)
 
-    if termination_status(cbm) != MOI.OPTIMAL # try to relax bound
+    if termination_status(cbm) != MOI.OPTIMAL # try to relax bound if failed optimization
         JuMP.delete(cbm, pfbacon)
-        @constraint(cbm, pfbacon, 0.999*λ <= sum(v[i] for i in objective_indices) <= λ) 
+        @constraint(cbm, 0.99999*λ <= sum(v[i] for i in objective_indices) <= λ) 
+        optimize!(cbm)    
+    end
+    if termination_status(cbm) != MOI.OPTIMAL # try to relax bound if failed optimization
+        JuMP.delete(cbm, pfbacon)
+        @constraint(cbm, 0.9999*λ <= sum(v[i] for i in objective_indices) <= λ) 
+        optimize!(cbm)    
+    end
+    if termination_status(cbm) != MOI.OPTIMAL # try to relax bound if failed optimization
+        JuMP.delete(cbm, pfbacon)
+        @constraint(cbm, 0.999*λ <= sum(v[i] for i in objective_indices) <= λ) 
         optimize!(cbm)    
     end
 
-    statuspfba = termination_status(cbm) == MOI.OPTIMAL
+    pfba_status = termination_status(cbm) == MOI.OPTIMAL
     
-    if statusfba && statuspfba
-        return ReactionFluxes(model.rxns, "Σ||v - ̄v||", objective_value(cbm), [value(v[i]) for i in eachindex(v)])        
+    if fba_status && pfba_status
+        arr = Array{ReactionFlux, 1}()
+        for (i, rxn) in enumerate(model.rxns)
+            push!(arr, ReactionFlux(rxn, value(v[i])))
+        end
+        return ReactionFluxes("Σᵢ||vᵢ||", objective_value(cbm), arr)        
     else
-        return ReactionFluxes(model.rxns, "", 0.0, zeros(length(v)))
+        return ReactionFluxes("Optimization issues", 0.0, Array{ReactionFlux, 1}())
     end
 end
 
 """
-Perform pFBA and calculate the atom balance across the model
+atom_balance_dict = atom_exchange(reactionfluxes)
+
+Return the composition of atoms consumed or produced by the model according to reactionfluxes.
 """
 function atom_exchange(rfs::ReactionFluxes)
     # find exchange reactions
-    ex_inds = [i for i in eachindex(rfs.rxns) if startswith(rfs.rxns[i].id, "EX_")]
+    ex_inds = [i for i in eachindex(rfs.rxnfluxes) if startswith(rfs.rxnfluxes[i].rxn.id, "EX_")]
     
     atom_balance = Dict{String, Float64}()
     for ex_ind in ex_inds
-        for (met, w) in rfs.rxns[ex_ind].metabolites
+        for (met, w) in rfs.rxnfluxes[ex_ind].rxn.metabolites
             for (atom, stoich) in getatoms(met)
-                atom_balance[atom] = get(atom_balance, atom, 0.0) + stoich*w*value(rfs.fluxes[ex_ind])
+                atom_balance[atom] = get(atom_balance, atom, 0.0) + stoich*w*value(rfs.rxnfluxes[ex_ind].flux)
             end
         end
     end
