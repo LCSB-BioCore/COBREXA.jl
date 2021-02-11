6e290afb106cda0656ee6d552f7adcf6ab55045a
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2021-02-11 11:39:53 +0100

    remove triple negation

--- test/base/header.jl
+++ test/base/header.jl
@@ -1,5 +1,5 @@
 
 @testset "Header" begin
     loadRes = COBREXA.loadSource(["base", "io", "reconstruction", "analysis"], joinpath(dirname(pathof(COBREXA))), true)
-    @test any(val->!val, loadRes) == false
+    @test all(loadRes)
 end
