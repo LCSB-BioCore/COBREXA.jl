f25804734c01d5f9c03c5758f5884f29ddf6044b
st elmo <stelmozors@gmail.com>
2021-02-19 12:57:28 +0100

    neaten main module

--- src/CobraTools.jl
+++ src/CobraTools.jl
@@ -1,46 +1,34 @@
 module CobraTools
 
-# IO of models and data
 using JSON
 using MATLAB
 using SBML
-
-# Model analysis
 using SparseArrays
 using JuMP
 using LinearAlgebra
+using Measurements
+using Statistics
+using PyCall # for Equilibrator
+using Random
+
 # Find a way to only import packages the user actually has...?
 using Gurobi
 using Tulip
 using GLPK
 using Ipopt
 
-# Gibbs
-using Measurements
-using Statistics
-using PyCall # for Equilibrator - ensure that it is installed
-
-# Sampling
-using Random
-
 include("cobra_base.jl")
-export Reaction, Metabolite, Gene
-
 include("parse_models.jl")
-
 include("rxn_tools.jl")
+include("met_tools.jl")
+include("basic_analysis.jl")
+include("gibbs_tools.jl")
+include("sampling.jl")
+
 ∅ = Metabolite("∅") # for exchange reactions
 export ∅, ⟶, →, ←, ⟵, ↔, ⟷
+export Reaction, Metabolite, Gene
 
-include("met_tools.jl")
-
-include("basic_analysis.jl")
-# export Solution
-
-include("gibbs_tools.jl")
-include("name_space.jl")
-
-include("sampling.jl")
 
 # Initialization functions
 include("init_functions.jl")
