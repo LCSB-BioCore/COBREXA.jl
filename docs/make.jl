6bd1c2ef858314611de5c45c7eb75d54535025af
st elmo <stelmozors@gmail.com>
2021-02-25 21:19:47 +0100

    fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(
         "Optimization Based Analysis" => "basic_analysis.md",
         "Sampling Tools" => "sampling_tools.md",
         "Equilibrator Interface" => "thermo_tools.md",
-        "Brenda Interface" => "brenda_tools.jl"
+        "Brenda Interface" => "brenda_tools.md"
     ]
 )
 
