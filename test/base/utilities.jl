e99d52057baddc174416b598e7221466c2129500
Mirek Kratochvil <exa.exa@gmail.com>
2021-02-26 13:00:08 +0100

    replace parFVA with the distributed implementation

--- test/base/utilities.jl
+++ test/base/utilities.jl
@@ -13,10 +13,3 @@
     cp = test_coupledLP()
     @test nCouplingConstraints(cp) == 2000
 end
-
-@testset "Allocating reactions" begin
-    @test COBREXA.allocateReacs([1], 1) == [[1]]
-    @test COBREXA.allocateReacs(collect(1:3), 2) == [[1;2], [3]]
-    @test COBREXA.allocateReacs(collect(1:8), 4) == [[1;2], [3;4], [5;6], [7;8]]
-    @test COBREXA.allocateReacs(collect(1:11), 3) == [[1;2;3;4], [5;6;7;8], [9;10;11]]
-end
