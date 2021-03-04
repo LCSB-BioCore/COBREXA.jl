549b9e3edc8883deea56a51ae4eaa59183091874
Mirek Kratochvil <exa.exa@gmail.com>
2021-03-04 19:39:59 +0100

    fba is now trivial

--- src/analysis/fba.jl
+++ src/analysis/fba.jl
@@ -1,36 +1,10 @@
 """
-Convert LinearModel to the JuMP model, place objectives and the equality
-constraint.
-"""
-function makeOptimizationModel(model::LinearModel, optimizer)
-    m, n = size(model.S)
-
-    optimization_model = JuMP.Model(optimizer)
-    @variable(
-        optimization_model,
-        x[i = 1:n],
-        lower_bound = model.xl[i],
-        upper_bound = model.xu[i]
-    )
-    @objective(optimization_model, Max, model.c' * x)
-    @constraint(optimization_model, model.S * x .== model.b)
-    return (optimization_model, x)
-end
-
-"""
-Flux Balance Analysis
+Flux balance analysis, solves the following problem for the input `model`:
 ```
 max cᵀx
 s.t. S x = b
      xₗ ≤ x ≤ xᵤ
 ```
 """
-function fluxBalanceAnalysis(model::LinearModel, optimizer)
-
-    optimization_model, x = makeOptimizationModel(model, optimizer)
-    JuMP.optimize!(optimization_model)
-    return (optimization_model, x)
-    # TODO we might like this to take optimization model and return x, and let
-    # the user convert LinearModel to JuMP model. That would make the API much
-    # more orthogonal.
-end
+fluxBalanceAnalysis(model::LM, optimizer) where {LM<:AbstractLinearModel} =
+    solveLP(model, optimizer; sense = MOI.MAX_SENSE)
