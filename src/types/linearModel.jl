3b044b0351157eb6f6d1cb2c7a278f0c2923c74f
Mirek Kratochvil <exa.exa@gmail.com>
2021-03-04 15:36:00 +0100

    abstract interface for linear models from any package

--- src/types/linearModel.jl
+++ src/types/linearModel.jl
@@ -1,41 +1,99 @@
-const VT = AbstractVector{Float64}
-const Vt = SparseVector{Float64,Int64}
+
+"""
+    abstract type AbstractLinearModel end
+
+A helper supertype that wraps everything usable as a LinearModel for COBREXA
+functions. If you want to use your own type, make it a subtype (so that the
+functions typecheck) and add instances for the data accessor methods below.
+"""
+abstract type AbstractLinearModel end
+
+const SpMtx = SparseMatrixCSC{Float64,Int}
+const SpVec = SparseVector{Float64,Int}
+const StrVec = Vector{String}
 const MT = AbstractMatrix{Float64}
-const Mt = SparseMatrixCSC{Float64,Int64}
+const VT = AbstractVector{Float64}
 const ST = AbstractVector{String}
-const St = Vector{String}
+
+_missingImplError = (m, a) -> throw(MethodError(m, a))
+
+function reactions(a::LM)::StrVec where {LM<:AbstractLinearModel}
+    _missingImplError(reactions, (a,))
+end
+
+function metabolites(a::LM)::StrVec where {LM<:AbstractLinearModel}
+    _missingImplError(metabolites, (a,))
+end
+
+function nReactions(a::LM)::Int where {LM<:AbstractLinearModel}
+    length(reactions(a))
+end
+
+function nMetabolites(a::LM)::Int where {LM<:AbstractLinearModel}
+    length(metabolites(a))
+end
+
+function stoichiometry(a::LM)::SpMtx where {LM<:AbstractLinearModel}
+    _missingImplError(stoichiometry, (a,))
+end
+
+function bounds(a::LM)::Tuple{SpVec,SpVec} where {LM<:AbstractLinearModel}
+    _missingImplError(bounds, (a,))
+end
+
+function balance(a::LM)::SpVec where {LM<:AbstractLinearModel}
+    _missingImplError(balance, (a,))
+end
+
+function objective(a::LM)::SpVec where {LM<:AbstractLinearModel}
+    _missingImplError(objective, (a,))
+end
+
+function coupling(a::LM)::SpMtx where {LM<:AbstractLinearModel}
+    _missingImplError(coupling, (a,))
+end
+
+function nCouplingConstraints(a::LM)::Int where {LM<:AbstractLinearModel}
+    size(coupling(a), 1)
+end
+
+function couplingBounds(a::LM)::Tuple{SpVec,SpVec} where {LM<:AbstractLinearModel}
+    _missingImplError(couplingBounds, (a,))
+end
 
 
 """
-A linear optimization problem of the form:
+A concrete linear optimization problem of the form:
 ```
 min c^T x
 s.t. S x = b
     cₗ ≤ C x ≤ cᵤ
     xₗ ≤ x ≤ xᵤ
 ```
+
+This is the default model supported by COBREXA model building functions.
 """
-mutable struct LinearModel
-    S::Mt
-    b::Vt
-    C::Mt
-    cl::Vt
-    cu::Vt
-    c::Vt
-    xl::Vt
-    xu::Vt
-    rxns::St
-    mets::St
+mutable struct LinearModel <: AbstractLinearModel
+    S::SpMtx
+    b::SpVec
+    C::SpMtx
+    cl::SpVec
+    cu::SpVec
+    c::SpVec
+    xl::SpVec
+    xu::SpVec
+    rxns::StrVec
+    mets::StrVec
 
     function LinearModel(
-        S::MX,
-        b::VX,
-        c::VX,
-        xl::VX,
-        xu::VX,
-        rxns::SX,
-        mets::SX,
-    ) where {VX<:VT,MX<:MT,SX<:ST}
+        S::M,
+        b::V,
+        c::V,
+        xl::V,
+        xu::V,
+        rxns::K,
+        mets::K,
+    ) where {V<:VT,M<:MT,K<:ST}
 
         sS = sparse(S)
         sb = sparse(b)
@@ -50,17 +108,17 @@ mutable struct LinearModel
     end
 
     function LinearModel(
-        S::MX,
-        b::VX,
-        C::MX2,
-        cl::VX,
-        cu::VX,
-        c::VX,
-        xl::VX,
-        xu::VX,
-        rxns::SX,
-        mets::SX,
-    ) where {VX<:VT,MX<:MT,MX2<:MT,SX<:ST}
+        S::M1,
+        b::V,
+        C::M2,
+        cl::V,
+        cu::V,
+        c::V,
+        xl::V,
+        xu::V,
+        rxns::K,
+        mets::K,
+    ) where {V<:VT,M1<:MT,M2<:MT,K<:ST}
 
         checkInputDimensions(S, b, C, cl, cu, c, xl, xu, rxns, mets)
 
@@ -75,4 +133,36 @@ mutable struct LinearModel
 
         new(sS, sb, sC, scl, scu, sc, sxl, sxu, rxns, mets)
     end
+end
+
+function reactions(a::LinearModel)::StrVec
+    a.rxns
+end
+
+function metabolites(a::LinearModel)::StrVec
+    a.mets
+end
+
+function stoichiometry(a::LinearModel)::SpMtx
+    a.S
+end
+
+function bounds(a::LinearModel)::Tuple{SpVec,SpVec}
+    (a.xl, a.xu)
+end
+
+function balance(a::LinearModel)::SpVec
+    a.b
+end
+
+function objective(a::LinearModel)::SpVec
+    a.c
+end
+
+function coupling(a::LinearModel)::SpMtx
+    a.C
+end
+
+function couplingBounds(a::LinearModel)::Tuple{SpVec,SpVec}
+    (a.cl, a.cu)
 end
