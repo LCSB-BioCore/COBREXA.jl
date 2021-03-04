3acb479c2d4445e80b3484d8005fab51d4ae1846
Mirek Kratochvil <exa.exa@gmail.com>
2021-03-04 19:39:26 +0100

    move all problem-solving functions to solver.jl, cleanup

--- src/base/solver.jl
+++ src/base/solver.jl
@@ -1,20 +1,32 @@
 """
-Use JuMP to solve an instance of LinearModel
+Convert LinearModel to the JuMP model, place objectives and the equality
+constraint.
 """
-function solveLP(model::LinearModel, optimizer)
-    m, n = size(model.S)
+function makeOptimizationModel(
+    model::LM,
+    optimizer;
+    sense = MOI.MAX_SENSE,
+) where {LM<:AbstractLinearModel}
+    m, n = size(stoichiometry(model))
+    xl, xu = bounds(model)
 
     optimization_model = JuMP.Model(optimizer)
-    @variable(
-        optimization_model,
-        x[i = 1:n],
-        lower_bound = model.xl[i],
-        upper_bound = model.xu[i]
-    )
-    @objective(optimization_model, Min, model.c' * x)
-    @constraint(optimization_model, model.S * x .== model.b)
+    @variable(optimization_model, x[i = 1:n], lower_bound = xl[i], upper_bound = xu[i])
+    @objective(optimization_model, sense, objective(model)' * x)
+    @constraint(optimization_model, stoichiometry(model) * x .== balance(model))
 
+    return (optimization_model, x)
+end
+
+"""
+Use JuMP to solve an instance of LinearModel
+"""
+function solveLP(
+    model::LM,
+    optimizer;
+    sense = MOI.MIN_SENSE,
+) where {LM<:AbstractLinearModel}
+    optimization_model, x = makeOptimizationModel(model, optimizer; sense = sense)
     JuMP.optimize!(optimization_model)
-
     return (optimization_model, x)
 end
