d9b09e244f2db0c1ae1be74353ffbfcab2a8a173
st elmo <stelmozors@gmail.com>
2021-03-13 16:41:51 +0100

    format

--- test/external/brenda_tests.jl
+++ test/external/brenda_tests.jl
@@ -7,29 +7,40 @@
 
     model = CobraTools.read_model(joinpath("data", "e_coli_core.json"))
     biomass = findfirst(model.reactions, "BIOMASS_Ecoli_core_w_GAM")
-    atts = Dict("eps_abs" => 5e-4,"eps_rel" => 5e-4, "max_iter" => 100_000, "verbose"=>false) 
-    sol = pfba(model, biomass, [Tulip.Optimizer, OSQP.Optimizer]; solver_attributes=Dict("opt1" => Dict{Any, Any}(), "opt2" => atts)) # try two optimizers
+    atts = Dict(
+        "eps_abs" => 5e-4,
+        "eps_rel" => 5e-4,
+        "max_iter" => 100_000,
+        "verbose" => false,
+    )
+    sol = pfba(
+        model,
+        biomass,
+        [Tulip.Optimizer, OSQP.Optimizer];
+        solver_attributes = Dict("opt1" => Dict{Any,Any}(), "opt2" => atts),
+    ) # try two optimizers
 
     r_str = CobraTools.build_rxn_string(model.reactions[64])
-    rs = split(replace(r_str, r"(1.0 )|( \+)|(= )|(KEGG:)"=>""), " ")
-    @test all([x in ["C00354", "C00111", "C00661"] for x in rs ])
+    rs = split(replace(r_str, r"(1.0 )|( \+)|(= )|(KEGG:)" => ""), " ")
+    @test all([x in ["C00354", "C00111", "C00661"] for x in rs])
 
-    sprint(show, MIME("text/plain"), brenda_data[1].TN[1]) == "Value: 0.52\nSubstrate: DL-glyceraldehyde\npH: 7.0\nTemperature: 25.0\n"
+    sprint(show, MIME("text/plain"), brenda_data[1].TN[1]) ==
+    "Value: 0.52\nSubstrate: DL-glyceraldehyde\npH: 7.0\nTemperature: 25.0\n"
     sprint(show, MIME("text/plain"), brenda_data[1]) == "EC: 1.1.1.10\n"
-    
+
     gibbs_str = JSON.parsefile(joinpath("data", "gibbs.json"))
-    gibbs = Dict{String, Measurement{Float64}}()
+    gibbs = Dict{String,Measurement{Float64}}()
     for (k, v) in gibbs_str
         gibbs[k] = parse(Measurement{Float64}, v)
     end
-    
+
     ΔG, mf = map_gibbs_external(sol, gibbs)
-    @test isapprox(ΔG.val, -3006.0, atol=10.0)
-    @test isapprox(ΔG.err, 94.0, atol=10.0)
-    @test isapprox(mf, 0.034094939786929804, atol=1e-1)
-        
+    @test isapprox(ΔG.val, -3006.0, atol = 10.0)
+    @test isapprox(ΔG.err, 94.0, atol = 10.0)
+    @test isapprox(mf, 0.034094939786929804, atol = 1e-1)
+
     ΔG, mf = map_gibbs_internal(sol, gibbs)
-    @test isapprox(ΔG.val, -11200.0, atol=100.0)
-    @test isapprox(ΔG.err, 1300.0, atol=50.0)
-    @test isapprox(mf,  0.20039554480374108, atol=1e-1)
+    @test isapprox(ΔG.val, -11200.0, atol = 100.0)
+    @test isapprox(ΔG.err, 1300.0, atol = 50.0)
+    @test isapprox(mf, 0.20039554480374108, atol = 1e-1)
 end
--- test/testing_functions.jl
+++ test/testing_functions.jl
@@ -11,8 +11,10 @@ function model_comparison_test(model1, model2)
     rxns2 = [r.id for r in model2.reactions]
     mets1 = [m.id for m in model1.metabolites]
     mets2 = [m.id for m in model2.metabolites]
-    rxns_same = (isempty(setdiff(rxns1, rxns2)) && isempty(setdiff(rxns2, rxns1))) ? true : false
-    mets_same = (isempty(setdiff(mets1, mets2)) && isempty(setdiff(mets2, mets1))) ? true : false
+    rxns_same =
+        (isempty(setdiff(rxns1, rxns2)) && isempty(setdiff(rxns2, rxns1))) ? true : false
+    mets_same =
+        (isempty(setdiff(mets1, mets2)) && isempty(setdiff(mets2, mets1))) ? true : false
 
     # test lb and ub the same (indirectly)
     lbs1 = [r.lb for r in model1.reactions]
@@ -22,7 +24,7 @@ function model_comparison_test(model1, model2)
 
     lbs_same = sum(abs, lbs1) == sum(abs, lbs2)
     ubs_same = sum(abs, ubs1) == sum(abs, ubs2)
-        
+
     S1, _, _, _ = get_core_model(model1)
     S2, _, _, _ = get_core_model(model2)
     Ss_same = sum(S1) == sum(S2) ? true : false
@@ -47,7 +49,7 @@ end
 Test if model is the same after it was read in, saved, and then re-read. 
 """
 function read_write_read_test(model, format)
-    tmpfile = joinpath("data", "temp."*format)
+    tmpfile = joinpath("data", "temp." * format)
 
     CobraTools.save_model(model, tmpfile)
     tmpmodel = CobraTools.read_model(tmpfile)
