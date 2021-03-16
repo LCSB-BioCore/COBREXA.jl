4564c8b66e1db007c1d1014d5c1541dfcb0de8e3
st elmo <stelmozors@gmail.com>
2021-03-16 16:32:54 +0100

    update toml and combine pkg root

--- src/COBREXA.jl
+++ src/COBREXA.jl
@@ -51,3 +51,85 @@ export speye, LinearModel, nReactions, nMetabolites, nCouplingConstraints,
        convertToExportable
 
 end
+
+
+##########################################################
+using Requires
+using JSON
+using Measurements
+using Statistics, Random
+using PyCall
+using JuMP, Tulip, OSQP # OSQP sucks for LPs
+
+import Base: findfirst, getindex, show
+
+# definitions of structs
+include(joinpath("base", "metabolite.jl"))
+include(joinpath("base", "gene.jl"))
+include(joinpath("base", "reaction.jl"))
+include(joinpath("base", "model.jl"))
+
+# Tools
+include(joinpath("io", "io_tools.jl"))
+include(joinpath("construction", "construction_overloading.jl"))
+include(joinpath("construction", "model_manipulations.jl"))
+include(joinpath("optimization_analysis", "basic_analysis.jl"))
+include(joinpath("sampling", "sampling_tools.jl"))
+include(joinpath("external", "brenda_tools.jl"))
+include(joinpath("external", "equilibrator_tools.jl"))
+
+∅ = Metabolite("∅") # for exchange reactions
+
+export ∅,
+
+    # base
+    Model,
+    Gene,
+    Reaction,
+    Metabolite,
+    check_duplicate_annotations,
+    get_atoms,
+    check_same_formula,
+    is_mass_balanced,
+    check_duplicate_reaction,
+
+    # construction
+    ⟶,
+    →,
+    ←,
+    ⟵,
+    ↔,
+    ⟷,
+    add!,
+    rm!,
+    fix_model!,
+
+    # io
+    read_model,
+    save_model,
+
+    # optimization_analysis
+    get_core_model,
+    build_cbm,
+    fba,
+    map_fluxes,
+    set_bound,
+    pfba,
+    atom_exchange,
+    exchange_reactions,
+    metabolite_fluxes,
+    fva,
+
+    # sampling
+    hit_and_run,
+    test_samples,
+    achr,
+
+    # external
+    parse_brenda,
+    map_gibbs_rxns,
+    map_gibbs_external,
+    map_gibbs_internal
+
+# Initialization functions
+include("init_functions.jl")
