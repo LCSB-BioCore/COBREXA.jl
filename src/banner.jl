2e6109c74c5b242f85d884862982bbb3a5fa0fa4
Mirek Kratochvil <exa.exa@gmail.com>
2021-03-20 12:13:03 +0100

    clean up banner printing
    (because of indentation it printed out 4 spaces on the line below)

--- src/banner.jl
+++ src/banner.jl
@@ -12,14 +12,13 @@ function _printBanner()
     r = c[:bold] * c[:red]
     g = c[:bold] * c[:green]
     m = c[:bold] * c[:magenta]
-    print(
+    println(
         "
   ____ ___  ____  ____  _____$(g)__$(n)  $(r)__$(n)    _     |
  / ___/ _ \\| __ )|  _ \\| ____$(g)\\ \\$(n)$(r)/ /$(n)   / \\    | Constraint-Based Reconstruction
 | |  | | | |  _ \\| |_) |  _|  $(g)\\$(n)  $(r)/$(n)   / _ \\   | and EXascale Analysis in Julia
 | |__| |_| | |_) |  _ <| |___ $(m)/$(n)  $(b)\\$(n)  / ___ \\  |
  \\____\\___/|____/|_| \\_\\_____$(m)/_/$(n)$(b)\\_\\$(n)/_/   \\_\\ | Version: v$(COBREXA_VERSION)
-                                             |
-   ",
+                                             |",
     )
 end
