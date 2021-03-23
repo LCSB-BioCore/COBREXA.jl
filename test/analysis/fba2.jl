e298efd530c8c05664c64038599071ad3de4c018
st elmo <stelmozors@gmail.com>
2021-03-23 14:34:39 +0100

    fba tests pass

--- test/analysis/fba2.jl
+++ test/analysis/fba2.jl
@@ -6,24 +6,22 @@
     biomass = findfirst(model.reactions, "BIOMASS_Ecoli_core_w_GAM")
     cons = Dict("EX_glc__D_e" => (-12.0, -12.0))
     optimizer = Tulip.Optimizer # quiet by default
-    sol = fba(model, optimizer; objective=biomass, constraints = cons)
+    sol = fba(model, optimizer; objective_func=biomass, constraints = cons)
     pfl = findfirst(model.reactions, "PFL")
-    solmulti = fba(model, optimizer; objective=[biomass, pfl], weights = [0.8, 0.2]) # classic flux balance analysis
+    solmulti = fba(model, optimizer; objective_func=[biomass, pfl], weights = [0.8, 0.2]) # classic flux balance analysis
 
 
     flux_vec = [sol[rxn.id] for rxn in model.reactions]
     sol_mapped = map_fluxes(flux_vec, model)
-    @testset "FBA" begin
-        @test isapprox(
+    @test isapprox(
             sol_mapped["BIOMASS_Ecoli_core_w_GAM"],
             1.0572509997013568,
             atol = 1e-6,
         )
         @test isapprox(sol["BIOMASS_Ecoli_core_w_GAM"], 1.0572509997013568, atol = 1e-6)
         @test !isempty(solmulti)
-    end
-
-    sol = fba(model, optimizer; objective=biomass)
+    
+    sol = fba(model, optimizer; objective_func=biomass)
 
     # atom tracker
     atom_fluxes = atom_exchange(sol, model)
@@ -39,17 +37,14 @@
     @test isapprox(producing["atp_c"]["PYK"], 1.75818, atol = 1e-3)
 
     # set bounds
-    # cbmodel, v, mb, ubs, lbs = build_cbm(model)
-    cbmodel, v = makeOptimizationModel(model, optimizer, sense=sense)
-    ubs = all_constraints(cbm, VariableRef, MOI.GreaterThan{Float64})
-    lbs = all_constraints(cbm, VariableRef, MOI.LessThan{Float64})
-    # glucose_index = model[findfirst(model.reactions, "EX_glc__D_e")]
-    # o2_index = model[findfirst(model.reactions, "EX_o2_e")]
-    # atpm_index = model[findfirst(model.reactions, "ATPM")]
-    # set_bound(glucose_index, ubs, lbs; ub = -1.0, lb = -1.0)
-    # @test normalized_rhs(ubs[glucose_index]) == -1.0
-    # @test normalized_rhs(lbs[glucose_index]) == 1.0
-    # set_bound(o2_index, ubs, lbs; ub = 1.0, lb = 1.0)
-    # @test normalized_rhs(ubs[o2_index]) == 1.0
-    # @test normalized_rhs(lbs[o2_index]) == -1.0
+    cbm, v, mb, lbs, ubs = makeOptimizationModel(model, optimizer)
+    glucose_index = model[findfirst(model.reactions, "EX_glc__D_e")]
+    o2_index = model[findfirst(model.reactions, "EX_o2_e")]
+    atpm_index = model[findfirst(model.reactions, "ATPM")]
+    set_bound(glucose_index, lbs, ubs; ub = -1.0, lb = -1.0)
+    @test normalized_rhs(ubs[glucose_index]) == -1.0
+    @test normalized_rhs(lbs[glucose_index]) == 1.0
+    set_bound(o2_index, lbs, ubs; ub = 1.0, lb = 1.0)
+    @test normalized_rhs(ubs[o2_index]) == 1.0
+    @test normalized_rhs(lbs[o2_index]) == -1.0
 end
