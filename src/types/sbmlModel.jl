233dc92b01b8aca6e7ed8c8fb318c7c6124073de
Mirek Kratochvil <exa.exa@gmail.com>
2021-03-24 18:20:17 +0100

    SBML data are a model too, make the loading more flexible

---
+++ src/types/sbmlModel.jl
@@ -0,0 +1,58 @@
+"""
+    struct SBMLModel
+
+Thin wrapper around the model from SBML.jl library. Allows easy conversion from
+SBML to any other model format.
+"""
+struct SBMLModel <: MetabolicModel
+    m::SBML.Model
+end
+
+reactions(a::SBMLModel)::Vector{String} = [k for k in keys(a.m.reactions)]
+metabolites(a::SBMLModel)::Vector{String} = [k for k in keys(a.m.species)]
+nReactions(a::SBMLModel)::Int = length(a.m.reactions)
+nMetabolites(a::SBMLModel)::Int = length(a.m.species)
+
+"""
+    stoichiometry(a::SBMLModel)::SparseMat
+
+Recreate the stoichiometry matrix from the SBML model.
+"""
+function stoichiometry(a::SBMLModel)::SparseMat
+    _, _, S = SBML.getS(a.m)
+    return S
+end
+
+"""
+    bounds(a::SBMLModel)::Tuple{SparseVec,SparseVec}
+
+Get the lower and upper flux bounds of a `SBMLModel`. Throws `DomainError` in
+case if the SBML contains mismatching units.
+"""
+function bounds(a::SBMLModel)::Tuple{SparseVec,SparseVec}
+    lbu = SBML.getLBs(a.m)
+    ubu = SBML.getUBs(a.m)
+
+    unit = lbu[1][2]
+    getvalue = (val, _)::Tuple -> val
+    getunit = (_, unit)::Tuple -> unit
+
+    allunits = unique([getunit.(lbu) getunit.(ubu)])
+
+    if length(allunits) != 1
+        throw(
+            DomainError(
+                allunits,
+                "The SBML file uses multiple units; loading needs conversion",
+            ),
+        )
+    end
+
+    return (getvalue.(lbu), getvalue.(ubu))
+end
+
+balance(a::SBMLModel)::SparseVec = spzeros(nMetabolites(a))
+objective(a::SBMLModel)::SparseVec = SBML.getOCs(a.m)
+coupling(a::SBMLModel)::SparseMat = spzeros(0, nReactions(a))
+nCouplingConstraints(a::SBMLModel)::Int = 0
+couplingBounds(a::SBMLModel)::Tuple{SparseVec,SparseVec} = ([], [])
