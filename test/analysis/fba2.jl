6a1743ac5bf6048e530e0b088697ac713e653871
st elmo <stelmozors@gmail.com>
2021-03-30 15:19:11 +0200

    format fix

--- test/analysis/fba2.jl
+++ test/analysis/fba2.jl
@@ -1,27 +1,28 @@
 @testset "Flux balance analysis with CobraModel" begin
-    model = read_model(Downloads.download("http://bigg.ucsd.edu/static/models/e_coli_core.json", joinpath("data", "e_coli_core.json")))
+    model = read_model(
+        Downloads.download(
+            "http://bigg.ucsd.edu/static/models/e_coli_core.json",
+            joinpath("data", "e_coli_core.json"),
+        ),
+    )
     @test length(model.reactions) == 95 # read in correctly
 
     # FBA
     biomass = findfirst(model.reactions, "BIOMASS_Ecoli_core_w_GAM")
     cons = Dict("EX_glc__D_e" => (-12.0, -12.0))
     optimizer = Tulip.Optimizer # quiet by default
-    sol = fba(model, optimizer; objective_func=biomass, constraints = cons)
+    sol = fba(model, optimizer; objective_func = biomass, constraints = cons)
     pfl = findfirst(model.reactions, "PFL")
-    solmulti = fba(model, optimizer; objective_func=[biomass, pfl], weights = [0.8, 0.2]) # classic flux balance analysis
+    solmulti = fba(model, optimizer; objective_func = [biomass, pfl], weights = [0.8, 0.2]) # classic flux balance analysis
 
 
     flux_vec = [sol[rxn.id] for rxn in model.reactions]
     sol_mapped = map_fluxes(flux_vec, model)
-    @test isapprox(
-            sol_mapped["BIOMASS_Ecoli_core_w_GAM"],
-            1.0572509997013568,
-            atol = 1e-6,
-        )
-        @test isapprox(sol["BIOMASS_Ecoli_core_w_GAM"], 1.0572509997013568, atol = 1e-6)
-        @test !isempty(solmulti)
-    
-    sol = fba(model, optimizer; objective_func=biomass)
+    @test isapprox(sol_mapped["BIOMASS_Ecoli_core_w_GAM"], 1.0572509997013568, atol = 1e-6)
+    @test isapprox(sol["BIOMASS_Ecoli_core_w_GAM"], 1.0572509997013568, atol = 1e-6)
+    @test !isempty(solmulti)
+
+    sol = fba(model, optimizer; objective_func = biomass)
 
     # atom tracker
     atom_fluxes = atom_exchange(sol, model)
