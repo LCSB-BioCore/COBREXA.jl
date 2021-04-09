2f90e5cf8c53eb13f66120749ca508611c8b95c6
Mirek Kratochvil <exa.exa@gmail.com>
2021-04-09 13:00:03 +0200

    restyle

--- test/reconstruction/test_add_reactions_macro.jl
+++ test/reconstruction/test_add_reactions_macro.jl
@@ -8,10 +8,10 @@
     add!(mod, C)
 
     @add_reactions! mod begin
-            v1, ∅ ⟷ A
-            v2, ∅ ⟷ B, -500
-            v3, ∅ ⟷ C, -500, 500
-        end
+        v1, ∅ ⟷ A
+        v2, ∅ ⟷ B, -500
+        v3, ∅ ⟷ C, -500, 500
+    end
 
     rxn = mod.reactions[1]
     @test rxn.lb == -1000.0
@@ -20,7 +20,7 @@
     rxn = mod.reactions[2]
     @test rxn.lb == -500
     @test rxn.ub == 1000.0
-        
+
     rxn = mod.reactions[3]
     @test rxn.lb == -500
     @test rxn.ub == 500
