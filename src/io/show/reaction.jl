592f303c8027189ddca57e81077e5c9fbcfaf58f
Mirek Kratochvil <exa.exa@gmail.com>
2021-04-09 11:42:26 +0200

    break reaction into files

---
+++ src/io/show/reaction.jl
@@ -0,0 +1,61 @@
+
+"""
+Pretty printing of reaction::Reaction.
+"""
+function Base.show(io::IO, ::MIME"text/plain", r::Reaction)
+    if r.ub > 0.0 && r.lb < 0.0
+        arrow = " ⟷  "
+    elseif r.ub == 0.0 && r.lb < 0.0
+        arrow = " ⟵  "
+    elseif r.ub > 0.0 && r.lb == 0.0
+        arrow = " ⟶  "
+    else
+        arrow = " →∣←  " # blocked reaction
+    end
+    substrates = String[]
+    products = String[]
+    for (k, v) in r.metabolites
+        if v < 0.0
+            push!(substrates, string(abs(v)) * " " * k.id)
+        else
+            push!(products, string(abs(v)) * " " * k.id)
+        end
+    end
+    isempty(substrates) && (substrates = "∅")
+    isempty(products) && (products = "∅")
+
+    println(io, "Reaction ID: ", r.id)
+    println(io, "Reaction name: ", r.name)
+    println(io, "Reaction subsystem: ", r.subsystem)
+    if length(substrates) > 5 && length(products) > 5
+        sp = substrates[1] * " + ... + " * substrates[end]
+        pp = products[1] * " + ... + " * products[end]
+        println(io, sp * arrow * pp)
+    elseif length(substrates) > 5
+        sp = substrates[1] * " + ... + " * substrates[end]
+        println(io, sp * arrow * join(products, " + "))
+    elseif length(products) > 5
+        pp = products[1] * " + ... + " * products[end]
+        println(io, join(substrates, " + ") * arrow * pp)
+    else
+        println(io, join(substrates, " + ") * arrow * join(products, " + "))
+    end
+    println(io, "Lower bound: ", r.lb)
+    println(io, "Upper bound: ", r.ub)
+
+    grr_strings = String[]
+    for gr in r.grr
+        push!(grr_strings, "(" * join([g.id for g in gr], " and ") * ")")
+    end
+    grr_string = join(grr_strings, " or ")
+    (isnothing(grr_string) || grr_string == "") && (grr_string = "")
+    println(io, "Genes: ", grr_string)
+    println(io, "E.C. number: ", join(get(r.annotation, "ec-code", [""]), " or "))
+end
+
+"""
+Pretty printing of reactions::Vector{Reaction}.
+"""
+function Base.show(io::IO, ::MIME"text/plain", rs::Vector{Reaction})
+    println(io, "Reaction set of length: ", length(rs))
+end
---
+++ src/utils/reaction.jl
@@ -0,0 +1,109 @@
+"""
+    getindex(rxns::Vector{Reaction}, rxn::Reaction)
+
+Get the index of a reaction `rxn` in an array of reactions `rxns`, based in `id`.
+Return -1 if no matches found.
+
+Typically used, `index = rxns[rxn]`.
+"""
+function Base.getindex(rxns::Vector{Reaction}, rxn::Reaction)
+    for i in eachindex(rxns)
+        if rxns[i].id == rxn.id
+            return i
+        end
+    end
+    return -1
+end
+
+"""
+    findfirst(rxns::Vector{Reaction}, rxnid::String)
+
+Return the reaction with `rxnid` in `rxns` or else `nothing`.
+
+Typically used: `findfirst(model.rxns, rxnid)`.
+"""
+function Base.findfirst(rxns::Vector{Reaction}, rxnid::String)
+    for i in eachindex(rxns)
+        if rxns[i].id == rxnid
+            return rxns[i]
+        end
+    end
+    return nothing
+end
+
+"""
+    check_duplicate_reaction(rxns::Vector{Reaction}, rxn::Reaction)
+
+Check if `rxn` already exists in `rxns` but has another id.
+Looks through all the reaction equations of `rxns` and compares metabolite `id`s and their stoichiometric coefficients to those of `rxn`.
+If `rxn` has the same reaction equation as another reaction in `rxns`, the return true and the index of the first match.
+
+See also: [`is_mass_balanced`](@ref)
+"""
+function check_duplicate_reaction(rxns::Vector{Reaction}, crxn::Reaction)
+    ceq = Dict{String,Float64}(k.id => v for (k, v) in crxn.metabolites)
+    for rxn in rxns
+        if rxn.id != crxn.id
+            req = Dict{String,Float64}(k.id => v for (k, v) in rxn.metabolites)
+            if isempty(setdiff(collect(keys(ceq)), collect(keys(req)))) &&
+               isempty(setdiff(collect(keys(req)), collect(keys(ceq)))) # same metabolites
+                all([req[k] == v for (k, v) in ceq]) && (return true, rxns[rxn])
+            end
+        else
+            return true, rxns[rxn]
+        end
+    end
+    return false, -1
+end
+
+"""
+    check_duplicate_annotations(rxns::Vector{Reaction}, rxn::Gene)
+
+Determine if a `rxn` is has overlapping annotations in `rxns`.
+The annotations checked are: ["bigg.reaction", "biocyc", "ec-code", "kegg.reaction", "metanetx.reaction", "rhea", "sabiork", "seed.reaction"].
+Return true and the index of the first hit, otherwise false and -1.
+"""
+function check_duplicate_annotations(rxns::Vector{Reaction}, crxn::Reaction)
+    inspect_annotations = [
+        "bigg.reaction",
+        "biocyc",
+        "ec-code",
+        "kegg.reaction",
+        "metanetx.reaction",
+        "rhea",
+        "sabiork",
+        "seed.reaction",
+    ]
+    for rxn in rxns
+        for anno in inspect_annotations
+            if any([
+                x in get(crxn.annotation, anno, ["c1"]) for
+                x in get(rxn.annotation, anno, ["c2"])
+            ])
+                return true, rxns[rxn]
+            end
+        end
+    end
+    return false, -1
+end
+
+"""
+    is_mass_balanced(rxn::Reaction)
+
+Checks if `rxn` is atom balanced. Returns a boolean for whether the reaction is balanced,
+and the associated balance of atoms for convenience (useful if not balanced).
+
+See also: [`get_atoms`](@ref), [`check_duplicate_reaction`](@ref)
+"""
+function is_mass_balanced(rxn::Reaction)
+    atom_balances = Dict{String,Float64}() # float here because stoichiometry is not Int64
+    for (met, stoich) in rxn.metabolites
+        atoms = get_atoms(met)
+        isempty(atoms) && continue # ignore blanks
+        for (k, v) in atoms
+            atom_balances[k] = get(atom_balances, k, 0) + v * stoich
+        end
+    end
+
+    return all(sum(values(atom_balances)) == 0), atom_balances
+end
