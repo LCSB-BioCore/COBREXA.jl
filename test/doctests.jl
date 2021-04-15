753ef0d773bcceea6cdb7f6a1e31668a7294176c
laurentheirendt <laurent.heirendt@uni.lu>
2021-04-15 11:11:38 +0200

    module level setup

--- test/doctests.jl
+++ test/doctests.jl
@@ -1,4 +1,14 @@
 using Test, Documenter, COBREXA
+
+ex = quote
+    using COBREXA, Tulip
+    include(joinpath(dirname(pathof(COBREXA)), "..", "test", "data", "test_models.jl"))
+    model = test_LP()
+end
+
+# set module-level metadata
+DocMeta.setdocmeta!(COBREXA, :DocTestSetup, ex; recursive=true)
+
 @testset "Documentation tests" begin
-    doctest(COBREXA; manual = false)
+    doctest(COBREXA; manual = true)
 end
