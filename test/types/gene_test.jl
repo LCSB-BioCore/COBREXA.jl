9f4cbab27b758182b8b4b9dfb20bd99f755d1e31
St. Elmo <stelmozors@gmail.com>
2021-04-18 16:55:52 +0200

    fixed gene test

--- test/types/gene_test.jl
+++ test/types/gene_test.jl
@@ -9,27 +9,18 @@
 
     g2 = Gene("gene2")
 
-    genes = [g, g2]
-    @test sprint(show, MIME("text/plain"), genes) == "Gene set of length: 2\n"
-
-    gene_list = [[g], [g2]]
-    @test sprint(show, MIME("text/plain"), gene_list) == "(gene1) or (gene2)\n"
-
-    @test genes[g] == 1
-
-    gg = findfirst(genes, g2.id)
-    @test gg.id == g2.id
+    genelist = [g, g2]
+    @test sprint(show, MIME("text/plain"), genelist) == "Gene set of length: 2\n"
 
     g3 = Gene("g3")
     g3.annotation = Dict("ncbigene" => "sbo", "ncbigene" => ["ads", "asds"])
 
-    dup, ind = check_duplicate_annotations(genes, g3)
-    @test dup && ind == 1
-
-    @test isnothing(findfirst(genes, "nope"))
+    genes_d = OrderedDict(zip([g.id for g in genelist], genelist))
+    dup, ind = check_duplicate_annotations(g3, genes_d)
+    @test dup && ind == "gene1"
 
     g4 = Gene("g4")
     g4.annotation = Dict("ncbigene" => "sbo", "ncbigene" => ["ads22", "asd22s"])
-    dup, ind = check_duplicate_annotations(genes, g4)
-    @test !dup && ind == -1
+    dup, ind = check_duplicate_annotations(g4, genes_d)
+    @test !dup && ind == ""
 end
