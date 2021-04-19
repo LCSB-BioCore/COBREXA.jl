8f8dae1033a8402a8829f090a49ddf53633bda0a
Mirek Kratochvil <exa.exa@gmail.com>
2021-04-19 10:36:38 +0200

    support converting to SBMLModel
    It would be nice if we knew how to actually _write_ the SBML model back to
    disk. But that's a completely different story (and package).

--- src/types/SBMLModel.jl
+++ src/types/SBMLModel.jl
@@ -54,33 +54,58 @@ objective(a::SBMLModel)::SparseVec = SBML.getOCs(a.m)
 genes(a::SBMLModel)::Vector{String} = [k for k in a.m.gene_products]
 
 reaction_gene_association(a::SBMLModel, rid::String)::Maybe{GeneAssociation} =
-    mapmaybe(_parse_grr, a.m.reactions[rid].grr)
+    maybemap(_parse_grr, a.m.reactions[rid].gene_product_association)
 
-metabolite_chemistry(a::SBMLModel, mid::String)::Maybe{MetaboliteChemistry} = mapmaybe(
-    (fs) -> (_formula_to_atoms(fs), default(0, m.species[mid].charge)),
-    m.species[mid].formula,
+metabolite_chemistry(a::SBMLModel, mid::String)::Maybe{MetaboliteChemistry} = maybemap(
+    (fs) -> (_formula_to_atoms(fs), default(0, a.m.species[mid].charge)),
+    a.m.species[mid].formula,
 )
 
-function Base.convert(::Type{SBMLModel}, m::MetabolicModel)::SBMLModel
+function Base.convert(::Type{SBMLModel}, m::MetabolicModel)
+    mets = metabolites(m)
+    met_id = Dict([mid => i for (i, mid) in enumerate(mets)])
+    rxns = reactions(m)
+    stoi = stoichiometry(m)
+    (lbs, ubs) = bounds(m)
+    ocs = objective(m)
 
     return SBMLModel(
-        Dict(), # parameters
-        Dict("" => []), # units
-        Dict([
-            "" => SBML.Compartment(nothing, nothing, nothing, nothing, nothing, nothing),
-        ]),
-        Dict(
-            [mid => SBML.Species(
-                nothing,
-                "",
-                nothing,
-                mapmaybe((x, _) -> _dict_to_formula(x), metabolite_chemistry(m, mid)),
-                mapmaybe((_, x) -> x, metabolite_chemistry(m, mid)),
-                nothing,
-                nothing,
-            ) for mid in metabolites(m)],
+        SBML.Model(
+            Dict(), # parameters
+            Dict("" => []), # units
+            Dict([
+                "" =>
+                    SBML.Compartment(nothing, nothing, nothing, nothing, nothing, nothing),
+            ]),
+            Dict(
+                [mid => SBML.Species(
+                    nothing, # name
+                    "", # compartment
+                    nothing, # no information about boundary conditions
+                    maybemap((x, _) -> _dict_to_formula(x), metabolite_chemistry(m, mid)),
+                    maybemap((_, x) -> x, metabolite_chemistry(m, mid)),
+                    nothing, # initial amount
+                    nothing, # only substance unit flags
+                ) for mid in metabolites(m)],
+            ),
+            Dict(
+                [rxns[ri] => SBML.Reaction(
+                    Dict(
+                        [mets[i] => stoi[i, ri] for
+                         i in SparseArrays.nonzeroinds(stoi[:, ri])],
+                    ),
+                    (lbs[ri], ""),
+                    (ubs[ri], ""),
+                    ocs[ri],
+                    maybemap(
+                        x -> _unparse_grr(SBML.GeneProductAssociation, x),
+                        reaction_gene_association(m, rxns[ri]),
+                    ),
+                    nothing,
+                ) for ri = 1:length(rxns)],
+            ),
+            Dict([gid => SBML.GeneProduct(nothing, nothing) for gid in genes(m)]),
+            Dict(), # function definitions
         ),
-        Dict([rid => SBML.Reaction(nothing) for rid in reactions(m)]),
-        #TODO
     )
 end
