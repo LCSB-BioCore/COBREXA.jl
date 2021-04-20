83b2a7ca2139c627200b535a70f374dca425f152
St. Elmo <stelmozors@gmail.com>
2021-04-20 22:31:41 +0200

    added metabolite pp

--- src/io/show/Metabolite.jl
+++ src/io/show/Metabolite.jl
@@ -3,15 +3,21 @@
 Pretty printing of metabolite::Metabolite.
 """
 function Base.show(io::IO, ::MIME"text/plain", m::Metabolite)
-    println(
-        io,
-        "Metabolite ID: $(m.id)\nMetabolite name: $(m.name)\nFormula: $(m.formula)\nCharge: $(m.charge)",
-    )
+    _pretty_print(io, "Metabolite ID: ", m.id)
+    _pretty_print(io, "Name: ", m.name)
+    _pretty_print(io, "Formula: ", m.formula)
+    _pretty_print(io, "Charge: ", string(m.charge))
+    _pretty_print(io, "Compartment: ", string(m.compartment))
+    _pretty_print(io, "Notes: ", m.notes)
+    _pretty_print(io, "Annotation: ", m.annotation)
+    _pretty_print(io, "Fields: ", join([string(x) for x in fieldnames(Metabolite)],", "))
 end
 
 """
 Pretty printing of metabolites::Vector{Metabolite}.
 """
 function Base.show(io::IO, ::MIME"text/plain", ms::Vector{Metabolite})
-    println(io, "Metabolite set of length: ", length(ms))
+    _pretty_print(io, "Metabolite vector of length: : ", string(length(ms)))
+    _pretty_print(io, "Each metabolite has fields: ", join([string(x) for x in fieldnames(Metabolite)],", "))
+    
 end
--- test/types/metabolite_test.jl
+++ test/types/metabolite_test.jl
@@ -8,8 +8,8 @@
     m1.notes = Dict("notes" => ["blah", "blah"])
     m1.annotation = Dict("sboterm" => ["sbo"], "kegg.compound" => ["ads", "asds"])
 
-    @test sprint(show, MIME("text/plain"), m1) ==
-          "Metabolite ID: met1\nMetabolite name: metabolite 1\nFormula: C6H12O6N\nCharge: 1\n"
+    @test sprint(show, MIME("text/plain"), m1) == "\e[34mMetabolite ID: \e[35mmet1\n\e[34mName: \e[35mmetabolite 1\n\e[34mFormula: \e[35mC6H12O6N\n\e[34mCharge: \e[35m1\n\e[34mCompartment: \e[35mc\n\e[34mNotes: 
+    \n\e[35m\tnotes: blah, blah\n\e[34mAnnotation: \n\e[35m\tkegg.compound: ads, asds\n\e[35m\tsboterm: sbo\n\e[34mFields: \e[35mid, name, formula, charge, compartment, notes, annotation\n"
 
     m2 = Metabolite("met2")
     m2.formula = "C6H12O6N"
@@ -20,7 +20,7 @@
 
     mets = [m1, m2, m3]
 
-    @test sprint(show, MIME("text/plain"), mets) == "Metabolite set of length: 3\n"
+    @test sprint(show, MIME("text/plain"), mets) == "\e[34mMetabolite vector of length: : \e[35m3\n\e[34mEach metabolite has fields: \e[35mid, name, formula, charge, compartment, notes, annotation\n"
 
     md = OrderedDict(m.id => m for m in mets)
     id = check_duplicate_annotations(m3, md)
