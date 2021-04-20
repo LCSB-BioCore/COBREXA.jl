0f3559443fa492151a689cfeb2b001323c94d582
St. Elmo <stelmozors@gmail.com>
2021-04-20 21:39:37 +0200

    added pretty printing of MetabolicModel

--- src/io/show/MetabolicModel.jl
+++ src/io/show/MetabolicModel.jl
@@ -3,14 +3,22 @@
 Pretty printing of model::MetabolicModel using generic interface.
 """
 function Base.show(io::IO, ::MIME"text/plain", m::MetabolicModel)
-    println(Crayon(foreground=:cyan), "Metabolic model: [INSERT ID]")
-    println(Crayon(foreground=:blue), stoichiometry(m))
+    println(Crayon(foreground=:cyan), "Metabolic model of type $(typeof(m))")
+    
+    print(Crayon(foreground=:blue), "Model ID: ")
+    print(Crayon(foreground=:magenta), "Use generic function to get ID")
+
+    if prod(size(stoichiometry(m))) < 5_000_000
+        println(Crayon(foreground=:blue), stoichiometry(m))
+    else # too big to display nicely
+        println(Crayon(foreground=:blue), "S × v = b")    
+    end
     
     print(Crayon(foreground=:blue), "Number of reactions: ")
     println(Crayon(foreground=:magenta), n_reactions(m))
     
     print(Crayon(foreground=:blue), "Number of metabolites: ")
-    println(Crayon(foreground=:magenta),  n_reactions(m))
+    println(Crayon(foreground=:magenta),  n_metabolites(m))
 
     print(Crayon(foreground=:blue), "Field names associated with this model include: ")
     print(Crayon(foreground=:magenta),join([string(x) for x in fieldnames(typeof(m))], ", "))
