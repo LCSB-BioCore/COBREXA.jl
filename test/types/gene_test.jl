c756e6d7bb13990e26c24b83bd3e77c87da7f4e7
St. Elmo <stelmozors@gmail.com>
2021-04-20 22:25:43 +0200

    updated gene tests

--- test/types/gene_test.jl
+++ test/types/gene_test.jl
@@ -5,12 +5,20 @@
     g.notes = Dict("notes" => ["blah", "blah"])
     g.annotation = Dict("sboterm" => ["sbo"], "ncbigene" => ["ads", "asds"])
 
-    @test sprint(show, MIME("text/plain"), g) == "Gene ID: gene1\nGene name: gene_name\n"
+    @test sprint(show, MIME("text/plain"), g) == "\e[34mGene ID: \e[35mgene1\n\e[34mName: \e[35mgene_name\n\e[34mNotes: \n\e[35m\tnotes: blah, blah\n\e[34mAnnotation: \n\e[35m\tncbigene: ads, asds\n\e[35m\tsboterm: sbo\n\e[34mFields: \e[35mid, name, notes, annotation\n"
 
     g2 = Gene("gene2")
 
     genes = [g, g2]
-    @test sprint(show, MIME("text/plain"), genes) == "Gene set of length: 2\n"
+    @test sprint(show, MIME("text/plain"), genes) == "\e[34mGene vector of length: \e[35m2\n\e[34mEach gene has fields: \e[35mid, name, notes, annotation\n"
+
+    gene_list = [[g], [g2]]
+    @test sprint(show, MIME("text/plain"), gene_list) == "\e[34mGene reaction rule: \e[35m(gene1 and gene2) or (gene2)\n"
+
+    @test genes[g] == 1
+
+    gg = findfirst(genes, g2.id)
+    @test gg.id == g2.id
 
     g3 = Gene("g3")
     g3.annotation = Dict("ncbigene" => ["sbo"], "ncbigene" => ["ads", "asds"])
