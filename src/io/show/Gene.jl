de15aaa79721a24d9fb1479c45b9df697b74c946
St. Elmo <stelmozors@gmail.com>
2021-04-20 22:19:38 +0200

    gene pretty printing

--- src/io/show/Gene.jl
+++ src/io/show/Gene.jl
@@ -2,15 +2,19 @@
 Pretty printing of `Gene`.
 """
 function Base.show(io::IO, ::MIME"text/plain", g::Gene)
-    println(io, "Gene ID: ", g.id)
-    println(io, "Gene name: ", g.name)
+    _pretty_print(io, "Gene ID: ", g.id)
+    _pretty_print(io, "Name: ", g.name)
+    _pretty_print(io, "Notes: ", g.notes)
+    _pretty_print(io, "Annotation: ", g.annotation)
+    _pretty_print(io, "Fields: ", join([string(x) for x in fieldnames(Gene)],", "))
 end
 
 """
 Pretty printing of `Vector{Gene}`.
 """
 function Base.show(io::IO, ::MIME"text/plain", gs::Vector{Gene})
-    println(io, "Gene set of length: ", length(gs))
+    _pretty_print(io, "Gene vector of length: ", string(length(gs)))
+    _pretty_print(io, "Each gene has fields: ", join([string(x) for x in fieldnames(Gene)], ", "))
 end
 
 """
@@ -21,5 +25,5 @@ function Base.show(io::IO, ::MIME"text/plain", grr::Vector{Vector{Gene}})
     for gr in grr
         push!(grr_strings, "(" * join([g.id for g in gr], " and ") * ")")
     end
-    println(io, join(grr_strings, " or "))
+    _pretty_print(io, "Gene reaction rule: ", join(grr_strings, " or "))
 end
