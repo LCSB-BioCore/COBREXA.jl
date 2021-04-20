a975c93753b37104ace9a67ca640f3021f136df3
St. Elmo <stelmozors@gmail.com>
2021-04-20 21:42:45 +0200

    added io to MetabolicModel show

--- src/io/show/MetabolicModel.jl
+++ src/io/show/MetabolicModel.jl
@@ -3,23 +3,23 @@
 Pretty printing of model::MetabolicModel using generic interface.
 """
 function Base.show(io::IO, ::MIME"text/plain", m::MetabolicModel)
-    println(Crayon(foreground=:cyan), "Metabolic model of type $(typeof(m))")
+    println(io, Crayon(foreground=:cyan), "Metabolic model of type $(typeof(m))")
     
-    print(Crayon(foreground=:blue), "Model ID: ")
-    print(Crayon(foreground=:magenta), "Use generic function to get ID")
+    print(io, Crayon(foreground=:blue), "Model ID: ")
+    print(io, Crayon(foreground=:magenta), "Use generic function to get ID")
 
     if prod(size(stoichiometry(m))) < 5_000_000
-        println(Crayon(foreground=:blue), stoichiometry(m))
+        println(io, Crayon(foreground=:blue), stoichiometry(m))
     else # too big to display nicely
-        println(Crayon(foreground=:blue), "S × v = b")    
+        println(io, Crayon(foreground=:blue), "S × v = b")    
     end
     
-    print(Crayon(foreground=:blue), "Number of reactions: ")
-    println(Crayon(foreground=:magenta), n_reactions(m))
+    print(io, Crayon(foreground=:blue), "Number of reactions: ")
+    println(io, Crayon(foreground=:magenta), n_reactions(m))
     
-    print(Crayon(foreground=:blue), "Number of metabolites: ")
-    println(Crayon(foreground=:magenta),  n_metabolites(m))
+    print(io, Crayon(foreground=:blue), "Number of metabolites: ")
+    println(io, Crayon(foreground=:magenta),  n_metabolites(m))
 
-    print(Crayon(foreground=:blue), "Field names associated with this model include: ")
-    print(Crayon(foreground=:magenta),join([string(x) for x in fieldnames(typeof(m))], ", "))
+    print(io, Crayon(foreground=:blue), "Field names associated with this model include: ")
+    print(io, Crayon(foreground=:magenta),join([string(x) for x in fieldnames(typeof(m))], ", "))
 end
