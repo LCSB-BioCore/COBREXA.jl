3984d2b57c15405f4b27daaa90f932d2fda73529
St. Elmo <stelmozors@gmail.com>
2021-04-20 22:23:35 +0200

    updated metabolicmodel pp

--- src/io/show/MetabolicModel.jl
+++ src/io/show/MetabolicModel.jl
@@ -4,9 +4,7 @@ Pretty printing of model::MetabolicModel using generic interface.
 """
 function Base.show(io::IO, ::MIME"text/plain", m::MetabolicModel)
     println(io, Crayon(foreground=:cyan), "Metabolic model of type $(typeof(m))")
-    
-    print(io, Crayon(foreground=:blue), "Model ID: ")
-    print(io, Crayon(foreground=:magenta), "Use generic function to get ID")
+    _pretty_print(io, "Model ID: ", "Use generic function to get ID")
 
     if prod(size(stoichiometry(m))) < 5_000_000
         println(io, Crayon(foreground=:blue), stoichiometry(m))
@@ -14,12 +12,7 @@ function Base.show(io::IO, ::MIME"text/plain", m::MetabolicModel)
         println(io, Crayon(foreground=:blue), "S × v = b")    
     end
     
-    print(io, Crayon(foreground=:blue), "Number of reactions: ")
-    println(io, Crayon(foreground=:magenta), n_reactions(m))
-    
-    print(io, Crayon(foreground=:blue), "Number of metabolites: ")
-    println(io, Crayon(foreground=:magenta),  n_metabolites(m))
-
-    print(io, Crayon(foreground=:blue), "Field names associated with this model include: ")
-    print(io, Crayon(foreground=:magenta),join([string(x) for x in fieldnames(typeof(m))], ", "))
+    _pretty_print(io, "Number of reactions: ", string(n_reactions(m)))
+    _pretty_print(io, "Number of metabolites: ", string(n_metabolites(m)))
+    _pretty_print(io, "Field names associated with this model include: ", join([string(x) for x in fieldnames(typeof(m))], ", "))
 end
