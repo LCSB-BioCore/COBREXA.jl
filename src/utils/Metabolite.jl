7a7cbc2eb192fe705652f804d218f79a147a92ba
St. Elmo <stelmozors@gmail.com>
2021-04-22 13:08:49 +0200

    fix utils

--- src/utils/Metabolite.jl
+++ src/utils/Metabolite.jl
@@ -1,50 +1,17 @@
-
 """
-    getindex(mets::Vector{Metabolite}, met::Metabolite)
+    check_duplicate_annotations(met::Metabolite, mets::Vector{Metabolite})
 
-Get the index of a `met` in an array of `mets`, based on `id` equality.
-Return -1 if no matches found.
-This function overrides the `[]` notation from Base.
-
-Typically used: `index = mets[met]` works.
-"""
-function Base.getindex(mets::Vector{Metabolite}, met::Metabolite)
-    for i in eachindex(mets)
-        if mets[i].id == met.id
-            return i
-        end
-    end
-    return -1
-end
-
-"""
-    findfirst(mets::Vector{Metabolite}, metid::String)
-
-Return the metabolite in `mets` with `metid`, based in `id` field.
-If nothing matches, return `nothing`.
-
-Typically used: `met = findfirst(model.mets, metid)`.
-"""
-function Base.findfirst(mets::Vector{Metabolite}, metid::String)
-    for i in eachindex(mets)
-        if mets[i].id == metid
-            return mets[i]
-        end
-    end
-    return nothing
-end
-
-"""
-    check_duplicate_annotations(mets::Vector{Metabolite}, met::Metabolite)
-
-Determine if a metabolite `met` has overlapping annotations in for some metabolite in `mets`.
+Check if a metabolite `met` has overlapping annotations with metabolites in `mets`.
 If the annotations overlap, then check if they share a compartment to determine if it a a true duplicate.
 The annotations checked are: ["kegg.compound", "bigg.metabolite", "chebi", "inchi_key", "sabiork", "hmdb", "seed.compound", "metanetx.chemical", "reactome.compound", "biocyc"].
 Return true and the index of the first hit, otherwise false and -1.
 
 See also: [`check_same_formula`](@ref), [`get_atoms`](@ref)
 """
-function check_duplicate_annotations(mets::Vector{Metabolite}, cmet::Metabolite)
+function check_duplicate_annotations(
+    cmet::Metabolite,
+    mets::OrderedDict{String,Metabolite},
+)::Tuple{Bool,String}
     inspect_annotations = [
         "kegg.compound",
         "bigg.metabolite",
@@ -57,39 +24,21 @@ function check_duplicate_annotations(mets::Vector{Metabolite}, cmet::Metabolite)
         "reactome.compound",
         "biocyc",
     ]
-    catoms = get_atoms(cmet)
-    for met in mets
-        for anno in inspect_annotations
-            if any([
-                x in get(cmet.annotation, anno, ["c1"]) for
-                x in get(met.annotation, anno, ["c2"])
-            ])
-                if met.compartment == cmet.compartment
-                    return true, mets[met]
+    for (k, met) in mets
+        if met.compartment == cmet.compartment # check if same compartment
+            for anno in inspect_annotations
+                if length(
+                    intersect(
+                        get(met.annotation, anno, ["c1"]),
+                        get(cmet.annotation, anno, ["c2"]),
+                    ),
+                ) != 0
+                    return true, k
                 end
             end
         end
     end
-    return false, -1
-end
-
-"""
-    check_same_formula(mets::Vector{Metabolite}, met::Metabolite)
-
-Return all metabolites in `mets` that have the same formula as `met`.
-Formula similarity is determined using a hash function, not with [`get_atoms`](@ref).
-
-See also: [`check_duplicate_annotations`](@ref), [`get_atoms`](@ref)
-"""
-function check_same_formula(mets::Vector{Metabolite}, met::Metabolite)
-    inds = []
-    met_formula_hash = hash(met.formula)
-    for (i, m) in enumerate(mets)
-        if met_formula_hash == hash(m.formula)
-            push!(inds, i)
-        end
-    end
-    return mets[inds]
+    return false, ""
 end
 
 """
@@ -99,4 +48,14 @@ Return a dictionary mapping the elements in a metabolite `met` to their stoichio
 
 See also: [`check_duplicate_annotations`](@ref), [`check_same_formula`](@ref)
 """
-get_atoms(met::Metabolite) = _formula_to_atoms(met.formula)
+function get_atoms(met::Metabolite)
+    atoms = Dict{String,Int}()
+    length(met.formula) == 0 && return atoms
+    for m in eachmatch(r"([A-Z]{1})([a-z]?)(\d*)", met.formula)
+        element = match(r"([A-Z]{1})([a-z]?)", m.match)
+        number = match(r"\d\d*", m.match)
+        atoms[string(element.match)] =
+            isnothing(number) ? 1 : parse(Int, string(number.match))
+    end
+    return atoms
+end
--- src/utils/Reaction.jl
+++ src/utils/Reaction.jl
@@ -1,36 +1,43 @@
 """
-    check_duplicate_reaction(rxns::Vector{Reaction}, rxn::Reaction)
+    check_duplicate_reaction(rxn::Reaction, rxns::Dict{String, Reaction})
 
 Check if `rxn` already exists in `rxns` but has another id.
-Looks through all the reaction equations of `rxns` and compares metabolite `id`s and their stoichiometric coefficients to those of `rxn`.
-If `rxn` has the same reaction equation as another reaction in `rxns`, the return true and the index of the first match.
+Looks through all the reaction equations of `rxns` and compares metabolite `id`s 
+and their stoichiometric coefficients to those of `rxn`.
+If `rxn` has the same reaction equation as another reaction in `rxns`, the return true and id.
+Otherwise return false and "".
 
 See also: [`is_mass_balanced`](@ref)
 """
-function check_duplicate_reaction(rxns::Vector{Reaction}, crxn::Reaction)
-    ceq = Dict{String,Float64}(k.id => v for (k, v) in crxn.metabolites)
-    for rxn in rxns
-        if rxn.id != crxn.id
-            req = Dict{String,Float64}(k.id => v for (k, v) in rxn.metabolites)
-            if isempty(setdiff(collect(keys(ceq)), collect(keys(req)))) &&
-               isempty(setdiff(collect(keys(req)), collect(keys(ceq)))) # same metabolites
-                all([req[k] == v for (k, v) in ceq]) && (return true, rxns[rxn])
+function check_duplicate_reaction(crxn::Reaction, rxns::OrderedDict{String,Reaction})
+    for (k, rxn) in rxns
+        if rxn.id != crxn.id # skip if same ID
+            reaction_checker = true
+            for (kk, vv) in rxn.metabolites # get reaction stoich
+                if get(crxn.metabolites, kk, 0) != vv # if at least one stoich doesn't match
+                    reaction_checker = false
+                    break
+                end
             end
-        else
-            return true, rxns[rxn]
+            if reaction_checker
+                return true, k
+            end
         end
     end
-    return false, -1
+    return false, ""
 end
 
 """
-    check_duplicate_annotations(rxns::Vector{Reaction}, rxn::Gene)
+    check_duplicate_annotations(rxn::Reaction, rxns::Dict{String, Reaction})
 
 Determine if a `rxn` is has overlapping annotations in `rxns`.
 The annotations checked are: ["bigg.reaction", "biocyc", "ec-code", "kegg.reaction", "metanetx.reaction", "rhea", "sabiork", "seed.reaction"].
-Return true and the index of the first hit, otherwise false and -1.
+Return true and the `id` of the first hit, otherwise false and "".
 """
-function check_duplicate_annotations(rxns::Vector{Reaction}, crxn::Reaction)
+function check_duplicate_annotations(
+    crxn::Reaction,
+    rxns::OrderedDict{String,Reaction},
+)::Tuple{Bool,String}
     inspect_annotations = [
         "bigg.reaction",
         "biocyc",
@@ -41,36 +48,28 @@ function check_duplicate_annotations(rxns::Vector{Reaction}, crxn::Reaction)
         "sabiork",
         "seed.reaction",
     ]
-    for rxn in rxns
+    for (k, rxn) in rxns
         for anno in inspect_annotations
-            if any([
-                x in get(crxn.annotation, anno, ["c1"]) for
-                x in get(rxn.annotation, anno, ["c2"])
-            ])
-                return true, rxns[rxn]
+            if length(
+                intersect(
+                    get(crxn.annotation, anno, ["c1"]),
+                    get(rxn.annotation, anno, ["c2"]),
+                ),
+            ) != 0
+                return true, k
             end
         end
     end
-    return false, -1
+    return false, ""
 end
 
 """
-    is_mass_balanced(rxn::Reaction)
+    is_boundary(rxn::Reaction)
 
-Checks if `rxn` is atom balanced. Returns a boolean for whether the reaction is balanced,
-and the associated balance of atoms for convenience (useful if not balanced).
-
-See also: [`get_atoms`](@ref), [`check_duplicate_reaction`](@ref)
+Return true if reaction is a boundary reaction, otherwise return false.
+Checks if boundary by inspecting number of metabolites in reaction equation. 
+Boundary reactions have only one metabolite, e.g. an exchange reaction, or a sink/demand reaction. 
 """
-function is_mass_balanced(rxn::Reaction)
-    atom_balances = Dict{String,Float64}() # float here because stoichiometry is not Int
-    for (met, stoich) in rxn.metabolites
-        atoms = get_atoms(met)
-        isempty(atoms) && continue # ignore blanks
-        for (k, v) in atoms
-            atom_balances[k] = get(atom_balances, k, 0) + v * stoich
-        end
-    end
-
-    return all(sum(values(atom_balances)) == 0), atom_balances
+function is_boundary(rxn::Reaction)::Bool
+    length(keys(rxn)) == 1 ? true : false
 end
