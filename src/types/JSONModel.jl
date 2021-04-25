394d3b94098de81a32cd24987bbf909c0dcdde7b
Mirek Kratochvil <exa.exa@gmail.com>
2021-04-25 09:40:26 +0200

    move key-guessing to utils

--- src/types/JSONModel.jl
+++ src/types/JSONModel.jl
@@ -22,28 +22,6 @@ struct JSONModel <: MetabolicModel
     m::Dict{String,Any}
 end
 
-
-"""
-    _guesskey(ks, possibilities)
-
-Unfortunately, JSON models do not have standardized field names, so we need to
-try a few possibilities and guess the best one.  The keys used to look for
-valid field names are specified in `src/base/constants.jl`.
-"""
-function _guesskey(avail, possibilities)
-    x = intersect(possibilities, avail)
-
-    if isempty(x)
-        @debug "could not find any of keys: $possibilities"
-        return nothing
-    end
-
-    if length(x) > 1
-        @debug "Possible ambiguity between keys: $x"
-    end
-    return x[1]
-end
-
 """
     reactions(model::JSONModel)
 
