6385cd9c74204f97ab7e357674d777adc74a44e1
Mirek Kratochvil <exa.exa@gmail.com>
2021-04-26 14:21:08 +0200

    add docs, remove the unused functions

--- src/types/MATModel.jl
+++ src/types/MATModel.jl
@@ -10,6 +10,11 @@ end
 n_metabolites(m::MATModel)::Int = size(m.mat["S"], 1)
 n_reactions(m::MATModel)::Int = size(m.mat["S"], 2)
 
+"""
+    reactions(m::MATModel)::Vector{String}
+
+Extracts reaction names from `rxns` key in the MAT file.
+"""
 function reactions(m::MATModel)::Vector{String}
     if haskey(m.mat, "rxns")
         reshape(m.mat["rxns"], n_reactions(m))
@@ -18,6 +23,11 @@ function reactions(m::MATModel)::Vector{String}
     end
 end
 
+"""
+    metabolites(m::MATModel)::Vector{String}
+
+Extracts metabolite names from `mets` key in the MAT file.
+"""
 function metabolites(m::MATModel)::Vector{String}
     if haskey(m.mat, "mets")
         reshape(m.mat["mets"], n_metabolites(m))
@@ -26,23 +36,44 @@ function metabolites(m::MATModel)::Vector{String}
     end
 end
 
+"""
+    stoichiometry(m::MATModel)
+
+Extract the stoichiometry matrix, stored under key `S`.
+"""
 stoichiometry(m::MATModel) = sparse(m.mat["S"])
 
+"""
+    bounds(m::MATModel)
+
+Extracts bounds from the MAT file, saved under `lb` and `ub`.
+"""
 bounds(m::MATModel) = (
     sparse(reshape(get(m.mat, "lb", fill(-Inf, n_reactions(m), 1)), n_reactions(m))),
     sparse(reshape(get(m.mat, "ub", fill(Inf, n_reactions(m), 1)), n_reactions(m))),
 )
 
+"""
+    balance(m::MATModel)
+
+Extracts balance from the MAT model, defaulting to zeroes if not present.
+"""
 balance(m::MATModel) =
     sparse(reshape(get(m.mat, "b", zeros(n_metabolites(m), 1)), n_metabolites(m)))
 
+"""
+    objective(m::MATModel)
+
+Extracts the objective from the MAT model (defaults to zeroes).
+"""
 objective(m::MATModel) =
     sparse(reshape(get(m.mat, "c", zeros(n_reactions(m), 1)), n_reactions(m)))
 
-coupling(a::MATModel)::SparseMat = spzeros(0, n_reactions(a))
-n_coupling_constraints(a::MATModel)::Int = 0
-coupling_bounds(a::MATModel)::Tuple{SparseVec,SparseVec} = (spzeros(0), spzeros(0))
+"""
+    Base.convert(::Type{MATModel}, m::MetabolicModel)
 
+Convert any metabolic model to `MATModel`.
+"""
 function Base.convert(::Type{MATModel}, m::MetabolicModel)
     lb, ub = bounds(m)
     nr = n_reactions(m)
