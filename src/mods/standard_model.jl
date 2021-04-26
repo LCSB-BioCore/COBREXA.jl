7d85ce5d92bbf369c3e544291dffa936d8612692
St. Elmo <stelmozors@gmail.com>
2021-04-26 12:03:17 +0200

    fix mods for stmodel

--- src/mods/standard_model.jl
+++ src/mods/standard_model.jl
@@ -1,49 +1,45 @@
 """
-    change_constraint(reaction::Reaction, lb, ub)
+    change_constraint(id::String, lb, ub)
 
-Change the lower and upper bounds (`lb` and `ub` respectively) of `reaction`.
+Change the lower and upper bounds (`lb` and `ub` respectively) of reaction `id`.
 """
-function change_constraint(reaction::Reaction, lb, ub)
+function change_constraint(id::String, lb, ub)
     (model, opt_model) -> begin
-        ind = index_of(reaction.id, reactions(model))
+        ind = first(indexin([id], reactions(model)))
         set_bound(ind, opt_model, lb = lb, ub = ub)
     end
 end
 
 """
-    change_objective(objective_functions::Union{Reaction, Vector{Reaction}}; weights=[], sense=MOI.MAX_SENSE)
+    change_objective(objective_functions::Union{String,Vector{String}}; weights=[], sense=MOI.MAX_SENSE)
 
 Callback function to change the objective function used in a constraint based analysis function. 
-`objective_functions` can be a single reaction or an array of reactions (of type `Reaction`).
+`objective_functions` can be a single reaction or an array of reactions (input their string `id`s).
 Optionally specify their `weights` and the sense of the new objective (`MOI.MAX_SENSE`, `MOI.MIN_SENSE`).
 Note, this function sets the sense of the objective to `MOI.MAX_SENSE` by default if not specified.
 """
 function change_objective(
-    objective_functions::Union{Reaction,Vector{Reaction}};
+    objective_functions::Union{String,Vector{String}};
     weights = [],
     sense = MOI.MAX_SENSE,
 )
     (model, opt_model) -> begin
 
         # Construct objective_indices array
-        if typeof(objective_functions) == Reaction
-            objective_indices = [index_of(objective_functions.id, reactions(model))]
+        if typeof(objective_functions) == String
+            objective_indices = [first(indexin([objective_functions], reactions(model)))]
         else
             objective_indices =
-                [index_of(rxn.id, reactions(model)) for rxn in objective_functions]
+                [first(indexin([rxnid], reactions(model))) for rxn in objective_functions]
         end
 
         # Initialize weights
-        opt_weights = zeros(length(model.reactions))
+        opt_weights = spzeros(length(model.reactions))
 
         isempty(weights) && (weights = ones(length(objective_indices))) # equal weights
 
-        wcounter = 1
-        for i = 1:length(model.reactions)
-            if i in objective_indices
-                opt_weights[i] = weights[wcounter]
-                wcounter += 1
-            end
+        for (j, i) in enumerate(objective_indices)
+            opt_weights[i] = weights[j]
         end
 
         v = opt_model[:x]
