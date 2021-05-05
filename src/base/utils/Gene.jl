10a2f4084c155fb8b57c83b5eaf6fbbf86014f50
St. Elmo <stelmozors@gmail.com>
2021-05-05 09:52:37 +0200

    make duplication check not check if ids the same

--- src/base/utils/Gene.jl
+++ src/base/utils/Gene.jl
@@ -11,14 +11,16 @@ function check_duplicate_annotations(
     inspect_annotations = _constants.gene_annotation_checks,
 )::Union{Nothing,String}
     for (k, gene) in gs
-        for anno in inspect_annotations
-            if length(
-                intersect(
-                    get(gene.annotations, anno, ["c1"]),
-                    get(check_gene.annotations, anno, "c2"),
-                ),
-            ) != 0
-                return k
+        if k != check_gene.id
+            for anno in inspect_annotations
+                if length(
+                    intersect(
+                        get(gene.annotations, anno, ["c1"]),
+                        get(check_gene.annotations, anno, "c2"),
+                    ),
+                ) != 0
+                    return k
+                end
             end
         end
     end
