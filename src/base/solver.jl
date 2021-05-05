1ce621f859d28a8cbdaf48193b45a8a81f0b19e7
Mirek Kratochvil <exa.exa@gmail.com>
2021-05-05 17:25:35 +0200

    clean solver

--- src/base/solver.jl
+++ src/base/solver.jl
@@ -1,12 +1,19 @@
+
 """
+function make_optimization_model(
+    model::MetabolicModel,
+    optimizer;
+    sense = MOI.MAX_SENSE,
+)
+
 Convert CoreModel to the JuMP model, place objectives and the equality
 constraint.
 """
 function make_optimization_model(
-    model::MM,
+    model::MetabolicModel,
     optimizer;
     sense = MOI.MAX_SENSE,
-) where {MM<:MetabolicModel}
+)
     m, n = size(stoichiometry(model))
     xl, xu = bounds(model)
 
@@ -21,13 +28,19 @@ function make_optimization_model(
 end
 
 """
+    optimize_model(
+        model::MetabolicModel,
+        optimizer;
+        sense = MOI.MIN_SENSE,
+    )
+
 Use JuMP to solve an instance of CoreModel
 """
 function optimize_model(
-    model::LM,
+    model::MetabolicModel,
     optimizer;
     sense = MOI.MIN_SENSE,
-) where {LM<:MetabolicModel}
+)
     optimization_model = make_optimization_model(model, optimizer; sense = sense)
     COBREXA.JuMP.optimize!(optimization_model)
     return optimization_model
