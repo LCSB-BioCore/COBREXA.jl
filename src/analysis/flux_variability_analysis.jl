3dc0251e69131aaf8fec60de2fed022a5a14d727
Mirek Kratochvil <exa.exa@gmail.com>
2021-05-06 11:47:26 +0200

    move bounds-generating helpers to a separate file

--- src/analysis/flux_variability_analysis.jl
+++ src/analysis/flux_variability_analysis.jl
@@ -124,30 +124,6 @@ function flux_variability_analysis_dict(model::MetabolicModel, optimizer; kwargs
 end
 
 """
-    gamma_bounds(gamma)
-
-A bounds-generating function for [`flux_variability_analysis`](@ref) that
-limits the objective value to be at least `gamma*Z₀`, as usual in COBRA
-packages. Use as the `bounds` argument:
-```
-flux_variability_analysis(model, some_optimizer; bounds = gamma_bounds(0.9))
-```
-"""
-gamma_bounds(gamma) = z -> (gamma * z, Inf)
-
-"""
-    (tolerance) = z -> begin
-
-A bounds-generating function for [`flux_variability_analysis`](@ref) that
-limits the objective value to a small multiple of Z₀. Use as `bounds` argument,
-similarly to [`gamma_bounds`](@ref).
-"""
-objective_bounds(tolerance) = z -> begin
-    vs = (z * tolerance, z / tolerance)
-    (minimum(vs), maximum(vs))
-end
-
-"""
     _FVA_add_constraint(model, c, x, Z)
 
 Internal helper function for adding constraints to a model. Exists mainly
