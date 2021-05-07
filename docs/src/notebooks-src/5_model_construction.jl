cafda0e15b06334ae3dc18c539b53ba04fa09cec
St. Elmo <stelmozors@gmail.com>
2021-05-07 14:58:03 +0200

    finished model construction

--- docs/src/notebooks-src/5_model_construction.jl
+++ docs/src/notebooks-src/5_model_construction.jl
@@ -35,22 +35,47 @@ add!(model, metabolite_list)
 
 # ### Add reactions to the model
 
-# There are two ways to create reactions. Using a function and using a macro.
+# There are three ways to create and add reactions to a model. 
+# These are using functions, a macro, and arrows.
 
 r_m1 = Reaction("EX_m1", Dict("m1" => -1.0), :bidirectional) # exchange reaction: m1 <-> ∅ (nothing)
 r1 = Reaction("r1", Dict("m1" => -1.0, "m2" => 1.0), :forward)
+r5.grr = [["g1", "g2"],["g3"]] # add some gene reaction rules
 r2 = Reaction("r2", Dict("m2" => -1.0, "m1" => 1.0), :backward)
 r3 = Reaction("r3", Dict("m2" => -1.0, "m3" => 1.0), :bidirectional)
 
-add!(model, [r1, r2, r3, r_m1]) 
+add!(model, [r1, r2, r3, r_m1]) # function approach
 
 m1 = metabolite_list[1]
 m2 = metabolite_list[2]
 m3 = metabolite_list[3]
 m4 = metabolite_list[4]
 
-@add_reactions! model begin
+@add_reactions! model begin # macro approach
     r4, m2 ⟶ m4, 0, 1000
     r_m3, m3 ⟷ ∅, -1000, 1000
     r_m4, m4 ⟶ ∅
 end
+
+model.reactions["r4"].grr = [["g5"],["g6"],["g7"]]
+
+r5 = m4 ⟶ m2 # arrow approach
+r5.grr = [["g8"]]
+add!(model, r5) # still need to add this reaction
+
+#md # !!! note "Note: Using reaction arrows"
+#md #       `COBREXA` exports arrows that can be used to construct reactions.
+#md #       Both the long and short arrows (`⟶ == →`) mean the same thing if they
+#md #       point in the same direction. 
+#md #
+#md #       These arrows are accessible by using the `LaTeX` completions built into
+#md #       Julia. For example:
+#md #       1. → is \rightarrow<tab>
+#md #       2. ⟶ is \longrightarrow<tab>
+#md #       3. ← is \leftarrow<tab>
+#md #       4. ⟵ is \longleftarrow<tab>
+#md #       5. ↔ is \leftrightarrow<tab>
+#md #       6. ⟷ is \longleftrightarrow<tab>
+
+# The model can now be inspected.
+model
