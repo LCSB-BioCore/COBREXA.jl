c609f2c3e2b65111832e3ab2bc196ed2568a91d6
Mirek Kratochvil <exa.exa@gmail.com>
2021-05-08 09:50:17 +0200

    clean up tutorial 1

--- docs/src/notebooks-src/1_loading_converting_saving.jl
+++ docs/src/notebooks-src/1_loading_converting_saving.jl
@@ -6,119 +6,148 @@
 # `COBREXA` can load models stored in `.mat`, `.json`, and `.xml` formats (with
 # the latter denoting SBML formatted models). 
 
-# We will primarily use the *E. coli* toy model to demonstrate the utilities
-# found in `COBREXA`. To begin, first download some models in a variety of
-# formats.
+# We will primarily use the *E. Coli* "core" model to demonstrate the utilities
+# found in `COBREXA`. First, let's download the model in several formats.
 
 ## Downloads the model files if they don't already exist
 !isfile("e_coli_core.mat") &&
-    download("http://bigg.ucsd.edu/static/models/e_coli_core.mat", "e_coli_core.mat")
+    download("http://bigg.ucsd.edu/static/models/e_coli_core.mat", "e_coli_core.mat");
 !isfile("e_coli_core.json") &&
-    download("http://bigg.ucsd.edu/static/models/e_coli_core.json", "e_coli_core.json")
+    download("http://bigg.ucsd.edu/static/models/e_coli_core.json", "e_coli_core.json");
 !isfile("e_coli_core.xml") &&
-    download("http://bigg.ucsd.edu/static/models/e_coli_core.xml", "e_coli_core.xml")
+    download("http://bigg.ucsd.edu/static/models/e_coli_core.xml", "e_coli_core.xml");
+
+# Now, load the package:
 
 using COBREXA
 
-#md # !!! tip "Tip: Model files" 
-#md #     To avoid having to download the models multiple times, save the models in
-#md #     a central location and always load them from that location.
-
-#nb # To avoid having to download the models multiple times, save the models in
-#nb # a central location and always load them from that location.
+#md # !!! tip "Save bandwidth!" 
+#md #     The published models usually do not change very often. It is
+#md #     therefore pretty useful to save them to a central location and load
+#md #     them from there. That saves your time, and does not unnecessarily
+#md #     consume the connectivity resources of the model repository.
 
 # ## Loading models
 
-# Now load the models using the `load_model` function. Each model has "pretty printing".
+# Load the models using the [`load_model`](@ref) function. Each model is able to
+# "pretty-print" itself, hiding the inner complexity.
 
 mat_model = load_model("e_coli_core.mat")
 #
+
 json_model = load_model("e_coli_core.json")
 #
+
 sbml_model = load_model("e_coli_core.xml")
 #
 
-#md # !!! note "Note: Model type is inferred from the file type"
+#md # !!! note "Note: `load_model` infers the output type from the file extension"
 #md #       Notice how each model was read into memory as a model type corresponding
 #md #       to its file type, i.e. the file ending with `.json` loaded as a
-#md #       `JSONModel`, the file ending with `.mat` loaded as a `MATModel`, and the
-#md #       file ending with `.xml` loaded as an `SBMLModel`. 
-#md #   
-#md #       When loading a model from file like this (i.e., when the file type matches
-#md #       model type), no information gets lost because the model is loaded directly
-#md #       into memory from the underlying file type.
+#md #       [`JSONModel`](@ref), the file ending with `.mat` loaded as [`MATModel`](@ref), and the
+#md #       file ending with `.xml` loaded as an [`SBMLModel`](@ref). 
 
-#nb # Notice how each model was read into memory as a model type corresponding
-#nb # to its file type, i.e. the file ending with `.json` loaded as a
-#nb # `JSONModel`, the file ending with `.mat` loaded as a `MATModel`, and the
-#nb # file ending with `.xml` loaded as an `SBMLModel`. 
+# You can directly inspect the model objects, although only with a specific way
+# for each specific type.
 
-#nb #  When loading a model from file like this (i.e., when the file type matches
-#nb #  model type), no information gets lost because the model is loaded directly
-#nb #  into memory from the underlying file type.
+# JSON models contain their corresponding JSON:
 
-# You can directly inspect the model object, although this is discouraged because
-# generic interface functions, which are applicable to __all__ model types,
-# are provided by `COBREXA`.
+json_model.json
 
-json_model.json # try to not use this functionality
+# SBML models contain a complicated structure from [`SBML.jl`
+# package](https://github.com/LCSB-BioCore/SBML.jl):
+
+typeof(sbml_model.sbml)
+
+# MAT models contain MATLAB data:
+
+mat_model.mat
 
 # ## Using the generic interface to access model details
 
-# `COBREXA` makes use of a generic interface that enforces a uniform set of
-# accessor functions for model attributes. This approach ensures that any custom
-# function you create and test using, e.g. `JSONModel`s, also works on, e.g.
-# `SBMLModel`s, etc.
+# To prevent the complexities of object representation, `COBREXA.jl` uses a set
+# of generic interface functions that extract various important information
+# from all supported model types. This approach ensures that the analysis
+# functions can work on any data.
 
-# Here is a list of all the currently supported accessor functions.
-using InteractiveUtils #hide 
-for (i, method) in enumerate(
-    filter(
-        x -> endswith(string(x.file), "MetabolicModel.jl"),
-        InteractiveUtils.methodswith(MetabolicModel, COBREXA),
-    ),
-)
-    println(i, ") ", method.name)
-end
-
-# By using this interface you can access internal details about models using the
-# exact same function, regardless of the underlying model.
+# For example, you can check the reactions and metabolites contained in SBML
+# and JSON models using the same accessor:
 
 reactions(json_model)
+
 #
-issetequal(reactions(json_model), reactions(mat_model)) # same reactions returned
 
-# ## Converting between models
+reactions(sbml_model)
 
-# It is also possible to convert model types to-and-fro. To do this, use the
-# `convert` function, which is overloaded from Julia's `Base` module and functions
-# in the same way. 
-
-#md # !!! danger "Danger: Data loss may occur when converting between models"
-#md #       The generic interface is used to convert between model types, so only
-#md #       data accessible through the generic accessors will be converted.
-
-#nb # The generic interface is used to convert between model types, so only
-#nb # data accessible through the generic accessors will be converted.
-
-
-json_model_to_mat_model = convert(MATModel, json_model)
-
-# ## Loading and converting models at the same time
-
-# `COBREXA` also allows you to load models into a specific format using a
-# slightly more advanced version of the `load_model` function.
-
-mat_model_from_json_model_file = load_model(MATModel, "e_coli_core.json") # specify the model type to load
-
-# It is important to remember that this format change is always associated with
-# a call to `convert`, thus data loss may occur.
-
-# ## Saving models
-
-# `COBREXA` also allows you to save models. The format the models should be
-# saved in is inferred from the file suffix.
-
-save_model(mat_model_from_json_model_file, "converted_model.json")
 #
-save_model(mat_model_from_json_model_file, "converted_model.mat")
+
+issetequal(reactions(json_model), reactions(mat_model)) # do models contain the same reactions?
+
+# All accessors are defined in a single file in COBREXA source code; you may
+# therefore get a list of all accessors as follows:
+using InteractiveUtils
+
+for method in filter(
+    x -> endswith(string(x.file), "MetabolicModel.jl"),
+    InteractiveUtils.methodswith(MetabolicModel, COBREXA),
+)
+    println(method.name)
+end
+
+# ## Converting between model types
+
+# It is possible to convert model types to-and-fro. To do this, use the
+# `convert` function, which is overloaded from Julia's `Base`.
+
+#md # !!! danger "Data loss may occur when converting between models"
+#md #     The conversion of models only uses the data accessible through the
+#md #     generic accessors. Other data may get lost.
+
+m = convert(MATModel, json_model)
+
+# `m` will now contain the MATLAB-style matrix representation of the model:
+
+Matrix(m.mat["S"])
+
+# The loading and conversion can be combined using a shortcut:
+
+m = load_model(MATModel, "e_coli_core.json")
+
+# ## Saving and exporting models
+
+# `COBREXA.jl` supports exporting the models in JSON and MAT format, using [`save_model`](@ref).
+
+save_model(m, "converted_model.json")
+save_model(m, "converted_model.mat")
+
+# If you need a non-standard suffix, use the type-specific saving functions:
+
+save_json_model(m, "file.without.a.good.suffix")
+save_mat_model(m, "another.file.matlab")
+
+# If you are saving the models only for future processing in Julia environment,
+# it is often wasteful to encode the models to external formats and decode them
+# back. Instead, you can use the "native" Julia data format, accessible with
+# package `Serialization`.
+#
+# This way, you can use `serialize` to save even the [`StandardModel`](@ref)
+# that has no file format associated:
+
+using Serialization
+
+sm = convert(StandardModel, m)
+
+open(f -> serialize(f, sm), "myModel.stdmodel", "w")
+
+# The models can then be loaded back using `deserialize`:
+
+sm2 = deserialize("myModel.stdmodel")
+issetequal(metabolites(sm), metabolites(sm2))
+
+# This form of loading operation is usually pretty quick:
+t = @elapsed deserialize("myModel.stdmodel")
+@info "Deserialization took $t seconds"
+# Notably, large and complicated models with thousands of reactions and
+# annotations can take seconds to decode properly. Serialization allows you to
+# almost completely remove this overhead, and scales well to tens of millions
+# of reactions.
--- docs/src/tutorials/1_loading.md
+++ docs/src/tutorials/1_loading.md
@@ -1,4 +1,141 @@
 
-# Loading and converting models
+# Loading and converting model data
 
-This is a test.
+COBREXA.jl supports several constrained model formats that can be loaded with
+built-in functions. You can load the SBML models that are compatible with
+[libsbml](http://sbml.org/Software/libSBML), JSON models (such as the ones from
+[CobraPy](https://github.com/opencobra/cobrapy)), and MATLAB-style models (such
+as those from [COBRA Toolbox](https://github.com/opencobra/cobratoolbox).
+
+These formats are commonly available from many model repositories, such as from
+BIGG, as seen e.g. on [*E. Coli*
+entry](http://bigg.ucsd.edu/models/e_coli_core).  we show how to load the basic
+formats and work with such models.
+
+!!! tip "Notebook available!"
+    Example code for this tutorial is [available
+    here](../notebooks/1_loading_converting_saving.md)
+
+## Loading models from files
+
+For most purposes, you should be able to open and load any model with
+[`load_model`](@ref), which detects the file type from extension (`.xml`,
+`.json` and `.mat`) and calls the specific loading function. After loading
+COBREXA library with `using COBREXA` and downloading the model, you may load
+its data as follows:
+
+```
+my_model = load_model("e_coli_core.xml")
+```
+
+You should get an information about the loaded model, possibly looking like this:
+```
+Metabolic model of type JSONModel
+
+  [9 ,  1]  =  1.0
+  [51,  1]  =  1.0
+  ⋮
+  [57, 95]  =  1.0
+  [59, 95]  =  -1.0
+Number of reactions: 95
+Number of metabolites: 72
+```
+
+If the file type can not be guessed from the file extension, use either of the
+specific loader functions:
+
+- [`load_sbml_model`](@ref) for SBML
+- [`load_json_model`](@ref) for JSON
+- [`load_mat_model`](@ref) for MATLAB models
+
+All formats may store slightly different information. By default, COBREXA
+attempts not to discard any information unless a conversion to a more strict
+format is required. For example, the [`JSONModel`](@ref) (which is returned by
+[`load_json_model`](@ref)) still holds the original JSON structure that you can
+freely access for any specific purposes:
+
+```
+jm = load_json_model("e_coli_core.json")
+jm.json["reactions"][1]
+```
+
+That should print out the first reaction in the model in a JSON-style scheme,
+in our case the process catalyzed by phosphofructokinase:
+
+```
+Dict{String,Any} with 9 entries:
+  "name"               => "Phosphofructokinase"
+  "metabolites"        => Dict{String,Any}("adp_c"=>1.0,"atp_c"=>-1.0,"f6p_c"=>…
+  "lower_bound"        => 0.0
+  "id"                 => "PFK"
+  "notes"              => Dict{String,Any}("original_bigg_ids"=>Any["PFK"])
+  "gene_reaction_rule" => "b3916 or b1723"
+  "upper_bound"        => 1000.0
+  "subsystem"          => "Glycolysis/Gluconeogenesis"
+  "annotation"         => Dict{String,Any}("ec-code"=>Any["2.7.1.11"],"metanetx…
+```
+
+[`MATModel`](@ref) and [`SBMLModel`](@ref) (returned by the respective loading
+functions) contain similar "full" model information -- you can access the whole
+MATLAB and SBML data and build on them without any restrictions.
+
+## Converting to other model types
+
+Despite JSON and SBML are great for storing the model, the data representation
+is not very suitable for analyzing the model and processing it mathematically.
+
+COBREXA.jl contains several model types that are much more viable for
+supporting the analysis tasks. You can use the following:
+
+- [`CoreModel`](@ref), which represents the "core" of the optimization problem
+  and the corresponding linear programming task -- a sparse matrix, bounds
+  vectors, etc.
+- [`StandardModel`](@ref) (a "standard" for COBREXA.jl), which represents a
+  highly flexible, object-like, dictionary-based representation of a model,
+  which contains individual [`Reaction`](@ref)s, [`Metabolite`](@ref)s,
+  [`Gene`](@ref)s, and other things.
+
+!!! note Conversion limitations and data loss
+    Because of the imposed structure, the converted models may not contain all
+    information from the source files. You may need to check if any complicated
+    and less-standard annotations are still present if you require them, and
+    either use a more complicated model, or collect them manually.
+
+A loaded model can be converted to any other model type using the standard
+Julia conversion:
+
+```
+cm = convert(CoreModel, jm)
+```
+
+You can also use a shortcut in [`load_model`](@ref):
+
+```
+cm = load_model(CoreModel, "e_coli_core.xml")
+```
+
+With [`CoreModel`](@ref), the information is easily accessible in matrix form.
+For example, `cm.S` now contains the sparse stoichiometry matrix, which you can
+convert to a dense matrix and work with it in Julia in the usual way, as with
+any other matrix data:
+
+```
+Matrix(cm.S)
+```
+...should give you the (relatively empty) stoichiometry of the model.
+
+[`StandardModel`](@ref) is more suitable for fine-grained access to individual
+items of the model, perhaps closer to the SBML-style models. For example, you
+can view and set reaction bounds as follows:
+
+```
+sm = load_model(StandardModel, "e_coli_core.json")
+sm.reactions["PGI"].ub
+```
+...this prints the upper bound of the reaction (in this case, `1000.0`); you
+can change it the usual way:
+```
+sm.reactions["PGI"].ub = 500
+```
+
+This change will naturally project to future analysis results.
