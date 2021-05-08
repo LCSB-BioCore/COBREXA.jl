a122c1798c077820bca43582c54d5e4a6e98b839
St. Elmo <stelmozors@gmail.com>
2021-05-08 20:06:55 +0200

    added more information to make the notebook more understandable

--- docs/src/notebooks/6_building_community_models.jl
+++ docs/src/notebooks/6_building_community_models.jl
@@ -11,12 +11,10 @@
 # metabolizing microbes. We will use an objective function that enforces equal
 # growth rates.
 
-# If it is not already present, load a large scale *E. coli* model.
+# ## Load the base model
 
 !isfile("iML1515.json") &&
-    download("http://bigg.ucsd.edu/static/models/iML1515.json", "iML1515.json")
-
-# ## Load the base model
+    download("http://bigg.ucsd.edu/static/models/iML1515.json", "iML1515.json");
 
 using COBREXA
 using SparseArrays
@@ -26,8 +24,8 @@ using Tulip
 model = load_model(CoreModel, "iML1515.json") 
 
 # ## Describe the variants
+# Each variant is described by the substrate it consumes for energy/carbon
 
-## Each variant is specified by the substrate it consumes for energy/carbon
 variants = ["glc__D_e", 
             "xyl__D_e",
             "fru_e",
@@ -45,12 +43,15 @@ variants = ["glc__D_e",
 
 n_models = length(variants) # number of different models to construct and merge
 
+# ## Build the community stoichiometric matrix
+# The community stoichiometric matrix is constructed by combining the
+# stoichiometric matrices of the individual models, as well as adding
+# environmental variables.
+
 ex_rxn_inds = find_exchange_reactions(model; exclude_biomass = true)
 ex_met_inds = find_exchange_metabolites(model; exclude_biomass = true)
 n_env_vars = length(ex_rxn_inds) # number of exchange metabolites/reactions are equal
 n_rows, n_cols = size(stoichiometry(model))
-
-# ## Build the community stoichiometric matrix
 
 S = spzeros(n_models * n_rows + n_env_vars, n_models * n_cols + n_env_vars)
  
@@ -77,7 +78,8 @@ for n = 1:n_models
 end
 
 # ## Build flux bound vectors
-
+# Each flux needs to be constrained. For the most part, use the flux constraints
+# contained in the default model.
 lbs = spzeros(n_models * n_cols + n_env_vars)
 ubs = spzeros(n_models * n_cols + n_env_vars)
 single_lbs, single_ubs = bounds(model) # same bounds for each organism
@@ -90,20 +92,28 @@ end
 
 ## match exchange bounds to environmental fluxes for simplicity
 lbs[n_models*n_cols+1:end] .= single_lbs[ex_rxn_inds]
-ubs[n_models*n_cols+1:end] .= single_ubs[ex_rxn_inds]
+ubs[n_models*n_cols+1:end] .= single_ubs[ex_rxn_inds];
 
 # ## Create new names for the model's reactions and metabolites
+# Append information about the variant to the reactions and metabolites of each
+# model. This makes it easier to inspect/understand the flux solution at the
+# end.
+
 env_mets = metabolites(model)[ex_met_inds]
 ## name reactions
 rxn_ids = [
     vcat([reactions(model) .* "_org_$(variants[i])" for i = 1:n_models]...)
     "EX_" .* env_mets .* "_ENV"
 ]
+#
 ## name metabolites
 met_ids =
     [vcat([metabolites(model) .* "_org_$(variants[i])" for i = 1:n_models]...); env_mets .* "_ENV"]
 
 # ##  Adjust variants 
+# Each variant can only consume the substrate associated with it for
+# carbon/energy. Adjust the constraints to reflect this.
+
 for n = 1:n_models
     
     ## can import only the variant specific substrate
@@ -130,6 +140,12 @@ for n = 1:n_models
 end
 
 # ## Add biomass objective function
+# A biomass reaction is added that ensures that each microbe will grow at the
+# same rate. In short, this is achieved by creating new metabolites, which are
+# just the biomass of each model. These biomass metabolites are then combined to
+# create an overall biomass reaction. Since the proportions are locked into the
+# reaction (weighted), each microbe must grow at the same rate.
+
 ## create biomass metabolites
 S = vcat(S, [spzeros(size(S, 2))' for n=1:n_models]...)
 met_ids = [met_ids; ["biomass_org_$(variants[n])" for n=1:n_models]]
@@ -146,6 +162,10 @@ S = [S obj_rxn]
 rxn_ids = [rxn_ids; "community_biomass"]
 
 # ## Create community model
+# A community model is created by combining the elements of the model created
+# thus far. `CoreModel` is used since it is made for large-scale community
+# analysis.
+
 lbs = [lbs; 0.0]
 ubs = [ubs; 1000.0]
 c = spzeros(length(lbs))
@@ -158,14 +178,20 @@ dropzeros!(S)
 community_model = CoreModel(S, b, c, lbs, ubs, rxn_ids, met_ids)
 
 # ## Analyze community model
+# Conventional flux balance analysis is performed, and the individual growth
+# rates are inspected, as well as the environmental exchanges of the variant
+# metabolites. 
+
 d = flux_balance_analysis_dict(community_model, Tulip.Optimizer; modifications = [change_optimizer_attribute("IPM_IterationsLimit", 1000)])
 
 bof_rxn_inds = findall(x-> occursin("BIOMASS_Ec_iML1515_core", x), rxn_ids)
+## print the growth rate of each variant, should be the same
 for obj_id in rxn_ids[bof_rxn_inds]
    println(obj_id, ": ", d[obj_id]) 
 end
 
 # 
+## Print the substrate uptake associated with each variant
 for n in 1:n_models
     env_ex = "EX_"*variants[n]*"_org_$(variants[n])"
     println(env_ex, ": ", d[env_ex])
