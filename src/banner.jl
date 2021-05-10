4c0b4e18c025e3e8edb34d06eb6a5d2b9fba5b7d
St. Elmo <stelmozors@gmail.com>
2021-05-10 19:24:53 +0200

    new banner

--- src/banner.jl
+++ src/banner.jl
@@ -7,18 +7,21 @@ const COBREXA_VERSION =
 
 function _print_banner()
     c = Base.text_colors
-    n = c[:normal] # text
-    b = c[:bold] * c[:blue]
-    r = c[:bold] * c[:red]
-    g = c[:bold] * c[:green]
-    m = c[:bold] * c[:magenta]
+    n = c[:normal]
+    y = c[:bold] * c[:cyan]
+
     println(
+        "       
+             $(y)//   $(n) |
+  \\\\\\\\\\  // $(y)//    $(n) |
+   \\\\ \\\\// $(y)//     $(n) |    
+    \\\\ \\/ $(y)//      $(n) | COnstraint-Based Reconstruction
+     \\\\  $(y)//       $(n) | and EXascale Analysis in Julia
+     //  $(y)\\\\       $(n) | Version: v$(COBREXA_VERSION)
+    // $(y)/\\ \\\\      $(n) | 
+   // $(y)//\\\\ \\\\      $(n)|
+  // $(y)//  \\\\\\\\\\     $(n)|
+ //                $(n)|
         "
-  ____ ___  ____  ____  _____$(g)__$(n)  $(r)__$(n)    _     |
- / ___/ _ \\| __ )|  _ \\| ____$(g)\\ \\$(n)$(r)/ /$(n)   / \\    | Constraint-Based Reconstruction
-| |  | | | |  _ \\| |_) |  _|  $(g)\\$(n)  $(r)/$(n)   / _ \\   | and EXascale Analysis in Julia
-| |__| |_| | |_) |  _ <| |___ $(m)/$(n)  $(b)\\$(n)  / ___ \\  |
- \\____\\___/|____/|_| \\_\\_____$(m)/_/$(n)$(b)\\_\\$(n)/_/   \\_\\ | Version: v$(COBREXA_VERSION)
-                                             |",
     )
 end
