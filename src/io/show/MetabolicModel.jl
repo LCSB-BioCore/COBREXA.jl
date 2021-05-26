119a52e174585c901e94c8ba828d6f04b305e6c6
St. Elmo <stelmozors@gmail.com>
2021-05-26 20:51:36 +0200

    finished community stuff

--- src/io/show/MetabolicModel.jl
+++ src/io/show/MetabolicModel.jl
@@ -3,7 +3,7 @@ Pretty printing of everything metabolic-modelish.
 """
 function Base.show(io::IO, ::MIME"text/plain", m::MetabolicModel)
     _pretty_print_keyvals(io, "", "Metabolic model of type $(typeof(m))")
-    if prod(size(stoichiometry(m))) < 5_000_000
+    if prod(size(stoichiometry(m))) < 20_000_000
         println(io, stoichiometry(m))
     else # too big to display nicely
         println(io, "S = [...]")
