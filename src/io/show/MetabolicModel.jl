3921b0d2a56dbf5aaea91ce3ccb9279060bc6fe4
St. Elmo <stelmozors@gmail.com>
2021-05-27 10:09:02 +0200

    added stoich view constant

--- src/io/show/MetabolicModel.jl
+++ src/io/show/MetabolicModel.jl
@@ -3,7 +3,7 @@ Pretty printing of everything metabolic-modelish.
 """
 function Base.show(io::IO, ::MIME"text/plain", m::MetabolicModel)
     _pretty_print_keyvals(io, "", "Metabolic model of type $(typeof(m))")
-    if prod(size(stoichiometry(m))) < 20_000_000
+    if prod(size(stoichiometry(m))) < _constants.default_stoichiometrix_view_size
         println(io, stoichiometry(m))
     else # too big to display nicely
         println(io, "S = [...]")
