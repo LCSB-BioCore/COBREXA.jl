c61b837f24a3b0f60ebe84da82d552ceb6863ffe
St. Elmo <stelmozors@gmail.com>
2021-06-05 20:15:55 +0200

    :truck: move core model test functions to utils dir

--- test/base/utils/CoreModel.jl
+++ test/base/utils/CoreModel.jl
@@ -1,3 +1,49 @@
+@testset "Find exchange reactions and metabolites" begin
+    cp = test_LP()
+    @test isempty(find_exchange_reactions(cp))
+    @test isempty(find_exchange_metabolites(cp))
+
+    cp = test_simpleLP()
+    @test isempty(find_exchange_reactions(cp))
+    @test isempty(find_exchange_metabolites(cp))
+
+    cp = CoreModel(
+        [-1.0 -1 -2; 0 -1 0; 0 0 0],
+        zeros(3),
+        ones(3),
+        ones(3),
+        ones(3),
+        ["EX_m1"; "r2"; "r3"],
+        ["m1"; "m2"; "m3"],
+    )
+    @test find_exchange_reactions(cp) == [1]
+
+    cp = CoreModel(
+        [-1.0 0 0; 0 0 -1; 0 -1 0],
+        zeros(3),
+        ones(3),
+        ones(3),
+        ones(3),
+        ["EX_m1"; "Exch_m3"; "Ex_m2"],
+        ["m1"; "m2"; "m3"],
+    )
+    @test find_exchange_reactions(cp) == [1; 2; 3]
+    @test find_exchange_metabolites(cp) == [1; 3; 2]
+    @test find_exchange_reactions(cp, exc_prefs = ["Exch_"]) == [2]
+    @test find_exchange_metabolites(cp, exc_prefs = ["Exch_"]) == [3]
+
+    # this is originally the "toyModel1.mat"
+    cp = test_toyModel()
+
+    @test find_exchange_reactions(cp) == [4; 5; 6]
+    @test find_exchange_metabolites(cp) == [4; 5; 6]
+    @test find_exchange_reactions(cp, exclude_biomass = true) == [4; 5]
+    @test find_exchange_metabolites(cp, exclude_biomass = true) == [4; 5]
+    @test find_exchange_reactions(cp, exclude_biomass = true, biomass_str = "biom") ==
+          [4; 5]
+    @test find_exchange_metabolites(cp, exclude_biomass = true, biomass_str = "biom") ==
+          [4; 5]
+end
 
 @testset "CoreModel utilities" begin
     cp = test_LP()
