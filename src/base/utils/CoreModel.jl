4281af7cf5664fa1ac0a26ba0d4e1fd70fa24960
St. Elmo <stelmozors@gmail.com>
2021-06-05 15:06:29 +0200

    :wheelchair: move find_xxx functions to utilities
    Former-commit-id: 3c523d0b952b379bd05ece8dd9103e35b6c129c1

--- src/base/utils/CoreModel.jl
+++ src/base/utils/CoreModel.jl
@@ -17,3 +17,46 @@ Base.isequal(model1::CoreModelCoupled, model2::CoreModelCoupled) =
     isequal(model1.cu, model2.cu)
 
 Base.copy(model::CoreModelCoupled) = CoreModelCoupled(model.lm, model.C, model.cl, model.cu)
+
+"""
+Returns indices of exchange reactions.
+Exchange reactions are identified based on most commonly used prefixes.
+"""
+function find_exchange_reactions(
+    model::CoreModel;
+    exclude_biomass = false,
+    biomass_str::String = "biomass",
+    exc_prefs = ["EX_"; "Exch_"; "Ex_"],
+)
+    is_exc = falses(n_reactions(model))
+    for pref in exc_prefs
+        is_exc = is_exc .| startswith.(model.rxns, pref)
+    end
+    exc_inds = findall(is_exc)
+    if exclude_biomass
+        biom_inds = findall(x -> occursin(biomass_str, x), model.rxns)
+        exc_inds = setdiff(exc_inds, biom_inds)
+    end
+    return exc_inds
+end
+
+"""
+Returns indices of exchanged metabolites, ie, the outermost metabolites in the network
+In practice returns the metabolites consumed by the reactions given by `find_exchange_reactions`
+and if called with the same arguments, the two outputs correspond.
+"""
+function find_exchange_metabolites(
+    model::CoreModel;
+    exclude_biomass = false,
+    biomass_str::String = "biomass",
+    exc_prefs = ["EX_"; "Exch_"; "Ex_"],
+)
+    exc_rxn_inds = find_exchange_reactions(
+        model,
+        exclude_biomass = exclude_biomass,
+        biomass_str = biomass_str,
+        exc_prefs = exc_prefs,
+    )
+    exc_met_inds = [findfirst(x -> x == -1, model.S[:, j]) for j in exc_rxn_inds]
+    return exc_met_inds
+end
