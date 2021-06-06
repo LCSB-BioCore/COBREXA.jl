cd56c2e0c5f469d5152a49da4b4284bf1350fc70
St. Elmo <stelmozors@gmail.com>
2021-06-06 13:21:21 +0200

    larger models can now be displayed
    Former-commit-id: ce394f30809708767d81ff12a8f3474362cda51a

--- src/io/show/MetabolicModel.jl
+++ src/io/show/MetabolicModel.jl
@@ -3,7 +3,7 @@ Pretty printing of everything metabolic-modelish.
 """
 function Base.show(io::IO, ::MIME"text/plain", m::MetabolicModel)
     _pretty_print_keyvals(io, "", "Metabolic model of type $(typeof(m))")
-    if prod(size(stoichiometry(m))) < 5_000_000
+    if n_reactions(m) <= _constants.default_stoich_show_size
         println(io, stoichiometry(m))
     else # too big to display nicely
         println(io, "S = [...]")
