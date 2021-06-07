0f269066170e6369f614e341ad95b38c43b251dd
St. Elmo <stelmozors@gmail.com>
2021-06-07 14:52:06 +0200

    added downloads + fixed JSONModel issue

--- src/base/types/JSONModel.jl
+++ src/base/types/JSONModel.jl
@@ -283,7 +283,7 @@ Return the reaction equation of reaction with id `rxn_id` in model. The reaction
 equation maps metabolite ids to their stoichiometric coefficients.
 """
 function reaction_equation(m::JSONModel, rxn_id::String)::Dict{String, Float64}
-    ind = first(indexin([rxn_id], m.json["reactions"]))
+    ind = findfirst(x -> x["id"] == rxn_id, m.json["reactions"])
     m.json["reactions"][ind]["metabolites"]
 end
 
