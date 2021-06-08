6dd86eef4719abf7c52c0be75ba9ac4b884acf55
St. Elmo <stelmozors@gmail.com>
2021-06-08 10:31:54 +0200

    fix error
    Former-commit-id: 014c88f22ce2d1918d76726ce70808b1a9174ca4

--- test/base/utils/looks_like.jl
+++ test/base/utils/looks_like.jl
@@ -26,16 +26,16 @@
         ["m1"; "m2"; "m3_e"],
     )
     @test filter(looks_like_exchange_reaction, reactions(cp)) == ["EX_m1", "Exch_m3", "Ex_m2"]
-    @test filter(x -> looks_like_exchange_reaction(x; ex_prefixes = ["Exch_"]), reactions(cp)) == ["Exch_m3"]
+    @test filter(x -> looks_like_exchange_reaction(x; exchange_prefixes = ["Exch_"]), reactions(cp)) == ["Exch_m3"]
 
     # this is originally the "toyModel1.mat"
     cp = test_toyModel()
 
     @test filter(looks_like_exchange_reaction, reactions(cp)) == ["EX_m1(e)", "EX_m3(e)", "EX_biomass(e)"]
-    @test filter(x -> looks_like_exchange_reaction(x; exclude_biomass=true), reactions(cp))== ["EX_m1(e)", "EX_m3(e)"]
-    @test length(filter(looks_like_exchange_metabolite, metabolites(cp))) == 2
-    @test length(filter(looks_like_biomass_reaction, reactions(cp))) == 2
-    @test length(filter(x -> looks_like_biomass_reaction(x; exclude_exchanges=true), reactions(cp))) == 1
+    @test filter(x -> looks_like_exchange_reaction(x; exclude_biomass=true), reactions(cp)) == ["EX_m1(e)", "EX_m3(e)"]
+    @test filter(looks_like_exchange_metabolite, metabolites(cp)) == ["m1[e]", "m3[e]"]
+    @test filter(looks_like_biomass_reaction, reactions(cp)) == ["EX_biomass(e)", "biomass1"]
+    @test filter(x -> looks_like_biomass_reaction(x; exclude_exchanges=true), reactions(cp))
 end
 
 @testset "Looks like functions, basic" begin
