8c751cca8fe2004bba4d09667b836a23c1d80136
St. Elmo <stelmozors@gmail.com>
2021-06-08 08:52:18 +0200

    update meaning of looks_like_exchange_reaction
    Former-commit-id: 9c8948fcf2cc45e3c3725a5daab6da41e4c1555e

--- src/base/utils/looks_like.jl
+++ src/base/utils/looks_like.jl
@@ -5,7 +5,7 @@
         ex_prefixes = _constants.exchange_prefixes,
     )
 
-Returns a predicate function that can be used to identify reactions that are probably 
+A predicate function that can be used to identify reactions that are probably 
 exchange or biomass reactions. Exchange reactions are identified based on matching prefixes 
 in the set `ex_prefixes` and biomass reactions are identified by looking for occurences 
 of `biomass_strings` in the reaction id.
@@ -24,11 +24,37 @@ function looks_like_exchange_reaction(rxn_id::String;
     exclude_biomass = false,
     biomass_strings = _constants.biomass_strings,
     ex_prefixes = _constants.exchange_prefixes,
-)   
-    if any(startswith(rxn_id, x) for x in ex_prefixes) && !any(occursin(x, rxn_id) for x in biomass_strings) # found exchange reaction
-        return true
-    elseif !exclude_biomass && any([occursin(x, rxn_id) for x in biomass_strings]) # biomass
-        return true
+)::Bool
+    if exclude_biomass
+        if any(startswith(rxn_id, x) for x in ex_prefixes) && !any([occursin(x, rxn_id) for x in biomass_strings])
+            return true
+        end
+    else # don't exclude biomass
+        if any(startswith(rxn_id, x) for x in ex_prefixes)
+            return true
+        end
     end
+    return false
+end
+
+"""
+    looks_like_biomass_reaction(rxn_id::String;
+        biomass_strings = _constants.biomass_strings,
+    )::Bool
+
+A predicate function that can be used to identify reactions that are probably 
+biomass reactions. Biomass reactions are identified by looking for occurences 
+of `biomass_strings` in the reaction id.
+
+# Example
+```
+filter(looks_like_biomass_reaction, reactions(model)) # returns strings
+findall(looks_like_biomass_reaction, reactions(model)) # returns indices
+```
+"""
+function looks_like_biomass_reaction(rxn_id::String;
+    biomass_strings = _constants.biomass_strings,
+)::Bool
+    any(occursin(x, rxn_id) for x in biomass_strings) && return true
     return false
 end
