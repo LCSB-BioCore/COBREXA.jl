d52a2fb2a703d6e1f2d79de8268595fb2d4d17c7
St. Elmo <stelmozors@gmail.com>
2021-06-08 11:03:18 +0200

    add test for reaction_stoich in abstract

--- test/base/types/abstract/MetabolicModel.jl
+++ test/base/types/abstract/MetabolicModel.jl
@@ -6,7 +6,7 @@ end
 @testset "Base abstract model methods require proper minimal implementation" begin
     @test_throws MethodError reactions(123)
     x = FakeModel(123)
-    for m in [reactions, metabolites, stoichiometry, bounds, objective]
+    for m in [reactions, metabolites, stoichiometry, bounds, objective, reaction_stoichiometry]
         @test_throws MethodError m(x)
     end
 end
