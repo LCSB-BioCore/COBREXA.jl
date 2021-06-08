c092982cedb24623843be9b31baf924480408bd0
St. Elmo <stelmozors@gmail.com>
2021-06-08 09:23:57 +0200

    fix tests

--- test/base/utils/looks_like.jl
+++ test/base/utils/looks_like.jl
@@ -1,4 +1,4 @@
-@testset "Find exchange reactions and metabolites CoreModel" begin
+@testset "Looks like in CoreModel, detailed test" begin
     cp = test_LP()
     @test isempty(filter(looks_like_exchange_reaction, reactions(cp)))
 
@@ -23,7 +23,7 @@
         ones(3),
         ones(3),
         ["EX_m1"; "Exch_m3"; "Ex_m2"],
-        ["m1"; "m2"; "m3"],
+        ["m1"; "m2"; "m3_e"],
     )
     @test filter(looks_like_exchange_reaction, reactions(cp)) == ["EX_m1", "Exch_m3", "Ex_m2"]
     @test filter(x -> looks_like_exchange_reaction(x; ex_prefixes = ["Exch_"]), reactions(cp)) == ["Exch_m3"]
@@ -31,23 +31,37 @@
     # this is originally the "toyModel1.mat"
     cp = test_toyModel()
 
-    @test filter(looks_like_exchange_reaction, reactions(cp)) == ["EX_m1(e)", "EX_m3(e)", "EX_biomass(e)", "biomass1"]
+    @test filter(looks_like_exchange_reaction, reactions(cp)) == ["EX_m1(e)", "EX_m3(e)", "EX_biomass(e)"]
     @test filter(x -> looks_like_exchange_reaction(x; exclude_biomass=true), reactions(cp))== ["EX_m1(e)", "EX_m3(e)"]
+    @test length(filter(looks_like_exchange_metabolite, metabolites(cp))) == 2
+    @test length(filter(looks_like_biomass_reaction, reactions(cp))) == 1
+    
 end
 
-@testset "Find exchange reactions and metabolites: all models except core" begin
+@testset "Looks like functions, basic" begin
     model = load_model(model_paths["e_coli_core.json"])
-    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 21
+    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 20
+    @test length(filter(looks_like_exchange_metabolite, metabolites(model))) == 20
+    @test length(filter(looks_like_biomass_reaction, reactions(model))) == 1
     
     model = load_model(model_paths["e_coli_core.xml"])
-    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 21
-   
+    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 20
+    @test length(filter(looks_like_exchange_metabolite, metabolites(model))) == 20
+    @test length(filter(looks_like_biomass_reaction, reactions(model))) == 1
+    
     model = load_model(model_paths["e_coli_core.mat"])
-    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 21
+    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 20
+    @test length(filter(looks_like_exchange_metabolite, metabolites(model))) == 20
+    @test length(filter(looks_like_biomass_reaction, reactions(model))) == 1
     
     model = convert(StandardModel, model)
-    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 21
-   
+    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 20
+    @test length(filter(looks_like_exchange_metabolite, metabolites(model))) == 20
+    @test length(filter(looks_like_biomass_reaction, reactions(model))) == 1
+    
     model = convert(CoreModelCoupled, model)
-    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 21
+    @test length(filter(looks_like_exchange_reaction, reactions(model))) == 20
+    @test length(filter(looks_like_exchange_metabolite, metabolites(model))) == 20
+    @test length(filter(looks_like_biomass_reaction, reactions(model))) == 1
+    
 end
