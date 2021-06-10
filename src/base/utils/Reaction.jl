8df34b58bcb8d033b61abbdd93516cc67164c602
St. Elmo <stelmozors@gmail.com>
2021-06-10 23:03:18 +0200

    🩹make atom_exchage more robust

--- src/base/utils/Reaction.jl
+++ src/base/utils/Reaction.jl
@@ -60,7 +60,7 @@ See also: [`get_atoms`](@ref), [`check_duplicate_reaction`](@ref)
 function is_mass_balanced(rxn::Reaction, model::StandardModel)
     atom_balances = Dict{String,Float64}() # float here because stoichiometry is not Int
     for (met, stoich) in rxn.metabolites
-        atoms = get_atoms(model.metabolites[met])
+        atoms = metabolite_formula(model, met)
         isempty(atoms) && continue # ignore blanks
         for (k, v) in atoms
             atom_balances[k] = get(atom_balances, k, 0) + v * stoich
