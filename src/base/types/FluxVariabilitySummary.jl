ba9ee4b4fe439eea39f88d0cda7660472593df10
St. Elmo <stelmozors@gmail.com>
2021-06-11 14:55:02 +0200

    slightly better fluxsummary

--- src/base/types/FluxVariabilitySummary.jl
+++ src/base/types/FluxVariabilitySummary.jl
@@ -1,31 +1,47 @@
+"""
+    FluxVariabilitySummary
+
+A struct used to store summary information about the 
+solution of a flux variability analysis result.
+"""
 struct FluxVariabilitySummary
-    biomass_fluxes :: Dict{String, Vector{Float64}}
-    import_fluxes :: Dict{String, Vector{Float64}}
-    export_fluxes :: Dict{String, Vector{Float64}}
-    unbounded_fluxes :: Dict{String, Vector{Float64}}
+    biomass_fluxes :: Dict{String, Vector{Union{Float64, Nothing}}}
+    exchange_fluxes :: Dict{String, Vector{Union{Float64, Nothing}}} 
 end
 
-function flux_variability_summary(model::MetabolicModel, 
-    flux_result::Tuple{Dict{String, Dict{String, Float64}}, Dict{String, Dict{String, Float64}}}; 
+"""
+flux_variability_summary(flux_result::Tuple{Dict{String, Dict{String, Float64}}, Dict{String, Dict{String, Float64}}}; 
     exclude_exchanges = false,
     exchange_prefixes = _constants.exchange_prefixes,
     biomass_strings = _constants.biomass_strings,
     exclude_biomass = false,
-    small_flux_bound = 1.0/_constants.default_reaction_bound^2,
-    large_flux_bound = _constants.default_reaction_bound,
-    keep_unbounded = false,
+    )
+Return a 
+"""
+function flux_variability_summary(flux_result::Tuple{Dict{String, Dict{String, Float64}}, Dict{String, Dict{String, Float64}}}; 
+    exclude_exchanges = false,
+    exchange_prefixes = _constants.exchange_prefixes,
+    biomass_strings = _constants.biomass_strings,
+    exclude_biomass = false,
     )
 
     rxn_ids = keys(flux_result[1])
     ex_rxns = filter(x -> looks_like_exchange_reaction(x, exclude_biomass=exclude_biomass, biomass_strings=biomass_strings, exchange_prefixes=exchange_prefixes), rxn_ids)
     bmasses = filter(x -> looks_like_biomass_reaction(x; exclude_exchanges=exclude_exchanges, exchange_prefixes=exchange_prefixes, biomass_strings=biomass_strings), rxn_ids)
 
-
-    res = Dict{String, Vector{Float64}}()
-    for rxn_id in rxn_ids
-
+    biomass_fluxes = Dict{String, Vector{Union{Float64, Nothing}}}()
+    for rxn_id in bmasses
+        lb = isnothing(flux_result[1][rxn_id]) ? nothing : flux_result[1][rxn_id][rxn_id]
+        ub = isnothing(flux_result[2][rxn_id]) ? nothing : flux_result[2][rxn_id][rxn_id]
         res[rxn_id] = [lb, ub]
     end
 
+    ex_rxn_fluxes = Dict{String, Vector{Union{Float64, Nothing}}}() 
+    for rxn_id in ex_rxns
+        lb = isnothing(flux_result[1][rxn_id]) ? nothing : flux_result[1][rxn_id][rxn_id]
+        ub = isnothing(flux_result[2][rxn_id]) ? nothing : flux_result[2][rxn_id][rxn_id]
+        ex_rxn_fluxes[rxn_id] = [lb, ub]
+    end
 
+    return FluxVariabilitySummary(biomass_fluxes, ex_rxn_fluxes)
 end
--- src/io/show/FluxVariabilitySummary.jl
+++ src/io/show/FluxVariabilitySummary.jl
@@ -1,3 +1,3 @@
 function Base.show(io::IO, ::MIME"text/plain", flux_res::FluxVariabilitySummary)
-    # for the repl
+    
 end
