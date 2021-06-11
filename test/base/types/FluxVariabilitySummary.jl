e7830ac5eb2efb40db9bad4e3f85792b37e9fe8a
St. Elmo <stelmozors@gmail.com>
2021-06-11 15:58:54 +0200

    tests done

--- test/base/types/FluxVariabilitySummary.jl
+++ test/base/types/FluxVariabilitySummary.jl
@@ -1,13 +1,16 @@
 @testset "Flux variability summary" begin
     model = load_model(model_paths["e_coli_core.json"])
 
-    sol = flux_balance_analysis_dict(
+    sol = flux_variability_analysis_dict(
         model,
         Tulip.Optimizer;
+        bounds = objective_bounds(0.99),
         modifications = [
             change_optimizer_attribute("IPM_IterationsLimit", 200),
         ],
     )
 
     fr = flux_variability_summary(sol)
+    @test isapprox(fr.biomass_fluxes["BIOMASS_Ecoli_core_w_GAM"][1], 0.8651822872764786; atol=TEST_TOLERANCE)
+    @test isapprox(fr.exchange_fluxes["EX_for_e"][2], 1.1440672757160666; atol=TEST_TOLERANCE)
 end
