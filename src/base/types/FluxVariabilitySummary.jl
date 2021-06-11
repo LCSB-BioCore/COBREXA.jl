0f86d2b3baf0e0846f8c46d5046e9ebab1fcaa0c
St. Elmo <stelmozors@gmail.com>
2021-06-11 14:28:05 +0200

    delete summarize

--- src/base/types/FluxVariabilitySummary.jl
+++ src/base/types/FluxVariabilitySummary.jl
@@ -1,19 +1,26 @@
 struct FluxVariabilitySummary
-    biomass_fluxes :: Dict{String, Float64}
-    import_fluxes :: Dict{String, Float64}
-    export_fluxes :: Dict{String, Float64}
-    unbounded_fluxes :: Dict{String, Float64}
+    biomass_fluxes :: Dict{String, Vector{Float64}}
+    import_fluxes :: Dict{String, Vector{Float64}}
+    export_fluxes :: Dict{String, Vector{Float64}}
+    unbounded_fluxes :: Dict{String, Vector{Float64}}
 end
 
-function flux_variability_summary(model::MetabolicModel, flux_result::Dict{String, Dict{String, Float64}}; 
+function flux_variability_summary(model::MetabolicModel, 
+    flux_result::Tuple{Dict{String, Dict{String, Float64}}, Dict{String, Dict{String, Float64}}}; 
     exclude_exchanges = false,
     exchange_prefixes = _constants.exchange_prefixes,
     biomass_strings = _constants.biomass_strings,
     exclude_biomass = false,
     small_flux_bound = 1.0/_constants.default_reaction_bound^2,
     large_flux_bound = _constants.default_reaction_bound,
-    round_digits = 3,
     keep_unbounded = false,
     )
+
+    rxn_ids = keys(flux_result[1])
+    res = Dict{String, Vector{Float64}}()
+    for rxn_id in rxn_ids
+        
+    end
+
 
 end
