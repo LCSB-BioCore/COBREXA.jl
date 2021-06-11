b5dfe01eff37832c5b6d17ce2c71255b1702149f
St. Elmo <stelmozors@gmail.com>
2021-06-11 16:03:33 +0200

    better docstings

--- src/base/types/FluxVariabilitySummary.jl
+++ src/base/types/FluxVariabilitySummary.jl
@@ -10,13 +10,36 @@ struct FluxVariabilitySummary
 end
 
 """
-flux_variability_summary(flux_result::Tuple{Dict{String, Dict{String, Float64}}, Dict{String, Dict{String, Float64}}}; 
-    exclude_exchanges = false,
-    exchange_prefixes = _constants.exchange_prefixes,
-    biomass_strings = _constants.biomass_strings,
-    exclude_biomass = false,
-    )
-Return a 
+    flux_variability_summary(flux_result::Tuple{Dict{String, Dict{String, Float64}}, Dict{String, Dict{String, Float64}}}; 
+        exclude_exchanges = false,
+        exchange_prefixes = _constants.exchange_prefixes,
+        biomass_strings = _constants.biomass_strings,
+        exclude_biomass = false,
+        )
+Return a `FluxVariabilitySummary` struct based on the `flux_result` of a
+constraint based analysis simulation. Internally this function uses
+[`looks_like_biomass_reaction`](@ref) and
+[`looks_like_exchange_reaction`](@ref). The corresponding keyword arguments
+passed to these functions. Use this if your model has non-standard ids for
+reactions. 
+
+# Example
+```
+julia> sol = flux_variability_analysis_dict(model, Gurobi.Optimizer; bounds = objective_bounds(0.99))
+julia> flux_res = flux_variability_summary(sol)
+Biomass                     Lower bound   Upper bound
+  BIOMASS_Ecoli_core_w_GAM: 0.8652        0.8652
+Exchange
+  EX_h2o_e:                 28.34         28.34
+  EX_co2_e:                 22.0377       22.0377
+  EX_o2_e:                  -22.1815      -22.1815
+  EX_h_e:                   17.3556       17.3556
+  EX_glc__D_e:              -10.0         -10.0
+  EX_nh4_e:                 -4.8448       -4.8448
+  EX_pi_e:                  -3.2149       -3.2149
+  EX_for_e:                 0.0           0.0
+  ...                       ...           ...
+```
 """
 function flux_variability_summary(flux_result::Tuple{Dict{String, Dict{String, Float64}}, Dict{String, Dict{String, Float64}}}; 
     exclude_exchanges = false,
