2232c66cf3943ab6b761046cc686168362554673
St. Elmo <stelmozors@gmail.com>
2021-06-18 17:47:16 +0200

    added Reaction convenience constructors

--- src/base/types/Reaction.jl
+++ src/base/types/Reaction.jl
@@ -1,3 +1,4 @@
+using Base: Float64
 """
 Reaction struct.
 
@@ -50,24 +51,95 @@ mutable struct Reaction
         annotations,
         objective_coefficient,
     )
-
-    # this constructor is only used internally
-    function Reaction(
-        id::String,
-        metabolites::Dict{String,Float64},
-        dir = :bidirectional;
-        default_bound = _constants.default_reaction_bound,
-    )
-        if dir == :forward
-            lb = 0.0
-            ub = default_bound
-        elseif dir == :reverse
-            lb = -default_bound
-            ub = 0.0
-        else
-            lb = -default_bound
-            ub = default_bound
-        end
-        Reaction(id; metabolites = metabolites, lb = lb, ub = ub)
-    end
 end
+
+"""
+Reaction(
+    id::String,
+    metabolites::Dict{String,Union{Int, Float64}},
+    dir = :bidirectional;
+    default_bound = _constants.default_reaction_bound,
+)
+
+Convenience constructor for `Reaction`.
+"""
+function Reaction(
+    id::String,
+    metabolites::Dict{String,Float64},
+    dir = :bidirectional;
+    default_bound = _constants.default_reaction_bound,
+)
+    if dir == :forward
+        lb = 0.0
+        ub = default_bound
+    elseif dir == :reverse
+        lb = -default_bound
+        ub = 0.0
+    else
+        lb = -default_bound
+        ub = default_bound
+    end
+    Reaction(id; metabolites = metabolites, lb = lb, ub = ub)
+end
+
+"""
+Reaction(
+    id::String,
+    metabolites,
+    dir = :bidirectional;
+    default_bound = _constants.default_reaction_bound,
+)
+
+Convenience constructor for `Reaction`.
+"""
+Reaction(
+    id::String,
+    metabolites,
+    dir = :bidirectional;
+    default_bound = _constants.default_reaction_bound,
+) = Reaction(
+    id,
+    Dict(k => float(v) for (k, v) in metabolites),
+    dir;
+    default_bound = default_bound,
+)
+
+"""
+Reaction(
+    id::String;
+    name = nothing,
+    metabolites = Dict{String,Real}(),
+    lb = -_constants.default_reaction_bound,
+    ub = _constants.default_reaction_bound,
+    grr = nothing,
+    subsystem = nothing,
+    notes = Notes(),
+    annotations = Annotations(),
+    objective_coefficient = 0.0,
+)
+
+Convenience constructor for `Reaction`.
+"""
+Reaction(
+    id::String;
+    name = nothing,
+    metabolites = Dict{String,Real}(),
+    lb = -_constants.default_reaction_bound,
+    ub = _constants.default_reaction_bound,
+    grr = nothing,
+    subsystem = nothing,
+    notes = Notes(),
+    annotations = Annotations(),
+    objective_coefficient = 0.0,
+) = Reaction(
+    id,
+    name,
+    Dict(k => float(v) for (k, v) in metabolites),
+    lb,
+    ub,
+    grr,
+    subsystem,
+    notes,
+    annotations,
+    objective_coefficient,
+)
