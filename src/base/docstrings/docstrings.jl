83c30e995a0dda2d78826e4dc356e8c022c1bc9a
St. Elmo <stelmozors@gmail.com>
2021-06-18 22:41:26 +0200

    fixed error, now just not working

--- src/base/docstrings/docstrings.jl
+++ src/base/docstrings/docstrings.jl
@@ -2,8 +2,8 @@ macro _change_bound!(model_type, index_type, example)
     return :(
         """
         change_bound!(
-            model::$model_type,
-            rxn::$index_type;
+            model::$($model_type),
+            rxn::$($index_type);
             lower_bound = -_constants.default_reaction_bound,
             upper_bound = _constants.default_reaction_bound,
         )
@@ -16,9 +16,9 @@ macro _change_bound!(model_type, index_type, example)
     
     # Example
     ```
-    change_bound!(model, $example; lower_bound=-10, ub=10)
-    change_bound!(model, $example; lower_bound=-10.2) # upper_bound is set to _constants.default_reaction_bound
-    change_bound!(model, $example; upper_bound=10)
+    change_bound!(model, $($example); lower_bound=-10, ub=10)
+    change_bound!(model, $($example); lower_bound=-10.2) # upper_bound is set to _constants.default_reaction_bound
+    change_bound!(model, $($example); upper_bound=10)
     ```
     """
     )
