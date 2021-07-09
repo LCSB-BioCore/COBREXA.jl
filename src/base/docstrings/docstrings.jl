daf747ea387f0fcb8f7ad4a4af1c9f402eac6933
St. Elmo <stelmozors@gmail.com>
2021-07-09 11:50:17 +0200

    macros!!!!!!! #angryface

--- src/base/docstrings/docstrings.jl
+++ src/base/docstrings/docstrings.jl
@@ -1,39 +1,33 @@
-const is_inplace = true
-const not_inplace = false
-const is_plural = true
-const not_plural = false
-
-macro _change_bound_s_bang(model_type, index_name, index_type, example, isplural, isinplace)
-    if isplural
-        if isinplace
+macro _change_bound_s_bang(model_type, index_name, index_type, example, is_singular, is_inplace)
+    if eval(:($is_singular) != :singular)
+        if eval(:($is_inplace) == :inplace)
             add_bang = "!"
-            descr_sentence = "    Change the bounds of all reactions with `$(index_name)` in `model` in-place."
+            descr_sentence = "Change the bounds of all reactions with `$(index_name)` in `model` in-place."
         else
             add_bang = ""
-            descr_sentence = "    Return a shallow copy of the `model` where the bounds of all reactions with
-            `$(index_name)` have been changed."
+            descr_sentence = "Return a shallow copy of the `model` where the bounds of all reactions with `$(index_name)` have been changed."
         end
         add_s = "s"
         bounds1 = "lower_bounds=[-10, -20], upper_bounds=[10, 22]"
         bounds2 = "lower_bounds=[-10.2, -14.3]"
         bounds3 = "upper_bounds=[10.2, 23]"
-        lbs = "fill(-_constants.default_reaction_bound, length($(index_name)))"
-        ubs = "fill(_constants.default_reaction_bound, length($(index_name)))"
+        lbs = "fill(nothing, length($(index_name)))"
+        ubs = "fill(nothing, length($(index_name)))"
     else
-        if isinplace
+        if eval(:($is_inplace) == :inplace)
             add_bang = "!"
-            descr_sentence = "    Change the bounds of a reaction `$(index_name)` in `model` in-place."
+            descr_sentence = "Change the bounds of a reaction `$(index_name)` in `model` in-place."
         else
             add_bang = ""
-            descr_sentence = "    Return a shallow copy of the `model` where the bounds of reaction with
+            descr_sentence = "Return a shallow copy of the `model` where the bounds of reaction with
             `$(index_name)` has been changed."
         end
         add_s = ""
         bounds1 = "lower_bound=-10, upper_bound=10"
         bounds2 = "lower_bound=-10.2"
         bounds3 = "upper_bound=10"
-        lbs = "-_constants.default_reaction_bound"
-        ubs = "_constants.default_reaction_bound"
+        lbs = "nothing"
+        ubs = "nothing"
     end
     return :(
         """
@@ -44,16 +38,14 @@ macro _change_bound_s_bang(model_type, index_name, index_type, example, isplural
             upper_bound$($add_s) = $($ubs),
         )
     
-    $($descr_sentence) Note
-    that if the bound argument is not supplied then a default (see
-    `_constants.default_reaction_bound`) is used. 
+    $($descr_sentence) Note that if the bound argument is `nothing` then it is not changed. 
     
     See also: [`change_bound`](@ref), [`change_bounds!`](@ref), [`change_bound!`](@ref), [`change_bounds!`](@ref)
     
     # Example
     ```
     change_bound$($add_s)$($add_bang)(model, $($example); $($bounds1))
-    change_bound$($add_s)$($add_bang)(model, $($example); $($bounds2)) # upper_bound$($add_s) set to _constants.default_reaction_bound
+    change_bound$($add_s)$($add_bang)(model, $($example); $($bounds2))
     change_bound$($add_s)$($add_bang)(model, $($example); $($bounds3))
     ```
     """
--- src/reconstruction/CoreModelCoupled.jl
+++ src/reconstruction/CoreModelCoupled.jl
@@ -362,7 +362,7 @@ function change_coupling_bounds!(
     end
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idxs", "Vector{Int}", "[2, 43]", is_plural, is_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idxs", "Vector{Int}", "[2, 43]", is_plural, is_inplace)
 function change_bounds!(
     model::CoreModelCoupled,
     reaction_idxs::Vector{Int};
@@ -374,7 +374,7 @@ function change_bounds!(
     end
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idx", "Int", "2", not_plural, is_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idx", "Int", "2", not_plural, is_inplace)
 function change_bound!(
     model::CoreModelCoupled,
     rxn::Int;
@@ -386,7 +386,7 @@ function change_bound!(
     return nothing # so that nothing gets printed
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_ids", "Vector{String}", "[\"PFL\", \"FBA\"]", is_plural, is_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_ids", "Vector{String}", "[\"PFL\", \"FBA\"]", is_plural, is_inplace)
 function change_bounds!(
     model::CoreModelCoupled,
     rxn_ids::Vector{String};
@@ -396,7 +396,7 @@ function change_bounds!(
     change_bounds!(model, Int.(indexin(rxn_ids, reactions(model))); lower_bounds = lower_bounds, upper_bounds = upper_bounds)
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_id", "String", "\"PFL\"", not_plural, is_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_id", "String", "\"PFL\"", not_plural, is_inplace)
 function change_bound!(
     model::CoreModelCoupled,
     rxn_id::String;
@@ -406,7 +406,7 @@ function change_bound!(
     change_bound!(model, first(indexin([rxn_id], reactions(model))); lower_bound = lower_bound, upper_bound = upper_bound)
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idxs", "Vector{Int}", "[2, 43]", is_plural, not_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idxs", "Vector{Int}", "[2, 43]", is_plural, not_inplace)
 function change_bounds(
     model::CoreModelCoupled,
     rxns::Vector{Int};
@@ -423,7 +423,7 @@ function change_bounds(
     return m
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idx", "Int", "2", not_plural, not_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_idx", "Int", "2", not_plural, not_inplace)
 function change_bound(
     model::CoreModelCoupled,
     reaction_idx::Int;
@@ -438,7 +438,7 @@ function change_bound(
     return m
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_ids", "Vector{String}", "[\"PFL\", \"FBA\"]", is_plural, not_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_ids", "Vector{String}", "[\"PFL\", \"FBA\"]", is_plural, not_inplace)
 function change_bounds(
     model::CoreModelCoupled,
     rxn_ids::Vector{String};
@@ -448,7 +448,7 @@ function change_bounds(
     change_bounds(model, Int.(indexin(rxn_ids, reactions(model))); lower_bounds = lower_bounds, upper_bounds = upper_bounds)
 end
 
-@doc @_change_bound_s_bang("CoreModelCoupled", "rxn_id", "String", "\"PFL\"", not_plural, not_inplace)
+# @doc @_change_bound_s_bang("CoreModelCoupled", "rxn_id", "String", "\"PFL\"", not_plural, not_inplace)
 function change_bound(
     model::CoreModelCoupled,
     rxn_id::String;
