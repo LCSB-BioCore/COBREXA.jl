32ffaf3115631381f0c1e0f2963594806b950d3a
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2021-10-25 16:41:30 +0200

    explain the use of Serialized models better
    closes #361

--- src/base/utils/Serialized.jl
+++ src/base/utils/Serialized.jl
@@ -3,8 +3,15 @@
     serialize_model(model::MM, filename::String)::Serialized{MM} where {MM<:MetabolicModel}
 
 Serialize the `model` to file `filename`, returning a [`Serialized`](@ref)
-model that is able to load itself back automatically upon precaching by
-[`precache!`](@ref).
+model that can be loaded back transparently by [`precache!`](@ref). The result
+does _not_ contain the actual model data that are deferred to the disk; it may
+thus be used to save memory, or send the model efficiently to remote workers
+within distributed shared-storage environments.
+
+The benefit of using this over "raw" `Serialization.serialize` is that the
+resulting `Serialized` model will reload itself automatically with
+[`precache!`](@ref) at first usage, which needs to be done manually when using
+the `Serialization` package directly.
 """
 function serialize_model(
     model::MM,
