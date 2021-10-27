cf49055edc006c4fa84bdc825c1ecfa2552891f7
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2021-10-27 14:57:41 +0200

    remove some of the repeated code, make MOMA more flexible

--- src/analysis/minimize_metabolic_adjustment.jl
+++ src/analysis/minimize_metabolic_adjustment.jl
@@ -1,22 +1,27 @@
 """
-    minimize_metabolic_adjustment(
+    minimize_metabolic_adjustment_analysis(
         model::MetabolicModel,
-        flux_ref::Vector{Float64},
+        flux_ref::Union{Dict{String,Float64}, Vector{Float64}},
         optimizer;
         modifications = [],
+        kwargs...
     )
 
-Run minimization of metabolic adjustment (MOMA) on `model` with respect to `flux_ref`, which
-is a vector of fluxes in the order of `reactions(model)`. MOMA finds the shortest Euclidian
-distance between `flux_ref` and `model` with `modifications`:
+Run minimization of metabolic adjustment (MOMA) on `model` with respect to
+`flux_ref`, which is a vector of fluxes in the order of `reactions(model)`.
+MOMA finds the shortest Euclidian distance between `flux_ref` and `model` with
+`modifications`:
 ```
 min Σᵢ (xᵢ - flux_refᵢ)²
 s.t. S x = b
      xₗ ≤ x ≤ xᵤ
 ```
-Note, the problem has a quadratic objective, so a QP solver is required. See "Daniel,
-Vitkup & Church, Analysis of Optimality in Natural and Perturbed Metabolic Networks,
-Proceedings of the National Academy of Sciences, 2002" for more details.
+Because the problem has a quadratic objective, a QP solver is required. See
+"Daniel, Vitkup & Church, Analysis of Optimality in Natural and Perturbed
+Metabolic Networks, Proceedings of the National Academy of Sciences, 2002" for
+more details.
+
+Additional arguments are passed to [`flux_balance_analysis`](@ref).
 
 Returns an optimized model that contains the resultant nearest flux.
 
@@ -33,62 +38,58 @@ optmodel = minimize_metabolic_adjustment(
 value.(solution[:x])  # extract the flux from the optimizer
 ```
 """
-function minimize_metabolic_adjustment(
+minimize_metabolic_adjustment_analysis(
     model::MetabolicModel,
-    flux_ref::Vector{Float64},
+    flux_ref::Union{Dict{String,Float64},Vector{Float64}},
     optimizer;
     modifications = [],
+    kwargs...,
+) = flux_balance_analysis(
+    model,
+    optimizer;
+    modifications = vcat([minimize_metabolic_adjustment(flux_ref)], modifications),
+    kwargs...,
 )
-    opt_model = make_optimization_model(model, optimizer)
 
-    # prepare the model for MOMA
-    for mod in modifications
-        mod(model, opt_model)
+"""
+    minimize_metabolic_adjustment(flux_ref::Vector{Float64})
+
+An optimization model modification that implements the MOMA in
+[`minimize_metabolic_adjustment_analysis`](@ref).
+"""
+minimize_metabolic_adjustment(flux_ref::Vector{Float64}) =
+    (model, opt_model) -> begin
+        length(opt_model[:x]) == length(flux_ref) || throw(
+            DomainError(
+                flux_ref,
+                "length of the reference flux doesn't match the one in the optimization model",
+            ),
+        )
+        @objective(opt_model, Min, sum((opt_model[:x] .- flux_ref) .^ 2))
     end
 
-    # moma objective
-    v = opt_model[:x] # fluxes
-    @objective(opt_model, Min, sum((v[i] - flux_ref[i])^2 for i = 1:n_reactions(model)))
+"""
+    minimize_metabolic_adjustment(flux_ref_dict::Dict{String, Float64}) 
 
-    optimize!(opt_model)
-
-    is_solved(opt_model) || return nothing # MOMA failed
-
-    return opt_model
-end
+Overload of [`minimize_metabolic_adjustment`](@ref) that works with a
+dictionary of fluxes.
+"""
+minimize_metabolic_adjustment(flux_ref_dict::Dict{String,Float64}) =
+    (model, opt_model) ->
+        minimize_metabolic_adjustment([flux_ref_dict[rid] for rid in reactions(model)])(
+            model,
+            opt_model,
+        )
 
 """
-    minimize_metabolic_adjustment(
-        model::MetabolicModel,
-        flux_ref::Dict{String, Float64},
-        optimizer;
-        modifications = [],
-    )
-
-A variant of [`minimize_metabolic_adjustment`](@ref) that accepts a dictionary mapping
-reaction ids to reference fluxes instead of a vector.
-"""
-minimize_metabolic_adjustment(
-    model::MetabolicModel,
-    flux_ref::Dict{String,Float64},
-    optimizer;
-    modifications = [],
-) = minimize_metabolic_adjustment(
-    model,
-    [flux_ref[k] for k in reactions(model)],
-    optimizer;
-    modifications,
-)
-
-"""
-    minimize_metabolic_adjustment_vec(args...; kwargs...)
+    minimize_metabolic_adjustment_analysis_vec(args...; kwargs...)
 
 Perform minimization of metabolic adjustment (MOMA) and return a vector of fluxes in the
 same order as the reactions in `model`. Arguments are forwarded to
 [`minimize_metabolic_adjustment`](@ref) internally.
 """
-function minimize_metabolic_adjustment_vec(args...; kwargs...)
-    opt_model = minimize_metabolic_adjustment(args...; kwargs...)
+function minimize_metabolic_adjustment_analysis_vec(args...; kwargs...)
+    opt_model = minimize_metabolic_adjustment_analysis(args...; kwargs...)
 
     isnothing(opt_model) && return nothing
 
@@ -96,14 +97,18 @@ function minimize_metabolic_adjustment_vec(args...; kwargs...)
 end
 
 """
-    minimize_metabolic_adjustment_dict(args...; kwargs...)
+    minimize_metabolic_adjustment_analysis_dict(args...; kwargs...)
 
 Perform minimization of metabolic adjustment (MOMA) and return a dictionary mapping the
 reaction IDs to fluxes. Arguments are forwarded to [`minimize_metabolic_adjustment`](@ref)
 internally.
 """
-function minimize_metabolic_adjustment_dict(model::MetabolicModel, args...; kwargs...)
-    opt_fluxes = minimize_metabolic_adjustment_vec(model, args...; kwargs...)
+function minimize_metabolic_adjustment_analysis_dict(
+    model::MetabolicModel,
+    args...;
+    kwargs...,
+)
+    opt_fluxes = minimize_metabolic_adjustment_analysis_vec(model, args...; kwargs...)
 
     isnothing(opt_fluxes) && return nothing
 
