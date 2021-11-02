bb33214a4938216ad711ef676c460300da1bd258
St. Elmo <stelmozors@gmail.com>
2021-11-02 10:14:52 +0100

    add polish setting

--- test/analysis/minimize_metabolic_adjustment.jl
+++ test/analysis/minimize_metabolic_adjustment.jl
@@ -1,13 +1,21 @@
 @testset "MOMA" begin
     model = load_model(StandardModel, model_paths["e_coli_core.json"])
 
-    sol = parsimonious_flux_balance_analysis_dict(model, OSQP.Optimizer;)
+    sol = parsimonious_flux_balance_analysis_dict(
+        model,
+        OSQP.Optimizer;
+        modifications = [silence, change_optimizer_attribute("polish", true)],
+    )
 
     moma = minimize_metabolic_adjustment_analysis_dict(
         model,
         sol,
         OSQP.Optimizer;
-        modifications = [silence, change_constraint("CYTBD"; lb = 0.0, ub = 0.0)],
+        modifications = [
+            silence,
+            change_optimizer_attribute("polish", true),
+            change_constraint("CYTBD"; lb = 0.0, ub = 0.0),
+        ],
     )
 
     @test isapprox(moma["BIOMASS_Ecoli_core_w_GAM"], 0.06214149238730545, atol = 0.05)
