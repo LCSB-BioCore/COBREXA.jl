3e9c813e34361ff0ee437a79309d85ce49e6db82
St. Elmo <stelmozors@gmail.com>
2021-11-05 11:47:54 +0100

    remove unnecessary pFBA test + increase max iters

--- test/analysis/parsimonious_flux_balance_analysis.jl
+++ test/analysis/parsimonious_flux_balance_analysis.jl
@@ -10,19 +10,7 @@
         qp_modifications = [
             change_optimizer(OSQP.Optimizer),
             change_optimizer_attribute("polish", true),
-            silence,
-        ],
-    )
-    v = parsimonious_flux_balance_analysis_vec(
-        model,
-        Tulip.Optimizer;
-        modifications = [
-            change_constraint("EX_glc__D_e"; lb = -12, ub = -12),
-            change_optimizer_attribute("IPM_IterationsLimit", 500),
-        ],
-        qp_modifications = [
-            change_optimizer(OSQP.Optimizer),
-            change_optimizer_attribute("polish", true),
+            change_optimizer_attribute("max-iter", 10_000),
             silence,
         ],
     )
@@ -30,5 +18,4 @@
     # The used optimizer doesn't really converge to the same answer everytime
     # here, we therefore tolerate a wide range of results.
     @test isapprox(d["PGM"], -17.606459419216442, atol = QP_TEST_TOLERANCE)
-    @test isapprox(v[8], -17.606459419216442, atol = QP_TEST_TOLERANCE)
 end
