78f0d6bfeeb00d843a5b2e3fbf6de873eef1306d
Mirek Kratochvil <exa.exa@gmail.com>
2022-04-03 12:19:52 +0200

    make the model wrapping a bit more systematic

--- src/base/macros/model_wrapper.jl
+++ src/base/macros/model_wrapper.jl
@@ -1,3 +1,42 @@
+
+"""
+    _inherit_model_methods_impl(mtype::Symbol, arglist, access, fwdlist, fns...)
+
+A helper backend for [`@_inherit_model_methods`](@ref) and
+[`@_inherit_model_methods_fn`](@ref).
+"""
+function _inherit_model_methods_impl(
+    source,
+    mtype::Symbol,
+    arglist,
+    access,
+    fwdlist,
+    fns...,
+)
+    Expr(
+        :block,
+        (
+            begin
+                header = Expr(:call, fn, :(model::$mtype), arglist.args...)
+                call = Expr(:call, fn, access(:model), fwdlist.args...)
+                esc(
+                    Expr(
+                        :macrocall,
+                        Symbol("@doc"),
+                        source,
+                        """
+                            $header
+
+                        Evaluates [`$fn`](@ref) on the model contained in $mtype.
+                        """,
+                        Expr(:(=), header, Expr(:block, source, call)),
+                    ),
+                )
+            end for fn in fns
+        )...,
+    )
+end
+
 """
     @_inherit_model_methods
 
@@ -5,26 +44,29 @@ Generates trivial accessor functions listed in `fns` for a model that is
 wrapped in type `mtype` as field `member`.
 """
 macro _inherit_model_methods(mtype::Symbol, arglist, member::Symbol, fwdlist, fns...)
-    Expr(
-        :block,
-        (
-            begin
-                header = Expr(:call, fn, :(model::$mtype), arglist.args...)
-                call = Expr(:call, fn, :(model.$member), fwdlist.args...)
-                esc(
-                    Expr(
-                        :macrocall,
-                        Symbol("@doc"),
-                        __source__,
-                        """
-                            $header
+    _inherit_model_methods_impl(
+        __source__,
+        mtype,
+        arglist,
+        sym -> :($sym.$member),
+        fwdlist,
+        fns...,
+    )
+end
 
-                        Evaluates [`$fn`](@ref) on the model contained in $mtype.
-                        """,
-                        Expr(:(=), header, Expr(:block, __source__, call)),
-                    ),
-                )
-            end for fn in fns
-        )...,
+"""
+    @_inherit_model_methods_fn
+
+A more generic version of [`@_inherit_model_methods`](@ref) that accesses the
+"inner" model using an accessor function name.
+"""
+macro _inherit_model_methods_fn(mtype::Symbol, arglist, accessor, fwdlist, fns...)
+    _inherit_model_methods_impl(
+        __source__,
+        mtype,
+        arglist,
+        sym -> :($accessor($sym)),
+        fwdlist,
+        fns...,
     )
 end
