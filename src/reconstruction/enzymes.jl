3bcccccc3ad778fbc8fd287d905b81f030d7f090
Mirek Kratochvil <exa.exa@gmail.com>
2022-04-15 10:34:23 +0200

    regroup enzymatic model utilities, remove bounds-changing functions
    We might reintroduce bounds changing, but at the current state it its easier to
    change the bounds in the inner model and then just regenerate the top layer.

---
+++ src/reconstruction/enzymes.jl
@@ -0,0 +1,17 @@
+"""
+    with_smoment(; kwargs...)
+
+Specifies a model variant which adds extra semantics of the sMOMENT algorithm,
+giving a [`SMomentModel`](@ref). The arguments are forwarded to
+[`make_smoment_model`](@ref). Intended for usage with [`screen`](@ref).
+"""
+with_smoment(; kwargs...) = model -> make_smoment_model(model; kwargs...)
+
+"""
+    with_gecko(; kwargs...)
+
+Specifies a model variant which adds extra semantics of the Gecko algorithm,
+giving a [`GeckoModel`](@ref). The arguments are forwarded to
+[`make_gecko_model`](@ref). Intended for usage with [`screen`](@ref).
+"""
+with_gecko(; kwargs...) = model -> make_gecko_model(model; kwargs...)
