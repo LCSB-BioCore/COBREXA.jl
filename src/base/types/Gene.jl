f406440e8ae4256ecc57b0a306b90c48a5c15ea7
St. Elmo Wilken <stelmozors@gmail.com>
2022-04-21 22:34:40 +0200

    small fixes

--- src/base/types/Gene.jl
+++ src/base/types/Gene.jl
@@ -7,7 +7,6 @@ id :: String
 name :: Maybe{String}
 notes :: Dict{String, Vector{String}}
 annotation :: Dict{String, Union{Vector{String}, String}}
-molar_mass :: Maybe{Float64}
 ````
 """
 mutable struct Gene
@@ -15,13 +14,7 @@ mutable struct Gene
     name::Maybe{String}
     notes::Notes
     annotations::Annotations
-    molar_mass::Maybe{Float64}
 
-    Gene(
-        id::String = "";
-        name = nothing,
-        notes = Notes(),
-        annotations = Annotations(),
-        molar_mass = nothing,
-    ) = new(id, name, notes, annotations, molar_mass)
+    Gene(id::String = ""; name = nothing, notes = Notes(), annotations = Annotations()) =
+        new(id, name, notes, annotations)
 end
