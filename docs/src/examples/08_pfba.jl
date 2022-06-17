480dca745568518f3a03d7abf9b552e05877e26c
Mirek Kratochvil <exa.exa@gmail.com>
2022-06-17 10:41:22 +0200

    pFBA

--- docs/src/examples/08_pfba.jl
+++ docs/src/examples/08_pfba.jl
@@ -1,72 +1,65 @@
 # # Parsimonious flux balance analysis (pFBA)
 
-#md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/notebooks/@__NAME__.ipynb)
-#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/notebooks/@__NAME__.ipynb)
-
-# Here we will use [`flux_balance_analysis`](@ref), [`flux_variability_analysis`](@ref),
-# [`parsimonious_flux_balance_analysis`](@ref), and
-# [`minimize_metabolic_adjustment_analysis`](@ref), along with the modification functions of
-# `COBREXA.jl`, to analyze a toy model of *E. coli*.
-
-# If it is not already present, download the model.
+# Parsimonious flux balance analysis attempts to find a realistic flux of a
+# model, by trying to minimize squared sum of all fluxes while maintaining the
+# reached optimum. COBREXA.jl implements it in function
+# [`parsimonious_flux_balance_analysis`](@ref) (accompanied by vector- and
+# dictionary-returning variants
+# [`parsimonious_flux_balance_analysis_vec`](@ref) and
+# [`parsimonious_flux_balance_analysis_dict`](@ref)).
+#
+# As usual, we demonstrate the functionality on the *E. Coli* model:
 
 !isfile("e_coli_core.xml") &&
     download("http://bigg.ucsd.edu/static/models/e_coli_core.xml", "e_coli_core.xml")
 
-using COBREXA
-
-#md # !!! tip "Tip: use `?` to get quick help about functions"
-#md #       When you are unsure about how a function works, write `?
-#md #       function_name` to see the function reference documentation.
+using COBREXA, Tulip, OSQP
 
 model = load_model("e_coli_core.xml")
 
-# ## Optimization solvers in `COBREXA`
-#
-# To actually perform any optimization based analysis we need to load an
-# optimizer. Any [`JuMP.jl`-supported
-# optimizers](https://jump.dev/JuMP.jl/stable/installation/#Supported-solvers)
-# will work. Here, we will use [`Tulip.jl`](https://github.com/ds4dm/Tulip.jl)
-# to optimize linear programs and
-# [`OSQP.jl`](https://osqp.org/docs/get_started/julia.html) to optimize quadratic
-# programs.
+# Because the parsimonious objective is quadratic, we need a proper optimizer.
+# As the simplest choice, we can use
+# [`OSQP.jl`](https://osqp.org/docs/get_started/julia.html), but any any
+# [`JuMP.jl`-supported
+# optimizer](https://jump.dev/JuMP.jl/stable/installation/#Supported-solvers)
+# that supports quadratic programming will work.
 
 #md # !!! note "Note: OSQP can be sensitive"
-#md #       We recommend reading the docs of `OSQP` before using it, since
+#md #       We recommend reading the documentation of `OSQP` before using it, since
 #md #       it may give inconsistent results depending on what settings
 #md #       you use. Commercial solvers like `Gurobi`, `Mosek`, `CPLEX`, etc.
 #md #       require less user engagement.
 
-using Tulip, OSQP, GLPK
+# Running of basic pFBA is perfectly analogous to running of [FBA](TODO) and
+# other analyses. We add several modifications that improve the solution (using
+# functions [`silence`](@ref), and [`change_optimizer_attribute`](@ref)), and
+# fix the glucose exchange (using [`change_constraint`](@ref)) in order to get
+# a more reasonable result:
 
-# Parsimonious flux balance analysis (here in
-# [`parsimonious_flux_balance_analysis`](@ref) finds a unique flux solution
-# that minimizes the squared sum of fluxes of the system subject, while
-# maintaining the same objective value as the flux balance analysis solution.
-# Since we are optimizing a quadratic objective, we also need to switch to a
-# quadratic optimizer. In this case, OSQP will work. We demonstrate it on the
-# dictionary-returning variant of pFBA,
-# [`parsimonious_flux_balance_analysis_dict`](@ref):
-
-dict_soln = parsimonious_flux_balance_analysis_dict(
+fluxes = parsimonious_flux_balance_analysis_dict(
     model,
     OSQP.Optimizer;
     modifications = [
-        silence, # silence the optimizer (OSQP is very verbose by default)
-        change_constraint("R_EX_glc__D_e"; lb = -12, ub = -12),
-        change_optimizer_attribute("polish", true),
+        silence, # optionally silence the optimizer (OSQP is very verbose by default)
+        change_optimizer_attribute("polish", true), # tell OSQP to invest time into improving the precision of the solution
+        change_constraint("R_EX_glc__D_e"; lb = -12, ub = -12), # fix glucose consumption rate
     ],
 )
 
-# The function also has the expectable second variant that returns a vector of
-# solutions, in [`parsimonious_flux_balance_analysis_vec`](@ref). Here, we
-# utilize it to show how to use different optimizers for finding the optimum
-# and for solving the quadratic problem. That may be preferable if the
-# optimizer qualities differ for the differing tasks. pFBA allows you to
-# specify `qp_modifications` that are applied after the original optimum is
-# found, and before the quadratic part of the problem solving begins.
+# ## Using different optimizers for linear and quadratic problems
+#
+# It is quite useful to use specialized optimizers for specialized tasks in
+# pFBA. In particular, one would usually require to get a precise solution from
+# the linear programming part (where the precision is achievable), and trade
+# off a little precision for vast improvements in computation time in the
+# quadratic programming part.
+#
+# In pFBA, we can use the `modifications` and `qp_modifications` parameters to
+# switch and parametrize the solvers in the middle of the process, which allows
+# us to implement precisely that improvement. We demonstrate the switching on a
+# vector-returning variant of pFBA:
 
-vec_soln = parsimonious_flux_balance_analysis_vec(
+flux_vector = parsimonious_flux_balance_analysis_vec(
     model,
     Tulip.Optimizer; # start with Tulip
     modifications = [
