541bd936046302a1da750326602eb21dbaa491fd
St. Elmo Wilken <stelmozors@gmail.com>
2022-09-03 13:30:33 +0200

    format and change names

--- src/base/identifiers.jl
+++ src/base/identifiers.jl
@@ -3,8 +3,8 @@ This module uses annotation identifiers to characterize reactions, metabolites,
 genes, etc. If an subject has a matching annotation, then it is assumed that it
 is part of the associated class of objects. 
 """
-module Identifiers 
+module Identifiers
 
-    const EXCHANGE_REACTIONS = [SBOTerms.EXCHANGE_REACTION,]
+const EXCHANGE_REACTIONS = [SBOTerms.EXCHANGE_REACTION]
 
 end
