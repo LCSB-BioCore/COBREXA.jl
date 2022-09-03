968804bd2344b18a2741d06e9db78b660efc020d
stelmo <stelmo@users.noreply.github.com>
2022-09-03 13:35:46 +0000

    automatic formatting
    triggered by @stelmo on PR #667

--- src/base/identifiers.jl
+++ src/base/identifiers.jl
@@ -1,33 +1,33 @@
 """
 This module uses annotation identifiers to classify reactions, metabolites,
 genes, etc. If an subject has a matching annotation, then it is assumed that it
-is part of the associated class of objects. 
+is part of the associated class of objects.
 """
 module Identifiers
-    using ..COBREXA.SBOTerms
+using ..COBREXA.SBOTerms
 
-    const EXCHANGE_REACTIONS = [SBOTerms.EXCHANGE_REACTION]
+const EXCHANGE_REACTIONS = [SBOTerms.EXCHANGE_REACTION]
 
-    const TRANSPORT_REACTIONS = [
-        SBOTerms.TRANSPORT_REACTION,
-        SBOTerms.TRANSCELLULAR_MEMBRANE_INFLUX_REACTION,
-        SBOTerms.TRANSCELLULAR_MEMBRANE_EFFLUX_REACTION,
-    ]
+const TRANSPORT_REACTIONS = [
+    SBOTerms.TRANSPORT_REACTION,
+    SBOTerms.TRANSCELLULAR_MEMBRANE_INFLUX_REACTION,
+    SBOTerms.TRANSCELLULAR_MEMBRANE_EFFLUX_REACTION,
+]
 
-    const METABOLIC_REACTIONS = [SBOTerms.BIOCHEMICAL_REACTION]
+const METABOLIC_REACTIONS = [SBOTerms.BIOCHEMICAL_REACTION]
 
-    const BIOMASS_REACTIONS = [SBOTerms.BIOMASS_PRODUCTION]
+const BIOMASS_REACTIONS = [SBOTerms.BIOMASS_PRODUCTION]
 
-    const ATP_MAINTENANCE_REACTIONS = [SBOTerms.ATP_MAINTENANCE]
+const ATP_MAINTENANCE_REACTIONS = [SBOTerms.ATP_MAINTENANCE]
 
-    const PSEUDOREACTIONS = [
-        SBOTerms.EXCHANGE_REACTION,
-        SBOTerms.DEMAND_REACTION,
-        SBOTerms.BIOMASS_PRODUCTION,
-        SBOTerms.ATP_MAINTENANCE,
-        SBOTerms.PSEUDOREACTION,
-        SBOTerms.SINK_REACTION,
-    ]
+const PSEUDOREACTIONS = [
+    SBOTerms.EXCHANGE_REACTION,
+    SBOTerms.DEMAND_REACTION,
+    SBOTerms.BIOMASS_PRODUCTION,
+    SBOTerms.ATP_MAINTENANCE,
+    SBOTerms.PSEUDOREACTION,
+    SBOTerms.SINK_REACTION,
+]
 
-    const SPONTANEOUS_REACTIONS = [SBOTerms.SPONTANEOUS_REACTION]
+const SPONTANEOUS_REACTIONS = [SBOTerms.SPONTANEOUS_REACTION]
 end
