2de6d2f55edaab1e094b206c2bb1e858292f1551
St. Elmo Wilken <stelmozors@gmail.com>
2022-09-22 16:04:36 +0200

    get rid of autoloading

--- src/COBREXA.jl
+++ src/COBREXA.jl
@@ -51,39 +51,4 @@ include_dependency(joinpath(_PKG_ROOT_DIR, "Project.toml"))
 const COBREXA_VERSION =
     VersionNumber(Pkg.TOML.parsefile(joinpath(_PKG_ROOT_DIR, "Project.toml"))["version"])
 
-# autoloading
-const _inc(path...) = include(joinpath(path...))
-const _inc_all(dir) = _inc.(joinpath.(dir, filter(fn -> endswith(fn, ".jl"), readdir(dir))))
-
-_inc_all.(
-    joinpath.(
-        @__DIR__,
-        [
-            joinpath("base", "types", "abstract"),
-            joinpath("base", "logging"),
-            joinpath("base", "macros"),
-            joinpath("base", "types"),
-            joinpath("base", "types", "wrappers"),
-            joinpath("base", "ontologies"),
-            "base",
-            "io",
-            joinpath("io", "show"),
-            "reconstruction",
-            joinpath("reconstruction", "modifications"),
-            "analysis",
-            joinpath("analysis", "modifications"),
-            joinpath("analysis", "sampling"),
-            joinpath("base", "utils"),
-        ],
-    ),
-)
-
-# export everything that isn't prefixed with _ (inspired by JuMP.jl, thanks!)
-for sym in names(@__MODULE__, all = true)
-    if sym in [Symbol(@__MODULE__), :eval, :include] || startswith(string(sym), ['_', '#'])
-        continue
-    end
-    @eval export $sym
-end
-
 end # module
