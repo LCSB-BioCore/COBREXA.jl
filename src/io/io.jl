e139e582b34ad35d94ea69772a1e8b79aa557bed
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2022-10-25 14:26:13 +0200

    avoid shortcut arguments, clear out docs

--- src/io/io.jl
+++ src/io/io.jl
@@ -2,8 +2,9 @@
 $(TYPEDSIGNATURES)
 
 Generic function for loading models that chooses a specific loader function
-from the `ext` keyword, or throws an error.
-By default the `file_name` extension will be used.
+based on the `extension` argument (e.g., `".xml"` chooses loading of the SBML
+model format), or throws an error.  By default the extension from `file_name`
+is used.
 
 Currently, these model types are supported:
 
@@ -12,25 +13,28 @@ Currently, these model types are supported:
 - MATLAB models (`*.mat`, loaded with [`load_mat_model`](@ref))
 - HDF5 models (`*.h5`, loaded with [`load_h5_model`](@ref))
 """
-function load_model(file_name::String; ext = last(splitext(file_name)))::MetabolicModel
+function load_model(
+    file_name::String;
+    extension = last(splitext(file_name)),
+)::MetabolicModel
 
-    if ext == ".json"
+    if extension == ".json"
         return load_json_model(file_name)
-    elseif ext == ".xml"
+    elseif extension == ".xml"
         return load_sbml_model(file_name)
-    elseif ext == ".mat"
+    elseif extension == ".mat"
         return load_mat_model(file_name)
-    elseif ext == ".h5"
+    elseif extension == ".h5"
         return load_h5_model(file_name)
     else
-        throw(DomainError(ext, "Unknown file extension"))
+        throw(DomainError(extension, "Unknown file extension"))
     end
 end
 
 """
 $(TYPEDSIGNATURES)
 
-Helper function tht loads the model using [`load_model`](@ref) and return it
+Helper function that loads the model using [`load_model`](@ref) and returns it
 converted to `type`.
 
 # Example:
@@ -40,17 +44,17 @@ converted to `type`.
 function load_model(
     type::Type{T},
     file_name::String;
-    ext = last(splitext(file_name)),
+    extension = last(splitext(file_name)),
 )::T where {T<:MetabolicModel}
-    convert(type, load_model(file_name; ext))
+    convert(type, load_model(file_name; extension))
 end
 
 """
 $(TYPEDSIGNATURES)
 
-Generic function for saving models that chooses a specific writer function
-from the `ext` keyword, or throws an error.
-By default the `file_name` extension will be used.
+Generic function for saving models that chooses a specific writer function from
+the `extension` argument (such as `".xml"` for SBML format), or throws an
+error. By default the extension from `file_name` is used.
 
 Currently, these model types are supported:
 
@@ -62,17 +66,17 @@ Currently, these model types are supported:
 function save_model(
     model::MetabolicModel,
     file_name::String;
-    ext = last(splitext(file_name)),
+    extension = last(splitext(file_name)),
 )
-    if ext == ".json"
+    if extension == ".json"
         return save_json_model(model, file_name)
-    elseif ext == ".xml"
+    elseif extension == ".xml"
         return save_sbml_model(model, file_name)
-    elseif ext == ".mat"
+    elseif extension == ".mat"
         return save_mat_model(model, file_name)
-    elseif ext == ".h5"
+    elseif extension == ".h5"
         return save_h5_model(model, file_name)
     else
-        throw(DomainError(ext, "Unknown file extension"))
+        throw(DomainError(extension, "Unknown file extension"))
     end
 end
