c541f90ed4bcddf2152c67eabcf268c0c209fe2f
St. Elmo Wilken <stelmozors@gmail.com>
2022-12-12 12:21:47 +0100

    Update src/io/show/AbstractMetabolicModel.jl
    Co-authored-by: Mirek Kratochvil <exa.exa@gmail.com>

--- src/io/show/AbstractMetabolicModel.jl
+++ src/io/show/AbstractMetabolicModel.jl
@@ -7,6 +7,6 @@ function Base.show(io::Base.IO, ::MIME"text/plain", m::AbstractMetabolicModel)
     _pretty_print_keyvals(
         io,
         "",
-        "Metabolic model of type $(typeof(m)) with $(n_reactions(m)) reactions and $(n_metabolites(m)) metabolites.",
+        "$(typeof(m)){$(n_reactions(m)) reactions, $(n_metabolites(m)) metabolites}",
     )
 end
