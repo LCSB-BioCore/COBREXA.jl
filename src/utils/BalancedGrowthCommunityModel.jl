6592b2cb13a509723c72e3b0f77c0dedd836ec40
stelmo <stelmo@users.noreply.github.com>
2022-12-21 13:46:51 +0000

    automatic formatting
    triggered by @stelmo on PR #712

--- src/utils/BalancedGrowthCommunityModel.jl
+++ src/utils/BalancedGrowthCommunityModel.jl
@@ -5,9 +5,18 @@ Extract the solution of a specific `community_member` from `opt_model`, which is
 a solved optimization model built from the `community_model`. Removes the
 `community_member` prefix in the string ids of the returned dictionary.
 """
-get_solution(community_model::BalancedGrowthCommunityModel, opt_model, community_member::CommunityMember) = is_solved(opt_model) ? Dict(
-    string(last(split(rid, community_member.id*"#"))) => val for (rid, val) in zip(reactions(community_model), reaction_variables(community_model)' * value.(opt_model[:x])) if startswith(rid, community_member.id*"#")
-) : nothing
+get_solution(
+    community_model::BalancedGrowthCommunityModel,
+    opt_model,
+    community_member::CommunityMember,
+) =
+    is_solved(opt_model) ?
+    Dict(
+        string(last(split(rid, community_member.id * "#"))) => val for (rid, val) in zip(
+            reactions(community_model),
+            reaction_variables(community_model)' * value.(opt_model[:x]),
+        ) if startswith(rid, community_member.id * "#")
+    ) : nothing
 
 
 """
@@ -16,7 +25,11 @@ $(TYPEDSIGNATURES)
 Extract the environmental exchanges from `opt_model`, which is a solved
 optimization model built from the `community_model`.
 """
-get_environmental_exchanges(community_model::BalancedGrowthCommunityModel, opt_model) = is_solved(opt_model) ? Dict(
-    rid => val for (rid, val) in zip(reactions(community_model), reaction_variables(community_model)' * value.(opt_model[:x])) if !any(startswith(rid, cm.id*"#") for cm in community_model.members)
-) : nothing
-
+get_environmental_exchanges(community_model::BalancedGrowthCommunityModel, opt_model) =
+    is_solved(opt_model) ?
+    Dict(
+        rid => val for (rid, val) in zip(
+            reactions(community_model),
+            reaction_variables(community_model)' * value.(opt_model[:x]),
+        ) if !any(startswith(rid, cm.id * "#") for cm in community_model.members)
+    ) : nothing
