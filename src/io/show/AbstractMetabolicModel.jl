8e44a31426ab59d0b79877b98827c7ba44a6d586
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2023-01-03 11:35:02 +0100

    further vastly simplify the printing of metabolic models and their contents

--- src/io/show/AbstractMetabolicModel.jl
+++ src/io/show/AbstractMetabolicModel.jl
@@ -4,9 +4,8 @@ $(TYPEDSIGNATURES)
 Pretty printing of everything metabolic-modelish.
 """
 function Base.show(io::Base.IO, ::MIME"text/plain", m::AbstractMetabolicModel)
-    _pretty_print_keyvals(
+    print(
         io,
-        "",
-        "$(typeof(m)){$(n_reactions(m)) reactions, $(n_metabolites(m)) metabolites}",
+        "$(typeof(m))(#= $(n_reactions(m)) reactions, $(n_metabolites(m)) metabolites =#)",
     )
 end
