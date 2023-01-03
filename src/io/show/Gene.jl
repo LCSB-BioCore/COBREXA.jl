bbe05657aed9909a7c733a84287e272b5017a4e4
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2023-01-03 11:35:02 +0100

    further vastly simplify the printing of metabolic models and their contents

--- src/io/show/Gene.jl
+++
@@ -1,5 +0,0 @@
-function Base.show(io::Base.IO, ::MIME"text/plain", g::Gene)
-    for fname in fieldnames(Gene)
-        _pretty_print_keyvals(io, "Gene.$(string(fname)): ", getfield(g, fname))
-    end
-end
