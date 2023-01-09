980e15e6deac7ca082d31a5fe1eeb5c0fdfe94d6
St. Elmo Wilken <stelmozors@gmail.com>
2023-01-09 23:58:58 +0100

    Deprecate crowding constraints

---
+++ docs/src/examples/10_ADD_ANOTHER_EXAMPLE.jl
@@ -0,0 +1,14 @@
+# # FBA with ???
+
+# Let's starting with loading the models and packages.
+
+!isfile("e_coli_core.xml") &&
+    download("http://bigg.ucsd.edu/static/models/e_coli_core.xml", "e_coli_core.xml")
+
+using COBREXA, Tulip
+
+model = load_model("e_coli_core.xml")
+
+import Random
+Random.seed!(1) # for repeatability of random numbers below
+
