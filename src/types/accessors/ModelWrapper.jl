59b8567791bd60fe6ca3c0932c19e1dfb0ad52ec
St. Elmo Wilken <stelmozors@gmail.com>
2023-01-11 16:53:10 +0100

    add new accessors to wrappers

--- src/types/accessors/ModelWrapper.jl
+++ src/types/accessors/ModelWrapper.jl
@@ -16,8 +16,8 @@ end
 
 @inherit_model_methods_fn AbstractModelWrapper () unwrap_model () variables metabolites stoichiometry bounds balance objective reactions n_reactions reaction_variables coupling n_coupling_constraints coupling_bounds genes n_genes precache!
 
-@inherit_model_methods_fn AbstractModelWrapper (rid::String,) unwrap_model (rid,) reaction_gene_associations reaction_subsystem reaction_stoichiometry reaction_annotations reaction_notes
+@inherit_model_methods_fn AbstractModelWrapper (rid::String,) unwrap_model (rid,) reaction_gene_associations reaction_subsystem reaction_stoichiometry reaction_annotations reaction_notes reaction_isozymes
 
 @inherit_model_methods_fn AbstractModelWrapper (mid::String,) unwrap_model (mid,) metabolite_formula metabolite_charge metabolite_compartment metabolite_annotations metabolite_notes
 
-@inherit_model_methods_fn AbstractModelWrapper (gid::String,) unwrap_model (gid,) gene_annotations gene_notes
+@inherit_model_methods_fn AbstractModelWrapper (gid::String,) unwrap_model (gid,) gene_annotations gene_notes gene_product_molar_mass gene_product_lower_bound gene_product_upper_bound
