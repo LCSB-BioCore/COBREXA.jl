cd589ce34574d565ec6f959fab921d8dc875ab34
St. Elmo Wilken <stelmozors@gmail.com>
2023-01-27 16:45:09 +0100

    fix ordering issue in GRRs manually

--- test/analysis/gecko.jl
+++ test/analysis/gecko.jl
@@ -1,6 +1,24 @@
 @testset "GECKO" begin
     model = load_model(StandardModel, model_paths["e_coli_core.json"])
 
+        # fix ordering
+        model.reactions["ATPS4r"].grr = [
+            ["b3736", "b3737", "b3738", "b3731", "b3732", "b3733", "b3734", "b3735"],
+            ["b3736", "b3737", "b3738", "b3731", "b3732", "b3733", "b3734", "b3735", "b3739"]
+        ]
+        # model.reactions["GLCpts"].grr = [
+        #     ["b2417", "b1101", "b2415", "b2416"],
+        #     ["b1817", "b1818", "b1819", "b2415", "b2416"],
+        #     ["b2417", "b1621", "b2415", "b2416"],
+        # ]
+    
+        model.reactions["GLCpts"].grr = [
+            ["b2417", "b1621", "b2415", "b2416"],
+            ["b1817", "b1818", "b1819", "b2415", "b2416"],
+            ["b2417", "b1101", "b2415", "b2416"],
+        ]
+    
+
     get_reaction_isozymes =
         rid ->
             haskey(ecoli_core_reaction_kcats, rid) ?
--- test/analysis/smoment.jl
+++ test/analysis/smoment.jl
@@ -3,6 +3,24 @@
 
     get_gene_product_mass = gid -> get(ecoli_core_gene_product_masses, gid, 0.0)
 
+    # fix ordering
+    model.reactions["ATPS4r"].grr = [
+        ["b3736", "b3737", "b3738", "b3731", "b3732", "b3733", "b3734", "b3735"],
+        ["b3736", "b3737", "b3738", "b3731", "b3732", "b3733", "b3734", "b3735", "b3739"]
+    ]
+    # model.reactions["GLCpts"].grr = [
+    #     ["b2417", "b1101", "b2415", "b2416"],
+    #     ["b1817", "b1818", "b1819", "b2415", "b2416"],
+    #     ["b2417", "b1621", "b2415", "b2416"],
+    # ]
+
+    model.reactions["GLCpts"].grr = [
+        ["b2417", "b1621", "b2415", "b2416"],
+        ["b1817", "b1818", "b1819", "b2415", "b2416"],
+        ["b2417", "b1101", "b2415", "b2416"],
+    ]
+
+
     get_reaction_isozyme =
         rid ->
             haskey(ecoli_core_reaction_kcats, rid) ?
