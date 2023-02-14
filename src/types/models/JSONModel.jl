1915296d328c3116412cfaff121db83b6cdeb3b3
St. Elmo Wilken <stelmozors@gmail.com>
2023-02-14 16:05:38 +0100

    partially extend notes to JSONModel

--- src/types/models/JSONModel.jl
+++ src/types/models/JSONModel.jl
@@ -322,7 +322,7 @@ function Base.convert(::Type{JSONModel}, mm::AbstractMetabolicModel)
     ocs = objective(mm)
 
     json = Dict{String,Any}()
-    json["id"] = "model" # default
+    json["id"] = get(model_notes, "id", "missing model ID")
 
     json[first(constants.keynames.genes)] = [
         Dict([
