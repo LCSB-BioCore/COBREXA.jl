c6f06e05f4cac1d5b67f145f5f79c902fa5efe07
St. Elmo Wilken <stelmozors@gmail.com>
2023-02-15 16:06:37 +0100

    small fix for JSONModel

--- src/types/models/JSONModel.jl
+++ src/types/models/JSONModel.jl
@@ -304,9 +304,9 @@ Return the name of gene with ID `gid`.
 Accessors.gene_name(model::JSONModel, gid::String) =
     get(model.genes[model.gene_index[gid]], "name", nothing)
 
-Accessors.model_annotations(model::JSONModel)::Annotations = get(model["annotation"])
+Accessors.model_annotations(model::JSONModel)::Annotations = get(model.json, "annotation", Annotations())
 
-Accessors.model_notes(model::JSONModel)::Notes = get(model["notes"])
+Accessors.model_notes(model::JSONModel)::Notes = get(model.json, "notes", Notes())
 
 """
 $(TYPEDSIGNATURES)
