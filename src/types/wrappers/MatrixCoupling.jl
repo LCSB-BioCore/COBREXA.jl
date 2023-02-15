c58a1d24122c755f0ea2f5ad25acb5bdb39ed8b9
St. Elmo Wilken <stelmozors@gmail.com>
2023-02-15 23:28:47 +0100

    remove docstrings for MatrixCoupling

--- src/types/wrappers/MatrixCoupling.jl
+++ src/types/wrappers/MatrixCoupling.jl
@@ -34,30 +34,22 @@ end
 
 """
 $(TYPEDSIGNATURES)
-
-Get the internal [`MatrixModel`](@ref) out of [`MatrixCoupling`](@ref).
 """
 Accessors.unwrap_model(a::MatrixCoupling) = a.lm
 
 """
 $(TYPEDSIGNATURES)
-
-Coupling constraint matrix for a `MatrixCoupling`.
 """
 Accessors.coupling(a::MatrixCoupling)::SparseMat = vcat(coupling(a.lm), a.C)
 
 """
 $(TYPEDSIGNATURES)
-
-The number of coupling constraints in a `MatrixCoupling`.
 """
 Accessors.n_coupling_constraints(a::MatrixCoupling)::Int =
     n_coupling_constraints(a.lm) + size(a.C, 1)
 
 """
 $(TYPEDSIGNATURES)
-
-Coupling bounds for a `MatrixCoupling`.
 """
 Accessors.coupling_bounds(a::MatrixCoupling)::Tuple{Vector{Float64},Vector{Float64}} =
     vcat.(coupling_bounds(a.lm), (a.cl, a.cu))
