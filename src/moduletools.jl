ea0ce570c4d84e13f7950deac1683d2c903c8fed
St. Elmo Wilken <stelmozors@gmail.com>
2023-02-19 15:50:10 +0100

    revert const in reexport

--- src/moduletools.jl
+++ src/moduletools.jl
@@ -50,7 +50,7 @@ macro reexport(mods...)
             Base.isexported($modulename, sym) || continue
             typeof($(Expr(:., modulename, :sym))) == Module && continue
             sym in [:eval, :include] && continue
-            @eval $(Expr(:$, :sym)) = ($modulename).$(Expr(:$, :sym))
+            @eval const $(Expr(:$, :sym)) = ($modulename).$(Expr(:$, :sym))
             @eval export $(Expr(:$, :sym))
         end
     end)
