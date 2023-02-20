dabbe840a130f41693307836f448b90df5a42d05
stelmo <stelmo@users.noreply.github.com>
2023-02-20 16:13:43 +0000

    automatic formatting
    triggered by @stelmo on PR #747

--- src/types/wrappers/ParsimoniousModel.jl
+++ src/types/wrappers/ParsimoniousModel.jl
@@ -12,9 +12,9 @@ This is used to implement [`parsimonious_flux_balance_analysis`](@ref).
 
 # Example
 ```
-res = model |> 
-    with_changed_bound("biomass", lower_bound = 0.1) |> 
-    with_parsimonious_solution(:enzymes) |> 
+res = model |>
+    with_changed_bound("biomass", lower_bound = 0.1) |>
+    with_parsimonious_solution(:enzymes) |>
     flux_balance_analysis(Clarabel.Optimizer)
 ```
 """
@@ -37,10 +37,10 @@ Accessors.unwrap_model(m::ParsimoniousModel) = m.inner
 $(TYPEDSIGNATURES)
 
 Return a negative, uniformly weighted, quadratic-only objective representing the
-squared sum of `model.var_ids`. 
+squared sum of `model.var_ids`.
 """
 function Accessors.objective(model::ParsimoniousModel)::SparseMat
-    obj = spzeros(n_variables(model), n_variables(model) + 1) # + 1 for QP solver formulation 
+    obj = spzeros(n_variables(model), n_variables(model) + 1) # + 1 for QP solver formulation
 
     idxs = indexin(model.var_ids, variables(model))
     j = findfirst(isnothing, idxs)
