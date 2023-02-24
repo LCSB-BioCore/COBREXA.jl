63742843c2025207b38ff9e06885ae9512fb4807
St. Elmo Wilken <stelmozors@gmail.com>
2023-02-24 11:38:58 +0100

    Update src/io/show/ModelWithResult.jl
    Co-authored-by: Mirek Kratochvil <exa.exa@gmail.com>

--- src/io/show/ModelWithResult.jl
+++ src/io/show/ModelWithResult.jl
@@ -1,5 +1,3 @@
-function Base.show(io::Base.IO, ::MIME"text/plain", res::ModelWithResult)
-    println(io, "A ModelWithResult composed of:")
-    println(io, "model: $(typeof(res.model))")
-    println(io, "result: $(typeof(res.result))")
+function Base.show(io::Base.IO, ::MIME"text/plain", res::ModelWithResult{T}) where T
+    println(io, "ModelWithResult{$T}($(res.model), ...)")
 end
