dad432b29a63049ce1cfe2e951ded8e648bb83e1
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2023-09-22 10:58:17 +0200

    format up

--- src/builders/enzymes.jl
+++ src/builders/enzymes.jl
@@ -7,7 +7,8 @@
 # - anyone is probably able to do a partial sum over the above things themselves, we should make sure they squash well
 
 #TODO: this does not need the variables allocated, it's just a fancy product
-enzyme_mass_sum(; forward_fluxes, reverse_fluxes, enzymes, reaction_enzyme_association) = missing
+enzyme_mass_sum(; forward_fluxes, reverse_fluxes, enzymes, reaction_enzyme_association) =
+    missing
 
 isozyme_mass_mapping(; forward_fluxes, reverse_fluxes, isozymes, ...) = missing
 isozyme_mass_group_mapping(; forward_fluxes, reverse_fluxes, isozymes, ...) = missing
--- src/builders/genes.jl
+++ src/builders/genes.jl
@@ -2,14 +2,12 @@
 #TODO maybe separate this into simple boolean eval function and actual builder
 knockout_constraint(ko_genes; fluxes::SolutionTree, reaction_gene_association) =
     C.ConstraintTree(
-        rxn => C.Constraint(C.value(fluxes[rxn]), 0.0)
-        for rxn=keys(fluxes)
-        if begin
-            gss=reaction_gene_association(rxn)
+        rxn => C.Constraint(C.value(fluxes[rxn]), 0.0) for rxn in keys(fluxes) if begin
+            gss = reaction_gene_association(rxn)
             if isnothing(gss)
                 false
             else
-                all(gs -> any(g-> g in ko_genes, gs), gss)
+                all(gs -> any(g -> g in ko_genes, gs), gss)
             end
         end
     )
