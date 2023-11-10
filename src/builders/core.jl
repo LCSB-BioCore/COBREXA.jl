e877a1b51299050bf9a10cb57ea09618c0e0c74a
St. Elmo Wilken <stelmozors@gmail.com>
2023-11-10 12:32:43 +0100

    small fixes to core

--- src/builders/core.jl
+++ src/builders/core.jl
@@ -1,26 +1,29 @@
 
+import AbstractFBCModels as F
+import SparseArrays: sparse
+
 """
 $(TYPEDSIGNATURES)
 
 A constraint tree that models the content of the given instance of
 `AbstractFBCModel`.
 """
-function fbc_model_constraints(model::A.AbstractFBCModel)
-    rxns = Symbol.(A.reactions(model))
-    mets = Symbol.(A.metabolites(model))
-    lbs, ubs = A.bounds(model)
-    stoi = A.stoichiometry(model)
-    bals = A.balance(model)
-    obj = A.objective(model)
+function fbc_model_constraints(model::F.AbstractFBCModel)
+    rxns = Symbol.(F.reactions(model))
+    mets = Symbol.(F.metabolites(model))
+    lbs, ubs = F.bounds(model)
+    stoi = F.stoichiometry(model)
+    bal = F.balance(model)
+    obj = F.objective(model)
 
     #TODO: is sparse() required below?
     return C.ConstraintTree(
-        :fluxes => C.variables(keys = rxns, bounds = zip(lbs, ubs)),
-        :balances => C.ConstraintTree(
-            m => C.Constraint(value = C.LinearValue(sparse(row)), bound = b) for
-            (m, row, b) in zip(mets, eachrow(stoi), bals)
-        ),
-        :objective => C.Constraint(value = C.LinearValue(sparse(obj))),
+        :fluxes^C.variables(keys = rxns, bounds = zip(lbs, ubs)) *
+        :flux_stoichiometry^C.ConstraintTree(
+            met => C.Constraint(value = C.LinearValue(sparse(row)), bound = b) for
+            (met, row, b) in zip(mets, eachrow(stoi), bal)
+        ) *
+        :objective^C.Constraint(C.LinearValue(sparse(obj))),
     )
 end
 
@@ -67,11 +70,26 @@ sign_split_constraints(;
     signed::C.ConstraintTree,
 ) = C.ConstraintTree(
     k => C.Constraint(
-        value = s + (haskey(negative, k) ? C.value(negative[k]) : zero(C.Value)) -
-                (haskey(positive, k) ? C.value(positive[k]) : zero(C.Value)),
+        value = s.value +
+                (haskey(negative, k) ? negative[k].value : zero(typeof(s.value))) -
+                (haskey(positive, k) ? positive[k].value : zero(typeof(s.value))),
         bound = 0.0,
-    ) for (k, s) in C.elems(signed)
+    ) for (k, s) in signed
 )
 #TODO the example above might as well go to docs
 
 export sign_split_constraints
+
+function fluxes_in_direction(fluxes::C.ConstraintTree, direction = :forward)
+    keys = Symbol[]
+    for (id, flux) in fluxes
+        if direction == :forward
+            last(flux.bound) > 0 && push!(keys, id)
+        else
+            first(flux.bound) < 0 && push!(keys, id)
+        end
+    end
+    C.variables(; keys, bounds = Ref((0.0, Inf)))
+end
+
+export fluxes_in_direction
