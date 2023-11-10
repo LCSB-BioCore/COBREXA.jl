7d99991d2e4cf56c58c6796089e0f796a1ed02c9
St. Elmo Wilken <stelmozors@gmail.com>
2023-11-10 12:15:35 +0100

    get cobrexa to load

--- src/builders/genes.jl
+++ src/builders/genes.jl
@@ -1,19 +1,18 @@
 
-knockout_constraints(; fluxes::C.ConstraintTree, knockout_test) = C.ConstraintTree(
-    rxn => C.Constraint(C.value(fluxes[rxn]), 0.0) for
-    rxn in keys(fluxes) if knockout_test(rxn)
-)
+knockout_constraints(;fluxes::C.ConstraintTree, knockout_test) =
+    C.ConstraintTree(
+        rxn => C.Constraint(C.value(fluxes[rxn]), 0.0) for rxn in keys(fluxes) if knockout_test(rxn)
+    )
 
 export knockout_constraints
 
-fbc_gene_knockout_constraints(;
-    fluxes::C.ConstraintTree,
-    genes,
-    fbc_model::A.AbstractFBCModel,
-) = knockout_constraints(;
+fbc_gene_knockout_constraints(;fluxes::C.ConstraintTree, genes, fbc_model::A.AbstractFBCModel) = knockout_constraints(;
     fluxes,
-    knockout_test = rxn ->
-        !A.reaction_gene_products_available(rxn, g -> not(g in genes)),
+    knockout_test =
+        rxn -> !A.reaction_gene_products_available(
+            rxn,
+            g -> not(g in genes)
+        ),
 )
 
 export fbc_gene_knockout_constraints
