255ac7cfc5adf2c3751c636704eafbafe04fd5a7
St. Elmo Wilken <stelmozors@gmail.com>
2023-12-16 19:48:45 +0100

    implement reviews and improve docs

--- src/builders/thermodynamic.jl
+++ src/builders/thermodynamic.jl
@@ -1,81 +1,52 @@
 """
 $(TYPEDSIGNATURES)
 
-The function uses the supplied `optimizer` and
-`reaction_standard_gibbs_free_energies`. Optionally, `flux_solution` can be used
-to set the directions of each reaction in `model` (all reactions are assumed to
-proceed forward and are active by default). The supplied `flux_solution` should
-be free of internal cycles i.e. thermodynamically consistent. This optional
-input is important if a reaction in `model` normally runs in reverse (negative
-flux). Note, reactions in `flux_solution` that are smaller than `small_flux_tol`
-are also ignored in the analysis function (for numerical stability).
-
-
-Reactions specified in `ignore_reaction_ids` are internally ignored when
-calculating the max-min driving force. This should include water and proton
-importers.
-
-Since biochemical thermodynamics are assumed, the `proton_ids` and `water_ids`
-need to be specified so that they can be ignored in the calculations.
-Effectively this assumes an aqueous environment at constant pH is used.
-
-`constant_concentrations` is used to fix the concentrations of certain
-metabolites (such as CO₂). `concentration_ratios` is used to specify additional
-constraints on metabolite pair concentrations (typically, this is done with
-various cofactors such as the ATP/ADP ratio. For example, you can fix the
-concentration of ATP to be always 5× higher than of ADP by specifying
-`Dict(("ATP","ADP") => 5.0)`
-
-`concentration_lb` and `concentration_ub` set default concentration bounds, in M
-by default.
-
-`T` and `R` can be specified in the corresponding units; defaults are K and
-kJ/K/mol.
+Build a max min driving force analysis model. See the docstring of
+[`max_min_driving_force_analysis`](@ref) for details about the arguments.
 """
 function build_max_min_driving_force_model(
     model::A.AbstractFBCModel,
     reaction_standard_gibbs_free_energies::Dict{String,Float64};
-    flux_solution = Dict{String,Float64}(),
-    proton_ids = ["h_c", "h_e"],
-    water_ids = ["h2o_c", "h2o_e"],
-    concentration_lb = 1e-9,
-    concentration_ub = 100e-3,
+    reference_flux = Dict{String,Float64}(),
+    proton_ids::Vector{String},
+    water_ids::Vector{String},
+    concentration_lb = 1e-9, # M
+    concentration_ub = 1e-1, # M
     T = 298.15, # Kelvin
     R = 8.31446261815324e-3, # kJ/K/mol
     ignore_reaction_ids = String[],
 )
     # check if reactions that will be used in the model all have thermodynamic data, otherwise throw error.
     # only these reactions will form part of the model.
-    rxns = filter(
-        x -> !(x in ignore_reaction_ids),
-        isempty(flux_solution) ? A.reactions(model) : collect(keys(flux_solution)),
-    )
-    all(in.(rxns, Ref(collect(keys(reaction_standard_gibbs_free_energies))))) || throw(
-        ArgumentError("""
+    rxn_source =
+        isempty(reference_flux) ? A.reactions(model) : collect(keys(reference_flux))
+    rxns = Set(x for x in rxn_source if !(x in ignore_reaction_ids))
+    isempty(setdiff(rxns, keys(reaction_standard_gibbs_free_energies))) || throw(
+        DomainError(
+            reaction_standard_gibbs_free_energies,
+            """
                       Not all reactions have thermodynamic data. 
                       Either add the reactions with missing ΔG0s to ignore_reaction_ids, 
                       or add the data to reaction_standard_gibbs_free_energies.
-                      """),
+                      """,
+        ),
     )
+    rxns = collect(rxns)
 
     # import metabolite ids (if used), and reaction stoichiometries from an AbstractFBCModel
-    mets = Set(Symbol[])
-    for rid in rxns
-        for met in keys(A.reaction_stoichiometry(model, rid))
-            push!(mets, Symbol(met))
-        end
-    end
-    mets = [m for m in mets] # TODO: constrainttrees #12
+    mets = Set(m for rid in rxns for m in keys(A.reaction_stoichiometry(model, rid)))
+    mets = collect(mets)
     stoi = A.stoichiometry(model)
 
     # create thermodynamic variables
-    m = C.ConstraintTree(:max_min_driving_force^C.variable())
-    m +=
+    m = C.ConstraintTree(
+        :max_min_driving_force^C.variable() +
         :log_metabolite_concentrations^C.variables(
-            keys = mets,
+            keys = Symbol.(mets),
             bounds = zip(log(concentration_lb), log(concentration_ub)),
-        )
-    m += :delta_G_reactions^C.variables(keys = Symbol.(rxns))
+        ) +
+        :delta_G_reactions^C.variables(keys = Symbol.(rxns)),
+    )
 
     #= 
     Build gibbs free energy relations
@@ -83,27 +54,27 @@ function build_max_min_driving_force_model(
     =#
     model_rxns = A.reactions(model)
     model_mets = A.metabolites(model)
-    for rxn in rxns
+    dG0s_met_ids_stoichs = Vector{Tuple{Float64,Vector{String},Vector{Float64}}}()
+    for rxn in rxns # prepare to create CT below
         met_idxs, stoich_coeffs = findnz(stoi[:, findfirst(==(rxn), model_rxns)])
         met_ids = model_mets[met_idxs]
-
         dG0 = reaction_standard_gibbs_free_energies[rxn]
+        push!(dG0s_met_ids_stoichs, (dG0, met_ids, stoich_coeffs))
+    end
 
-        m *=
-            :delta_G_reaction_equations^Symbol(
-                rxn,
-            )^C.Constraint(
+    m *=
+        :delta_G_reaction_equations^C.ConstraintTree(
+            Symbol(rxn) => C.Constraint(
                 value = -m.delta_G_reactions[Symbol(rxn)].value +
                         dG0 +
                         R *
                         T *
                         sum(
-                            m.log_metabolite_concentrations[Symbol(met_id)].value *
-                            stoich for (met_id, stoich) in zip(met_ids, stoich_coeffs)
+                            m.log_metabolite_concentrations[Symbol(met_id)].value * stoich for (met_id, stoich) in zip(met_ids, stoich_coeffs)
                         ),
                 bound = 0.0,
-            )
-    end
+            ) for (rxn, (dG0, met_ids, stoich_coeffs)) in zip(rxns, dG0s_met_ids_stoichs)
+        )
 
     #= 
     Set proton log concentration to zero so that it won't impact any
@@ -111,54 +82,72 @@ function build_max_min_driving_force_model(
     to zero (aqueous condition assumptions). How true is "aqueous conditions"? 
     Debatable... 
     =#
-    log_met_conc_zero = [Symbol.(proton_ids); Symbol.(water_ids)]
-    for met in keys(m.log_metabolite_concentrations)
-        if met in log_met_conc_zero
-            m.log_metabolite_concentrations[met].bound = 0.0
-        end
+    for met in [Symbol.(proton_ids); Symbol.(water_ids)]
+        haskey(m.log_metabolite_concentrations, met) &&
+            (m.log_metabolite_concentrations[met].bound = 0.0)
     end
 
     #= 
     Add thermodynamic feasibility constraint (ΔG < 0 for a feasible reaction in flux direction).
     Add objective constraint to solve max min problem.
     =#
-    for rxn in rxns
-        m *=
-            :thermodynamic_feasibility^Symbol(
-                rxn,
-            )^C.Constraint(
+    m *=
+        :reaction_delta_G_margin^C.ConstraintTree(
+            Symbol(rxn) => C.Constraint(
                 value = m.delta_G_reactions[Symbol(rxn)].value *
-                        sign(get(flux_solution, rxn, 1.0)),
+                        sign(get(reference_flux, rxn, 1.0)),
                 bound = (-Inf, 0.0),
-            )
+            ) for rxn in rxns
+        )
 
-        m *=
-            :max_min_constraints^Symbol(
-                rxn,
-            )^C.Constraint(
+    m *=
+        :min_driving_force_margin^C.ConstraintTree(
+            Symbol(rxn) => C.Constraint(
                 value = m.max_min_driving_force.value +
                         m.delta_G_reactions[Symbol(rxn)].value *
-                        sign(get(flux_solution, rxn, 1.0)),
+                        sign(get(reference_flux, rxn, 1.0)),
                 bound = (-Inf, 0.0),
-            )
-    end
+            ) for rxn in rxns
+        )
 
     m
 end
 
 export build_max_min_driving_force_model
 
-function add_metabolite_ratio_constraints!(
-    m::C.ConstraintTree,
-    concentration_ratios::Dict{String,Tuple{String,String,Float64}},
+
+"""
+$(TYPEDSIGNATURES)
+
+Add constraints to `m` that represents ratios of variables in log space:
+`log(x/y) = log(const)` where `x` and `y` are variables specified by `on`. These
+constraints are called `name` in the resultant model. The constraints are
+specified by `ratios`, which is a dictionary mapping a constraint id to a tuple
+which consists of the variable ids, `x`, `y`, and the ratio value, `const`. The
+latter is logged internally.
+
+# Example
+```
+m = add_ratio_constraints(
+    m,
+    Dict("atp" => ("atp_c", "adp_c", log(10.0)), "nadh" => ("nadh_c", "nad_c", log(0.13)));
+    name = :metabolite_ratio_constraints,
+    on = m.log_metabolite_concentrations,
 )
-    for (constraint_id, (met1, met2, ratio)) in concentration_ratios
+```
+"""
+function add_log_ratio_constraints(
+    m::C.ConstraintTree,
+    ratios::Dict{String,Tuple{String,String,Float64}};
+    name::Symbol,
+    on::C.ConstraintTree,
+)
+    for (cid, (var1, var2, ratio)) in ratios
         m *=
-            :metabolite_ratio_constraints^Symbol(
-                constraint_id,
+            name^Symbol(
+                cid,
             )^C.Constraint(
-                value = m.log_metabolite_concentrations[Symbol(met1)].value -
-                        m.log_metabolite_concentrations[Symbol(met2)].value,
+                value = on[Symbol(var1)].value - on[Symbol(var2)].value,
                 bound = log(ratio),
             )
     end
@@ -166,65 +155,73 @@ function add_metabolite_ratio_constraints!(
     m
 end
 
-export add_metabolite_ratio_constraints!
+export add_log_ratio_constraints
+
 
 """
 $(TYPEDSIGNATURES)
 
-Perform a max-min driving force analysis on the `model`, as defined by Noor, et al.,
-"Pathway thermodynamics highlights kinetic obstacles in central metabolism.", PLoS
-computational biology, 2014.
+Perform a max-min driving force analysis using `optimizer` on the `model` with
+supplied ΔG⁰s in `reaction_standard_gibbs_free_energies`, as defined by Noor, et
+al., "Pathway thermodynamics highlights kinetic obstacles in central
+metabolism.", PLoS computational biology, 2014.
 
-The function uses the supplied `optimizer` and `reaction_standard_gibbs_free_energies`.
-Optionally, `flux_solution` can be used to set the directions of each reaction in `model`
-(all reactions are assumed to proceed forward and are active by default). The supplied
-`flux_solution` should be free of internal cycles i.e. thermodynamically consistent. This
-optional input is important if a reaction in `model` normally runs in reverse (negative
-flux). Note, reactions in `flux_solution` that are smaller than `small_flux_tol` are also
-ignored in the analysis function (for numerical stability).
+Optionally, `reference_flux` can be used to set the directions of each reaction
+in `model` (all reactions are assumed to proceed forward by default). The
+supplied `reference_flux` should be free of internal cycles i.e.
+thermodynamically consistent. This optional input is important if a reaction in
+`model` normally runs in reverse (negative flux). Note, only the signs are
+extracted from this input, so be careful with floating point precision near 0.
 
-The max-min driving force algorithm returns the Gibbs free energy of the reactions, the
-concentrations of metabolites and the actual maximum minimum driving force. The optimization
-problem solved is:
+The max-min driving force algorithm returns the Gibbs free energy of the
+reactions, the concentrations of metabolites and the actual maximum minimum
+driving force. The optimization problem solved is:
 ```
 max min -ΔᵣG
 s.t. ΔᵣG = ΔrG⁰ + R T S' ln(C)
      ΔᵣG ≤ 0
      ln(Cₗ) ≤ ln(C) ≤ ln(Cᵤ)
 ```
-where `ΔrG` are the Gibbs energies dissipated by the reactions, R is the gas constant, T is
-the temperature, S is the stoichiometry of the model, and C is the vector of metabolite
-concentrations (and their respective lower and upper bounds).
+where `ΔrG` are the Gibbs energies dissipated by the reactions, R is the gas
+constant, T is the temperature, S is the stoichiometry of the model, and C is
+the vector of metabolite concentrations (and their respective lower and upper
+bounds).
 
 In case no feasible solution exists, `nothing` is returned.
 
-Reactions specified in `ignore_reaction_ids` are internally ignored when calculating the
-max-min driving force. This should include water and proton importers.
+Reactions specified in `ignore_reaction_ids` are internally ignored when
+calculating the max-min driving force. Importantly, this should include water
+and proton importers.
 
-Since biochemical thermodynamics are assumed, the `proton_ids` and `water_ids` need to be
-specified so that they can be ignored in the calculations. Effectively this assumes an
-aqueous environment at constant pH is used.
+Since biochemical thermodynamics are assumed, the `proton_ids` and `water_ids`
+need to be specified so that they can be ignored in the calculations.
+Effectively this assumes an aqueous environment at constant pH is used.
 
-`constant_concentrations` is used to fix the concentrations of certain metabolites (such as
-CO₂). `concentration_ratios` is used to specify additional constraints on metabolite pair
-concentrations (typically, this is done with various cofactors such as the ATP/ADP ratio.
-For example, you can fix the concentration of ATP to be always 5× higher than of ADP by
-specifying `Dict(("ATP","ADP") => 5.0)`
+`constant_concentrations` is used to fix the concentrations of certain
+metabolites (such as CO₂) by passing a dictionary mapping metabolite id to its
+constant value. `concentration_ratios` is used to specify additional constraints
+on metabolite pair concentrations (typically, this is done with various
+cofactors, such as the ATP/ADP ratio. For example, you can fix the concentration
+of ATP to be always 5× higher than of ADP by specifying `Dict("atp_ratio" =>
+("atp_c","adp_c", 5.0))` if these metabolites are called `atp_c` and `adp_c` in
+the model. `concentration_lb` and `concentration_ub` set the `Cₗ` and `Cᵤ` in
+the optimization problems (these are overwritten by `constant_concentrations` if
+supplied).
 
-`concentration_lb` and `concentration_ub` set the `Cₗ` and `Cᵤ` in the
-optimization problems.
-
-`T` and `R` can be specified in the corresponding units; defaults are K and kJ/K/mol.
+`T` and `R` can be specified in the corresponding units; defaults are K and
+kJ/K/mol. The unit of metabolite concentrations is typically molar. As usual,
+optimizer settings can be changed with `modifications`.
 """
 function max_min_driving_force_analysis(
     model::A.AbstractFBCModel,
     reaction_standard_gibbs_free_energies::Dict{String,Float64};
-    flux_solution = Dict{String,Float64}(),
+    reference_flux = Dict{String,Float64}(),
     concentration_ratios = Dict{String,Tuple{String,String,Float64}}(),
-    proton_ids = ["h_c", "h_e"],
-    water_ids = ["h2o_c", "h2o_e"],
-    concentration_lb = 1e-9,
-    concentration_ub = 100e-3,
+    constant_concentrations = Dict{String,Float64}(),
+    proton_ids,
+    water_ids,
+    concentration_lb = 1e-9, # M
+    concentration_ub = 1e-1, # M
     T = 298.15, # Kelvin
     R = 8.31446261815324e-3, # kJ/K/mol
     ignore_reaction_ids = String[],
@@ -234,7 +231,7 @@ function max_min_driving_force_analysis(
     m = build_max_min_driving_force_model(
         model,
         reaction_standard_gibbs_free_energies;
-        flux_solution,
+        reference_flux,
         concentration_lb,
         concentration_ub,
         R,
@@ -243,12 +240,19 @@ function max_min_driving_force_analysis(
         water_ids,
         proton_ids,
     )
-    
-    m = add_metabolite_ratio_constraints!(
+
+    for (mid, val) in constant_concentrations 
+        m.log_metabolite_concentrations[Symbol(mid)].bound = log(val)
+    end
+
+    m = add_log_ratio_constraints(
         m,
-        concentration_ratios,
+        concentration_ratios;
+        name = :metabolite_ratio_constraints,
+        on = m.log_metabolite_concentrations,
     )
-    
+
+
     optimized_constraints(
         m;
         objective = m.max_min_driving_force.value,
@@ -256,3 +260,5 @@ function max_min_driving_force_analysis(
         modifications,
     )
 end
+
+export max_min_driving_force_analysis
