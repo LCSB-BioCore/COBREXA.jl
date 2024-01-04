e03338e3923ce37878fda279b4872297bf681a9c
Mirek Kratochvil <exa.exa@gmail.com>
2024-01-04 14:40:43 +0100

    knockout cleanup

--- src/builders/knockouts.jl
+++ src/builders/knockouts.jl
@@ -16,42 +16,40 @@
 
 """
 $(TYPEDSIGNATURES)
+
+Make a `ConstraintTree` that knocks out fluxes given by the predicate
+`knockout_test`. The predicate function is called with a single parameter (the
+key of the flux in tree `fluxes`) and must return a boolean. Returning `true`
+means that the corresponding flux (usually a reaction flux) will be knocked
+out.
+
+Use [`fbc_gene_knockout_constraints`](@ref) to apply gene knockouts easily to
+models with `AbstractFBCModel` interface.
 """
-knockout_constraints(; fluxes::C.ConstraintTree, knockout_test) = C.ConstraintTree(
-    rxn => C.Constraint(C.value(fluxes[rxn]), C.EqualTo(0.0)) for
-    rxn in keys(fluxes) if knockout_test(rxn)
+knockout_constraints(knockout_test, fluxes::C.ConstraintTree) = C.ConstraintTree(
+    id => C.Constraint(C.value(flux), 0) for (id, flux) in fluxes if knockout_test(id)
 )
 
 """
 $(TYPEDSIGNATURES)
+
+Make a `ConstraintTree` that simulates a gene knockout of `knockout_genes` in
+the `model` and disables corresponding `fluxes` accordingly.
+
+Keys of the fluxes must correspond to the reaction identifiers in the `model`.
+
+`knockout_genes` may be any collection that support element tests using `in`.
+Since the test is done many times, a `Set` is a preferred contained for longer
+lists of genes.
 """
-gene_knockouts(;
+fbc_gene_knockout_constraints(;
     fluxes::C.ConstraintTree,
-    ko_genes::Vector{String},
+    knockout_genes,
     model::A.AbstractFBCModel,
-) = knockout_constraints(;
-    fluxes,
-    knockout_test = rxn -> begin
-        maybe_avail = A.reaction_gene_products_available(
-            model,
-            string(rxn),
-            g -> !(g in ko_genes), # not available if knocked out
+) =
+    knockout_constraints(fluxes) do rid
+        maybemap(
+            !,
+            A.reaction_gene_products_available(model, string(rid), !in(knockout_genes)),
         )
-        isnothing(maybe_avail) ? false : !maybe_avail # negate here because of knockout_constraints
-    end,
-)
-
-#TODO remove the bang from here, there's no side effect
-"""
-$(TYPEDSIGNATURES)
-"""
-knockout!(ctmodel::C.ConstraintTree, ko_genes::Vector{String}, model::A.AbstractFBCModel) =
-    ctmodel * :gene_knockouts^gene_knockouts(; fluxes = ctmodel.fluxes, ko_genes, model)
-
-"""
-$(TYPEDSIGNATURES)
-
-Pipe-able variant.
-"""
-knockout!(ko_genes::Vector{String}, model::A.AbstractFBCModel) =
-    m -> knockout!(m, ko_genes, model)
+    end
