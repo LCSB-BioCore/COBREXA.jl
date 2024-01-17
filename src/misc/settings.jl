4b7a34166c437ab5f43beaf7b88735341888e6a3
Mirek Kratochvil <exa.exa@gmail.com>
2024-01-17 15:32:28 +0100

    whew we did!

--- src/misc/settings.jl
+++ src/misc/settings.jl
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#TODO: at this point, consider renaming the whole thing to "settings"
-
 """
 $(TYPEDSIGNATURES)
 
